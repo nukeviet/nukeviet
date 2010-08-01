71eabfa1f74cedd4e95e36e5d73c75f585dae8e4
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2010-08-01 15:56:41 +0000

    Sửa lỗi thêm, sửa block của module ảo

--- nukeviet3/admin/modules/themes/loadblocks.php
+++ nukeviet3/admin/modules/themes/loadblocks.php
@@ -1,26 +1,35 @@
 <?php
+
 /**
  * @Project NUKEVIET 3.0
  * @Author VINADES.,JSC (contact@vinades.vn)
  * @Copyright (C) 2010 VINADES.,JSC. All rights reserved
  * @Createdate 2-2-2010 12:55
  */
-if (! defined ( 'NV_IS_FILE_THEMES' ))
-	die ( 'Stop!!!' );
-$type = filter_text_input( 'type', 'get', '',1);
-$bid = $nv_Request->get_int('bid','get');
-list($file)= $db->sql_fetchrow($db->sql_query("SELECT file_path FROM `" . NV_BLOCKS_TABLE . "` WHERE bid=" . $bid . ""));
-if ($type == 'global') {
-	$block_file_list = nv_scandir ( NV_ROOTDIR . "/includes/blocks", $global_config ['check_block_global'] );
-	foreach ( $block_file_list as $value ) {
-		$sel = ($file==$value)?' selected':'';
-		echo "<option value=\"" . $value . "\" ".$sel.">" . $value . "</option>\n";
+if ( ! defined( 'NV_IS_FILE_THEMES' ) ) die( 'Stop!!!' );
+$type = filter_text_input( 'type', 'get', '', 1 );
+$bid = $nv_Request->get_int( 'bid', 'get' );
+list( $file ) = $db->sql_fetchrow( $db->sql_query( "SELECT file_path FROM `" . NV_BLOCKS_TABLE . "` WHERE bid=" . $bid . "" ) );
+if ( $type == 'global' )
+{
+	$block_file_list = nv_scandir( NV_ROOTDIR . "/includes/blocks", $global_config['check_block_global'] );
+	foreach ( $block_file_list as $value )
+	{
+		$sel = ( $file == $value ) ? ' selected' : '';
+		echo "<option value=\"" . $value . "\" " . $sel . ">" . $value . "</option>\n";
 	}
-} else {
-	$block_file_list = nv_scandir ( NV_ROOTDIR . "/modules/" . $type . '/blocks', $global_config ['check_block_module'] );
-	foreach ( $block_file_list as $value ) {
-		$sel = ($file==$value)?' selected':'';
-		echo "<option value=\"" . $value . "\" ".$sel.">" . $value . "</option>\n";
+} elseif ( isset( $site_mods[$type] ) )
+{
+	$module_file = $site_mods[$type]['module_file'];
+	if ( file_exists( NV_ROOTDIR . "/modules/" . $module_file . '/blocks' ) )
+	{
+		$block_file_list = nv_scandir( NV_ROOTDIR . "/modules/" . $module_file . '/blocks', $global_config['check_block_module'] );
+		foreach ( $block_file_list as $value )
+		{
+			$sel = ( $file == $value ) ? ' selected' : '';
+			echo "<option value=\"" . $value . "\" " . $sel . ">" . $value . "</option>\n";
+		}
 	}
 }
+
 ?>
