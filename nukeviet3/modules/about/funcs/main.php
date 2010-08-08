c62590def9b20d40a09a0919805cea190f9072e7
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2010-08-08 07:06:03 +0000

    Sửa module about để có thể dùng block vertical_menu

--- nukeviet3/modules/about/funcs/main.php
+++ nukeviet3/modules/about/funcs/main.php
@@ -9,52 +9,6 @@
 
 if ( ! defined( 'NV_IS_MOD_ABOUT' ) ) die( 'Stop!!!' );
 
-$abouts = array();
-$ab_links = array();
-$id = 0;
-$a = 0;
-$sql = $db->sql_query( "SELECT `id`,`title`,`alias` FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE `status`=1 ORDER BY `weight` ASC" );
-while ( $row = $db->sql_fetchrow( $sql ) )
-{
-    $a ++;
-    if ( $a == 1 )
-    {
-        $id = $row['id'];
-    }
-    $abouts[$row['id']] = array(  //
-        'title' => $row['title'], //
-		'alias' => $row['alias'], //
-		'link' => NV_BASE_SITEURL . "?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $row['alias'] . "-" . $row['id'], //
-		'act' => 0  //
-    );
-}
-
-if ( isset( $abouts[$tmp_id] ) and $abouts[$tmp_id]['alias'] == $tmp_alias_url )
-{
-    $abouts[$tmp_id]['act'] = 1;
-    $id = $tmp_id;
-}
-elseif ( $id )
-{
-    $abouts[$id]['act'] = 1;
-}
-
-if ( count( $abouts ) > 1 )
-{
-    foreach ( $abouts as $about )
-    {
-        $nv_vertical_menu[] = array( 
-            $about['title'], $about['link'], $about['act'] 
-        );
-        if ( ! $about['act'] )
-        {
-            $ab_links[] = array( 
-                "title" => $about['title'], "title" => $about['title'], "link" => $about['link'] 
-            );
-        }
-    }
-}
-
 $contents = "";
 
 if ( $id )
