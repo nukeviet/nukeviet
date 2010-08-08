71ff16fe1b947fb50ce95d353f4fad7ae90a202c
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2010-08-08 11:49:53 +0000

    commit lại file data/config_global.php do upload nhầm R53

--- nukeviet3/data/config_global.php
+++ nukeviet3/data/config_global.php
@@ -4,7 +4,7 @@
  * @Project NUKEVIET 3.0
  * @Author VINADES.,JSC (contact@vinades.vn)
  * @Copyright (C) 2010 VINADES.,JSC. All rights reserved
- * @Createdate Sun, 08 Aug 2010 11:29:25 GMT
+ * @Createdate Sat, 31 Jul 2010 08:22:54 GMT
  */
 
 if ( ! defined( 'NV_MAINFILE' ) )
@@ -12,59 +12,5 @@ if ( ! defined( 'NV_MAINFILE' ) )
     die( 'Stop!!!' );
 }
 
-$global_config['admfirewall'] = 1;
-$global_config['admin_theme'] = "admin_default";
-$global_config['allowloginchange'] = 0;
-$global_config['allowmailchange'] = 1;
-$global_config['allowuserlogin'] = 1;
-$global_config['allowuserreg'] = 1;
-$global_config['allow_adminlangs'] = "cs,en,fr,vi";
-$global_config['allow_sitelangs'] = "vi";
-$global_config['block_admin_ip'] = 0;
-$global_config['cookie_prefix'] = "nv3c_Zcuvh";
-$global_config['date_pattern'] = "l, d-m-Y";
-$global_config['dump_autobackup'] = 1;
-$global_config['dump_backup_day'] = 30;
-$global_config['dump_backup_ext'] = "gz";
-$global_config['error_send_email'] = "thao@vindes.vn";
-$global_config['file_allowed_ext'] = "images,flash,documents,archives";
-$global_config['forbid_extensions'] = "php";
-$global_config['forbid_mimes'] = "";
-$global_config['ftp_check_login'] = 0;
-$global_config['ftp_path'] = "/";
-$global_config['ftp_port'] = 21;
-$global_config['ftp_server'] = "localhost";
-$global_config['ftp_user_name'] = "";
-$global_config['ftp_user_pass'] = "";
-$global_config['gfx_chk'] = 0;
-$global_config['gzip_method'] = 1;
-$global_config['is_url_rewrite'] = 0;
-$global_config['is_user_forum'] = 0;
-$global_config['lang_multi'] = 1;
-$global_config['mailer_mode'] = "";
-$global_config['my_domains'] = "127.0.0.3";
-$global_config['online_upd'] = 1;
-$global_config['openid_mode'] = 1;
-$global_config['openid_servers'] = "yahoo,google,myopenid";
-$global_config['proxy_blocker'] = 0;
-$global_config['read_type'] = 0;
-$global_config['rewrite_optional'] = 0;
-$global_config['security_tags'] = "script|object|iframe|applet|meta|style|form|img|onmouseover|body";
-$global_config['session_prefix'] = "nv3s_Hnesw1";
-$global_config['site_email'] = "thao@vindes.vn";
-$global_config['site_keywords'] = "Nukeviet, portal, mysql, php";
-$global_config['site_lang'] = "vi";
-$global_config['site_phone'] = "";
-$global_config['site_timezone'] = "Asia/Bangkok";
-$global_config['smtp_host'] = "smtp.gmail.com";
-$global_config['smtp_password'] = "userpass";
-$global_config['smtp_port'] = 465;
-$global_config['smtp_ssl'] = 1;
-$global_config['smtp_username'] = "user@gmail.com";
-$global_config['statistic'] = 1;
-$global_config['str_referer_blocker'] = 0;
-$global_config['time_pattern'] = "H:i";
-$global_config['useactivate'] = 2;
-$global_config['version'] = "3.0.05";
 
 ?>
