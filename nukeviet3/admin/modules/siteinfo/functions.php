0cc60435957befcdfad0395b6b17b3aa9e5d6af5
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2010-08-13 17:39:25 +0000

    Tắt menu các chức năng về phpinfo nếu hàm phpinfo bị chặn

--- nukeviet3/admin/modules/siteinfo/functions.php
+++ nukeviet3/admin/modules/siteinfo/functions.php
@@ -12,13 +12,20 @@ if ( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) or ! defined( 'NV_IS_
 unset( $page_title, $select_options );
 global $global_config;
 
-$menu_top = array( "title" => $module_name, "module_file" => "", "custom_title" => $lang_global['mod_siteinfo']);
-$submenu['main'] = $lang_module['site_configs_info'];
-$submenu['php_info_configuration'] = $lang_module['configuration_php'];
-$submenu['php_info_modules'] = $lang_module['extensions'];
-$submenu['php_info_environment'] = $lang_module['environment_php'];
-$submenu['php_info_variables'] = $lang_module['variables_php'];
-$allow_func = array( 'main', 'php_info_configuration', 'php_info_modules', 'php_info_environment', 'php_info_variables' );
+$menu_top = array( 
+    "title" => $module_name, "module_file" => "", "custom_title" => $lang_global['mod_siteinfo'] 
+);
+if ( function_exists( 'phpinfo' ) and ! in_array( 'phpinfo', $sys_info['disable_functions'] ) )
+{
+    $submenu['main'] = $lang_module['site_configs_info'];
+    $submenu['php_info_configuration'] = $lang_module['configuration_php'];
+    $submenu['php_info_modules'] = $lang_module['extensions'];
+    $submenu['php_info_environment'] = $lang_module['environment_php'];
+    $submenu['php_info_variables'] = $lang_module['variables_php'];
+}
+$allow_func = array( 
+    'main', 'php_info_configuration', 'php_info_modules', 'php_info_environment', 'php_info_variables' 
+);
 define( 'NV_IS_FILE_SITEINFO', true );
 
 ?>
