b45b1eacb85feab90347518ade2b2fa35595e5f3
atnmos <atnmos@a957d178-3835-f50f-e15a-6e012bba7686>
2010-08-24 04:27:46 +0000

    Thêm function xóa các ký tự lạ, dấu câu

--- nukeviet3/includes/functions.php
+++ nukeviet3/includes/functions.php
@@ -728,6 +728,42 @@ function nv_htmlspecialchars( $string )
 }
 
 /**
+ * strip_punctuation()
+ * Xoa cac ky tu la
+ * 
+ * @param mixed $text
+ * @return
+ */
+function strip_punctuation( $text )
+{
+    $urlbrackets = '\[\]\(\)';
+    $urlspacebefore = ':;\'_\*%@&?!' . $urlbrackets;
+    $urlspaceafter = '\.,:;\'\-_\*@&\/\\\\\?!#' . $urlbrackets;
+    $urlall = '\.,:;\'\-_\*%@&\/\\\\\?!#' . $urlbrackets;
+
+    $specialquotes = '\'"\*<>';
+
+    $fullstop = '\x{002E}\x{FE52}\x{FF0E}';
+    $comma = '\x{002C}\x{FE50}\x{FF0C}';
+    $arabsep = '\x{066B}\x{066C}';
+    $numseparators = $fullstop . $comma . $arabsep;
+
+    $numbersign = '\x{0023}\x{FE5F}\x{FF03}';
+    $percent = '\x{066A}\x{0025}\x{066A}\x{FE6A}\x{FF05}\x{2030}\x{2031}';
+    $prime = '\x{2032}\x{2033}\x{2034}\x{2057}';
+    $nummodifiers = $numbersign . $percent . $prime;
+
+    return preg_replace( array( // Remove separator, control, formatting, surrogate, open/close quotes.
+        '/[\p{Z}\p{Cc}\p{Cf}\p{Cs}\p{Pi}\p{Pf}]/u', // Remove other punctuation except special cases
+        '/\p{Po}(?<![' . $specialquotes . $numseparators . $urlall . $nummodifiers . '])/u', // Remove non-URL open/close brackets, except URL brackets.
+        '/[\p{Ps}\p{Pe}](?<![' . $urlbrackets . '])/u', // Remove special quotes, dashes, connectors, number separators, and URL characters followed by a space
+        '/[' . $specialquotes . $numseparators . $urlspaceafter . '\p{Pd}\p{Pc}]+((?= )|$)/u', // Remove special quotes, connectors, and URL characters preceded by a space
+        '/((?<= )|^)[' . $specialquotes . $urlspacebefore . '\p{Pc}]+/u', // Remove dashes preceded by a space, but not followed by a number
+        '/((?<= )|^)\p{Pd}+(?![\p{N}\p{Sc}])/u', // Remove consecutive spaces
+        '/ +/', ), ' ', $text );
+}
+
+/**
  * nv_nl2br()
  * 
  * @param mixed $text
