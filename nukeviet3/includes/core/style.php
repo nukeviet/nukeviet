de552ac6a00122791c09066b0d85782b6fb44663
atnmos <atnmos@a957d178-3835-f50f-e15a-6e012bba7686>
2010-11-21 16:06:50 +0000

    Nâng cấp chức năng tối ưu CSS file (Trình duyệt không cần tải file về nếu nó không thay đổi nội dung so với lần trước)

--- nukeviet3/includes/core/style.php
+++ nukeviet3/includes/core/style.php
@@ -13,7 +13,93 @@ $expire = 60 * 60 * 24; //file CSS duoc luu 1 ngay
 
 $module = ( isset( $_GET['mod'] ) and preg_match( "/^[a-z0-9\-]+$/", $_GET['mod'] ) ) ? $_GET['mod'] : "";
 
+/**
+ * checkLastMod()
+ * 
+ * @param mixed $lastmod
+ * @param mixed $files
+ * @return void
+ */
+function checkLastMod( $lastmod, $files )
+{
+    if ( isset( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) )
+    {
+        $modsince = strtotime( $_SERVER['HTTP_IF_MODIFIED_SINCE'] );
+        if ( $modsince != -1 && $modsince == $lastmod )
+        {
+            header( "HTTP/1.0 304 Not Modified" );
+            header( 'Content-Length: 0' );
+            exit();
+        }
+    }
+    else
+    {
+        $hash = $lastmod . '-' . md5( $files );
+
+        if ( isset( $_SERVER['HTTP_IF_NONE_MATCH'] ) && stripslashes( $_SERVER['HTTP_IF_NONE_MATCH'] ) == '"' . $hash . '"' )
+        {
+            header( "HTTP/1.0 304 Not Modified" );
+            header( 'Content-Length: 0' );
+            exit();
+        }
+
+        header( "Etag: \"" . $hash . "\"" );
+    }
+}
+
+/**
+ * callback()
+ * 
+ * @param mixed $matches
+ * @return
+ */
+function callback( $matches )
+{
+    global $cssdir, $realPathCssModDir;
+
+    $m2 = str_replace( '\\', '/', realpath( $realPathCssModDir . "/" . $matches[2] ) );
+    $dir = str_replace( '\\', '/', $cssdir );
+    $m2 = explode( "/", $m2 );
+    $dir = explode( "/", $dir );
+    $relative = array();
+    foreach ( $dir as $index => $part )
+    {
+        if ( isset( $m2[$index] ) && $m2[$index] == $part )
+        {
+            continue;
+        }
+
+        $relative[] = '..';
+    }
+
+    foreach ( $m2 as $index => $part )
+    {
+        if ( isset( $dir[$index] ) && $dir[$index] == $part )
+        {
+            continue;
+        }
+
+        $relative[] = $part;
+    }
+
+    $relative = implode( "/", $relative );
+
+    return $matches[1] . $relative . $matches[4];
+}
+
 $cssModContent = "";
+
+$lastmod = 0;
+$files = "";
+
+foreach ( $cssFiles as $file )
+{
+    $files .= $cssdir . "/" . $file;
+    $filemtime = filemtime( $cssdir . "/" . $file );
+    $lastmod = max( $lastmod, $filemtime );
+}
+
+$is_checkLastmod = false;
 
 if ( ! empty( $module ) )
 {
@@ -32,45 +118,11 @@ if ( ! empty( $module ) )
 
     if ( ! empty( $cssModFile ) )
     {
-        /**
-         * callback()
-         * 
-         * @param mixed $matches
-         * @return
-         */
-        function callback( $matches )
-        {
-            global $cssdir, $realPathCssModDir;
-            
-            $m2 = str_replace( '\\', '/', realpath($realPathCssModDir . "/" . $matches[2]) );
-            $dir = str_replace( '\\', '/', $cssdir );
-            $m2 = explode( "/",  $m2);
-            $dir = explode( "/", $dir );
-            $relative = array();
-            foreach ( $dir as $index => $part )
-            {
-                if ( isset( $m2[$index] ) && $m2[$index] == $part )
-                {
-                    continue;
-                }
-
-                $relative[] = '..';
-            }
-
-            foreach ( $m2 as $index => $part )
-            {
-                if ( isset( $dir[$index] ) && $dir[$index] == $part )
-                {
-                    continue;
-                }
-
-                $relative[] = $part;
-            }
-            
-            $relative = implode("/",$relative);
-
-            return $matches[1] . $relative . $matches[4];
-        }
+        $is_checkLastmod = true;
+        $files .= $cssModFile;
+        $filemtime = filemtime( $cssModFile );
+        $lastmod = max( $lastmod, $filemtime );
+        checkLastMod( $lastmod, $files );
 
         ob_start();
         include ( $cssModFile );
@@ -83,6 +135,11 @@ if ( ! empty( $module ) )
             $cssModContent = preg_replace_callback( "/(url\()((?!http(s?)|ftp\:\/\/)[^\)]+)(\))/", "callback", $cssModContent );
         }
     }
+}
+
+if ( ! $is_checkLastmod )
+{
+    checkLastMod( $lastmod, $files );
 }
 
 ob_start();
@@ -102,13 +159,13 @@ $cssContent = preg_replace( "/[\s]+/", " ", $cssContent ); //Xoa khoang trang
 $cssContent = preg_replace( array( "/[\s]*[\;]+[\s]*\}/", "/[\s]*[\;]+[\s]*/", "/[\s]*[\}]+[\s]*/", "/[\s]*[\{]+[\s]*/", "/[\s]*[\:]+[\s]*/", "/[\s]*[\,]+[\s]*/" ), array( "}", ";", "}", "{", ":", "," ), $cssContent );
 $cssContent = preg_replace( "/[^\}]+\{[\s|\;]*\}[\s]*/", "", $cssContent ); //Xoa nhung css khong co noi dung
 
-$expire = "expires: " . gmdate( "D, d M Y H:i:s", time() + $expire ) . " GMT";
+$currenttime = time();
 
 ob_start( "ob_gzhandler" );
 @header( "Content-Type: text/css; charset=utf-8" );
-@header( "cache-control: must-revalidate" );
-@Header( "Last-Modified: " . gmdate( "D, d M Y H:i:s", strtotime( "-1 day" ) ) . " GMT" );
-@header( $expire );
+@header( 'Cache-Control: public; max-age=' . $expire );
+@header( 'Last-Modified: ' . gmdate( "D, d M Y H:i:s", $lastmod ) . " GMT" );
+@header( "expires: " . gmdate( "D, d M Y H:i:s", $currenttime + $expire ) . " GMT" );
 
 echo $cssContent;
 
