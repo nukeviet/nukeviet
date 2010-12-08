fc76e225599422d4a6d6622e1e384b3e648d453e
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2010-12-08 09:21:35 +0000

    Module News: sửa chức năng siteinfo

--- nukeviet3/modules/news/siteinfo.php
+++ nukeviet3/modules/news/siteinfo.php
@@ -12,7 +12,7 @@ if ( ! defined( 'NV_IS_FILE_SITEINFO' ) ) die( 'Stop!!!' );
 $lang_siteinfo = nv_get_lang_module( $mod );
 
 // Tong so bai viet 
-list( $number ) = $db->sql_fetchrow( $db->sql_query("SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `status`= 1 AND `publtime` < " . NV_CURRENTTIME . " AND (`exptime`=0 OR `exptime`>" . NV_CURRENTTIME . ")"));
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `status`= 1 AND `publtime` < " . NV_CURRENTTIME . " AND (`exptime`=0 OR `exptime`>" . NV_CURRENTTIME . ")" ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
@@ -20,19 +20,30 @@ if ( $number > 0 )
     );
 }
 
-// So bai viet thanh vien gui toi  
-/*$sql = "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_users_send`";
-$array_data = nv_db_cache( $sql, '', $mod );
-$number = isset( $array_data[0]['number'] ) ? intval( $array_data[0]['number'] ) : 0;
+//So bai viet thanh vien gui toi
+if ( ! empty( $site_mods[$mod]['admins'] ) )
+{
+    $admins_module = explode( ",", $site_mods[$mod]['admins'] );
+}
+else
+{
+    $admins_module = array();
+}
+$result = $db->sql_query( "SELECT `admin_id` FROM `" . NV_AUTHORS_GLOBALTABLE . "` WHERE `lev`=1 or `lev`=2" );
+while ( $row = $db->sql_fetchrow( $result ) )
+{
+    $admins_module[] = $row['admin_id'];
+}
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `admin_id` not in (" . implode( ",", $admins_module ) . ") " ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
         'key' => $lang_siteinfo['siteinfo_users_send'], 'value' => $number 
     );
-}*/
+}
 
-// So bai viet cho dang 
-list( $number ) = $db->sql_fetchrow( $db->sql_query("SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `status`= 0 AND `publtime` < " . NV_CURRENTTIME . " AND (`exptime`=0 OR `exptime`>" . NV_CURRENTTIME . ")"));
+// So bai viet cho dang tu dong
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `status`= 1 AND `publtime` > " . NV_CURRENTTIME . " AND (`exptime`=0 OR `exptime`>" . NV_CURRENTTIME . ")" ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
@@ -41,7 +52,7 @@ if ( $number > 0 )
 }
 
 // So bai viet da het han  
-list( $number ) = $db->sql_fetchrow( $db->sql_query("SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `exptime` > 0 AND `exptime`<" . NV_CURRENTTIME.""));
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `exptime` > 0 AND `exptime`<" . NV_CURRENTTIME . "" ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
@@ -50,7 +61,7 @@ if ( $number > 0 )
 }
 
 // So bai viet sap het han   
-list( $number ) = $db->sql_fetchrow( $db->sql_query("SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `status` = 1 AND `exptime`>" . NV_CURRENTTIME.""));
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` where `status` = 1 AND `exptime`>" . NV_CURRENTTIME . "" ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
@@ -59,7 +70,7 @@ if ( $number > 0 )
 }
 
 // Tong so binh luan duoc dang   
-list( $number ) = $db->sql_fetchrow( $db->sql_query("SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_comments` where `status` = 1"));
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_comments` where `status` = 1" ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
@@ -68,7 +79,7 @@ if ( $number > 0 )
 }
 
 // So binh luan cho duyet   
-list( $number ) = $db->sql_fetchrow( $db->sql_query("SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_comments` where `status` = 0"));
+list( $number ) = $db->sql_fetchrow( $db->sql_query( "SELECT COUNT(*) as number FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_comments` where `status` = 0" ) );
 if ( $number > 0 )
 {
     $siteinfo[] = array( 
