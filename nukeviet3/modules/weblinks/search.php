6aa7775654e224709bfa447c4170aa6998b16c3a
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2010-12-10 07:41:25 +0000

    Sửa chức năng tìm kiếm module weblink

--- nukeviet3/modules/weblinks/search.php
+++ nukeviet3/modules/weblinks/search.php
@@ -9,9 +9,10 @@
 
 if ( ! defined( 'NV_IS_MOD_SEARCH' ) ) die( 'Stop!!!' );
 
-$sql = "SELECT SQL_CALC_FOUND_ROWS `id`,`title`,`description` 
+$sql = "SELECT SQL_CALC_FOUND_ROWS `id`,`catid`, `title`, `alias`, `description` 
 FROM `" . NV_PREFIXLANG . "_" . $m_values['module_data'] . "_rows` 
 WHERE (" . nv_like_logic( 'title', $dbkeyword, $logic ) . " 
+OR " . nv_like_logic( 'url', $dbkeyword, $logic ) . " 
 OR " . nv_like_logic( 'description', $dbkeyword, $logic ) . ") 
 LIMIT " . $pages . "," . $limit;
 
@@ -22,17 +23,23 @@ list( $all_page ) = $db->sql_fetchrow( $result );
 
 if ( $all_page )
 {
-    $link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $m_values['module_name'] . '&amp;' . NV_OP_VARIABLE . '=visitlink&amp;id=';
-
-    while ( list( $id, $tilterow, $content ) = $db->sql_fetchrow( $tmp_re ) )
+    $array_cat_url = array();
+    $array_cat_url[0] = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $m_values['module_name'] . "&amp;" . NV_OP_VARIABLE . "=other";
+    
+    $sql_cat = "SELECT `catid`, `alias` FROM `" . NV_PREFIXLANG . "_" . $m_values['module_data'] . "_cat`";
+    $re_cat = $db->sql_query( $sql_cat );
+    while ( list( $catid, $alias ) = $db->sql_fetchrow( $re_cat ) )
     {
-        $url = $link . $alias . "-" . $id;
-
-        $result_array[] = array( //
-            'link' => $link . $id, //
+        $array_cat_url[$catid] = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $m_values['module_name'] . "&amp;" . NV_OP_VARIABLE . "=" . $alias;
+    }
+    
+    while ( list( $id, $catid, $tilterow, $alias, $content ) = $db->sql_fetchrow( $tmp_re ) )
+    {
+        $result_array[] = array(  //
+            'link' => $array_cat_url[$catid] . '/' . $alias, //
             'title' => BoldKeywordInStr( $tilterow, $key, $logic ), //
-            'content' => BoldKeywordInStr( $content, $key, $logic ) //
-            );
+            'content' => BoldKeywordInStr( $content, $key, $logic )  //
+        );
     }
 }
 
