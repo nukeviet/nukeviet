5e12e48ad57fdf9a94c858cbddd10e3e68f58daf
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2011-01-27 09:03:15 +0000

    Sửa lỗi trang trắng khi dùng theme admin_full trên byethost

--- nukeviet3/admin/modules/webtools/functions.php
+++ nukeviet3/admin/modules/webtools/functions.php
@@ -14,21 +14,21 @@ $submenu['siteDiagnostic'] = $lang_module['siteDiagnostic'];
 $submenu['keywordRank'] = $lang_module['keywordRank'];
 $submenu['sitemapPing'] = $lang_module['sitemapPing'];
 $submenu['checkupdate'] = $lang_module['checkupdate'];
-
 //$submenu['revision'] = $lang_module['revision'];
 
-$new_version = nv_geVersion( 86400 ); //kem tra lai sau 24 tieng
-if ( ! empty( $new_version ) )
-{
-    global $global_config;
-	if ( nv_version_compare( $global_config['version'], $new_version->version ) < 0 )
-    {
-        $submenu['autoupdate'] = $lang_module['autoupdate_system'];
-    }
-}
 
 if ( $module_name == "webtools" )
 {
+    $new_version = nv_geVersion( 86400 ); //kem tra lai sau 24 tieng
+    if ( ! empty( $new_version ) )
+    {
+        global $global_config;
+        if ( nv_version_compare( $global_config['version'], $new_version->version ) < 0 )
+        {
+            $submenu['autoupdate'] = $lang_module['autoupdate_system'];
+        }
+    }
+    
     $allow_func = array( 
         'main', 'clearsystem', 'sitemapPing', 'checkupdate', 'revision', 'siteDiagnostic', 'keywordRank', 'autoupdate' 
     );
