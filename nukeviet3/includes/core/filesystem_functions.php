a0a854da96276d9380271bdb8e134cf25b4777d4
atnmos <atnmos@a957d178-3835-f50f-e15a-6e012bba7686>
2011-01-27 23:10:54 +0000

    Chuẩn bị thay mới module upload

--- nukeviet3/includes/core/filesystem_functions.php
+++ nukeviet3/includes/core/filesystem_functions.php
@@ -245,8 +245,9 @@ function nv_string_to_filename( $word )
 {
     $word = nv_EncString( $word );
     $word = preg_replace( '/[^a-z0-9\.\-\_ ]/i', '', $word );
-    $word = preg_replace( '/\s+/', '_', $word );
-    return preg_replace( '/\W-/', '', $word );
+    $word = preg_replace( '/^\W+|\W+$/', '', $word );
+    $word = preg_replace( '/\s+/', '-', $word );
+    return strtolower( preg_replace( '/\W-/', '', $word ) );
 }
 
 /**
