144d587b568f7bea2e450022fab206f736f875e8
laser.hp <laser.hp@a957d178-3835-f50f-e15a-6e012bba7686>
2011-01-30 16:12:07 +0000

    Cập nhật thông tin NukeViet 3.1.01

--- nukeviet3/install/config.php
+++ nukeviet3/install/config.php
@@ -40,7 +40,7 @@ $global_config['engine_allowed'] = array();
 $global_config['site_theme'] = "modern";
 $global_config['gfx_chk'] = 3;
 
-$global_config['version'] = "3.1.00"; //NUKEVIET 3.1 beta
-$global_config['revision'] = 782;
+$global_config['version'] = "3.1.01"; //NUKEVIET 3.1 RC1
+$global_config['revision'] = 851;
 
 ?>
