52d1265f7e69ca7277b29000a552b3e53d27f09f
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2011-02-10 08:35:03 +0000

    Module download: sửa chức năng tìm kiếm

--- nukeviet3/modules/download/funcs/search.php
+++ nukeviet3/modules/download/funcs/search.php
@@ -10,17 +10,20 @@ if ( ! defined( 'NV_IS_MOD_DOWNLOAD' ) ) die( 'Stop!!!' );
 
 global $global_config, $lang_module, $lang_global, $module_info, $module_name, $module_file, $nv_Request;
 $page_title = $lang_module['search'];
-$xtpl = new XTemplate( "viewcat_page.tpl", NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/" . $module_file . "/" );
-$xtpl->assign( 'LANG', $lang_module );
-$xtpl->assign( 'SEARCHTITLE', $lang_module['search_result'] );
+
 $page = $nv_Request->get_int( 'page', 'get', 0 );
 $per_page = 15;
 $base_url = NV_BASE_SITEURL . "?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . '&op=search';
 
-if ( $nv_Request->isset_request( 'submit', 'post' ) )
+$sql = "SELECT SQL_CALC_FOUND_ROWS `id`, `catid`, `title`, `alias`, `introtext` , `uploadtime`, 
+`author_name`, `filesize`, `fileimage`, `view_hits`, `download_hits`, `comment_allow`, `comment_hits` 
+FROM `" . NV_PREFIXLANG . "_" . $module_data . "`";
+
+$key = filter_text_input( 'q', 'post', '', 1, NV_MAX_SEARCH_LENGTH );
+
+if ( $nv_Request->isset_request( 'submit', 'post' ) and ! empty( $key ) )
 {
-    $key = filter_text_input( 'q', 'post', '', 1, NV_MAX_SEARCH_LENGTH );
-    $cat = $nv_Request->get_int( 'cat', 'post' );
+    $cat = $nv_Request->get_int( 'cat', 'post', 0 );
     if ( ! empty( $cat ) )
     {
         $allcat = array();
@@ -31,53 +34,82 @@ if ( $nv_Request->isset_request( 'submit', 'post' ) )
     {
         $allcat = '';
     }
-    
-    $sql1 = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE title LIKE '%" . $key . "%' " . $allcat . " AND `status`=1";
-    $sql2 = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE title LIKE '%" . $key . "%' " . $allcat . " AND `status`=1 ORDER BY `uploadtime` DESC LIMIT " . $page . ", " . $per_page;
+    $dbkey = $db->dblikeescape( $key );
+    $sql .= "WHERE (`title` LIKE '%" . $dbkey . "%' OR `description` LIKE '%" . $dbkey . "%' OR `introtext` LIKE '%" . $dbkey . "%') " . $allcat . " AND  `status`='1'";
 }
 else
 {
-    $sql1 = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE `status`=1";
-    $sql2 = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE `status`=1 ORDER BY `uploadtime` DESC LIMIT " . $page . ", " . $per_page;
+    $sql .= "WHERE `status`='1'";
 }
-$result = $db->sql_query( $sql1 );
-list( $all_page ) = $db->sql_fetchrow( $result );
+$sql .= "ORDER BY `uploadtime` DESC LIMIT " . $page . ", " . $per_page;
+
+$result = $db->sql_query( $sql );
+
+$result_all = $db->sql_query( "SELECT FOUND_ROWS()" );
+list( $all_page ) = $db->sql_fetchrow( $result_all );
+
 if ( ! empty( $all_page ) )
 {
-    $result = $db->sql_query( $sql2 );
-    $imageinfo = array();
+    $download_config = initial_config_data();
+    $list_cats = nv_list_cats( true );
+    
+    $array = array();
+    $today = mktime( 0, 0, 0, date( "n" ), date( "j" ), date( "Y" ) );
+    $yesterday = $today - 86400;
+    
     while ( $row = $db->sql_fetchrow( $result ) )
     {
-        list( $catalias ) = $db->sql_fetchrow( $db->sql_query( "SELECT alias FROM `" . NV_PREFIXLANG . "_" . $module_data . "_categories` WHERE id=" . $row['catid'] . "" ) );
-        $row['filesize'] = ! empty( $row['filesize'] ) ? nv_convertfromBytes( $row['filesize'] ) : "";
-        $row['view_hits'] = ( int )$row['view_hits'];
-        $row['download_hits'] = ( int )$row['download_hits'];
-        $row['more_link'] = NV_BASE_SITEURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=" . $catalias . '/' . $row['alias'];
-        $xtpl->assign( 'ROW', $row );
-        $imageinfo['src'] = $row['fileimage'];
-        if ( ! empty( $row['fileimage'] ) )
+        $cattitle = "<a href=\"" . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $list_cats[$row['catid']]['alias'] . "\">" . $list_cats[$row['catid']]['title'] . "</a>";
+        $more_link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $list_cats[$row['catid']]['alias'] . "/" . $row['alias'];
+        
+        $uploadtime = ( int )$row['uploadtime'];
+        if ( $uploadtime >= $today )
         {
-            $xtpl->assign( 'FILEIMAGE', $imageinfo );
-            $xtpl->parse( 'main.row.is_image' );
+            $uploadtime = $lang_module['today'] . ", " . date( "H:i", $row['uploadtime'] );
         }
-        $xtpl->parse( 'main.row' );
-    }
+        elseif ( $uploadtime >= $yesterday )
+        {
+            $uploadtime = $lang_module['yesterday'] . ", " . date( "H:i", $row['uploadtime'] );
+        }
+        else
+        {
+            $uploadtime = nv_date( "d/m/Y H:i", $row['uploadtime'] );
+        }
+        
+        $img = substr( $row['fileimage'], strlen( NV_BASE_SITEURL ) );
+        $imageinfo = nv_ImageInfo( NV_ROOTDIR . '/' . $img, 300, true, NV_UPLOADS_REAL_DIR . '/' . $module_name . '/thumb' );
+        
+        $array[$row['id']] = array(  //
+            'id' => ( int )$row['id'], //
+			'title' => $row['title'], //
+			'cattitle' => $cattitle, //
+			'introtext' => $row['introtext'], //
+			'uploadtime' => $uploadtime, //
+			'author_name' => $row['author_name'], //
+			'filesize' => ! empty( $row['filesize'] ) ? nv_convertfromBytes( $row['filesize'] ) : "", //
+			'fileimage' => $imageinfo, //
+			'view_hits' => ( int )$row['view_hits'], //
+			'download_hits' => ( int )$row['download_hits'], //
+			'more_link' => $more_link, //
+			'edit_link' => ( defined( 'NV_IS_MODADMIN' ) ) ? NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;edit=1&amp;id=" . ( int )$row['id'] : "", //
+			'del_link' => ( defined( 'NV_IS_MODADMIN' ) ) ? NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name : "" 
+        );
+        
+        if ( $row['comment_allow'] )
+        {
+            $array[$row['id']]['comment_hits'] = ( int )$row['comment_hits'];
+        }
     
-    $generate_page = nv_generate_page( $base_url, $all_page, $per_page, $page );
-    if ( ! empty( $generate_page ) )
-    {
-        $xtpl->assign( 'GENERATE_PAGE', $generate_page );
-        $xtpl->parse( 'main.generate_page' );
     }
+    $generate_page = x_generate_page( $base_url, $all_page, $per_page, $page );
+    
+    $contents = theme_viewcat_download( $array, $download_config, "", $generate_page );
+
 }
 else
 {
-    $xtpl->assign( 'GENERATE_PAGE', $lang_module['search_noresult'] );
-    $xtpl->parse( 'main.generate_page' );
+    $contents = $lang_module['search_noresult'];
 }
-
-$xtpl->parse( 'main' );
-$contents = $xtpl->text( 'main' );
 
 include ( NV_ROOTDIR . "/includes/header.php" );
 echo nv_site_theme( $contents );
