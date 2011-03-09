d0741c412844a58efef764dddc144d3a0218699f
phantandung92@gmail.com <phantandung92@gmail.com@a957d178-3835-f50f-e15a-6e012bba7686>
2011-03-09 18:29:43 +0000

    Modules news phần admin, tách HTML ra khỏi PHP trong file main.php

--- nukeviet3/modules/news/admin/main.php
+++ nukeviet3/modules/news/admin/main.php
@@ -13,6 +13,7 @@ $stype = $nv_Request->get_string( 'stype', 'get', '-' );
 $catid = $nv_Request->get_int( 'catid', 'get', 0 );
 $per_page_old = $nv_Request->get_int( 'per_page', 'cookie', 50 );
 $per_page = $nv_Request->get_int( 'per_page', 'get', $per_page_old );
+
 if ( $per_page < 1 and $per_page > 500 )
 {
     $per_page = 50;
@@ -26,8 +27,13 @@ $q = filter_text_input( 'q', 'get', '', 1 );
 $ordername = $nv_Request->get_string( 'ordername', 'get', 'publtime' );
 $order = $nv_Request->get_string( 'order', 'get' ) == "asc" ? 'asc' : 'desc';
 
-$sl = ( $catid == 0 ) ? " selected=\"selected\"" : "";
-$val_cat_content = "<option value=\"0\" " . $sl . ">" . $lang_module['search_cat_all'] . "</option>\n";
+$val_cat_content = array();
+$val_cat_content[] = array(
+	"value" => 0,
+	"selected" => ( $catid == 0 ) ? " selected=\"selected\"" : "",
+	"title" => $lang_module['search_cat_all']
+);
+
 $array_cat_view = array();
 foreach ( $global_array_cat as $catid_i => $array_value )
 {
@@ -60,6 +66,7 @@ foreach ( $global_array_cat as $catid_i => $array_value )
             $check_cat = true;
         }
     }
+	
     if ( $check_cat )
     {
         $xtitle_i = "";
@@ -79,10 +86,16 @@ foreach ( $global_array_cat as $catid_i => $array_value )
         {
             $sl = " selected=\"selected\"";
         }
-        $val_cat_content .= "<option value=\"" . $catid_i . "\" " . $sl . ">" . $xtitle_i . "</option>\n";
-        $array_cat_view[] = $catid_i;
+		
+		$val_cat_content[] = array(
+			"value" => $catid_i,
+			"selected" => $sl,
+			"title" => $xtitle_i
+		);
+		$array_cat_view[] = $catid_i;
     }
 }
+
 if ( ! defined( 'NV_IS_ADMIN_MODULE' ) and $catid > 0 and ! in_array( $catid, $array_cat_view ) )
 {
     Header( "Location: " . NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=main" );
@@ -90,22 +103,35 @@ if ( ! defined( 'NV_IS_ADMIN_MODULE' ) and $catid > 0 and ! in_array( $catid, $a
 }
 
 $array_search = array( 
-    "-" => "---", "title" => $lang_module['search_title'], "bodytext" => $lang_module['search_bodytext'], "author" => $lang_module['search_author'], "admin_id" => $lang_module['search_admin'] 
+    "-" => "---", 
+	"title" => $lang_module['search_title'], 
+	"bodytext" => $lang_module['search_bodytext'], 
+	"author" => $lang_module['search_author'], 
+	"admin_id" => $lang_module['search_admin'] 
 );
+
 $array_in_rows = array( 
-    "title", "bodytext", "author" 
+    "title", 
+	"bodytext", 
+	"author" 
 );
+
 $array_in_ordername = array( 
-    "title", "publtime", "exptime" 
+    "title", 
+	"publtime", 
+	"exptime" 
 );
+
 if ( ! in_array( $stype, array_keys( $array_search ) ) )
 {
     $stype = "-";
 }
+
 if ( ! in_array( $ordername, array_keys( $array_in_ordername ) ) )
 {
     $ordername = "id";
 }
+
 if ( $catid == 0 )
 {
     $from = "`" . NV_PREFIXLANG . "_" . $module_data . "_rows` as r LEFT JOIN " . NV_USERS_GLOBALTABLE . " as u ON r.admin_id=u.userid";
@@ -114,9 +140,11 @@ else
 {
     $from = "`" . NV_PREFIXLANG . "_" . $module_data . "_" . $catid . "` as r LEFT JOIN " . NV_USERS_GLOBALTABLE . " as u ON r.admin_id=u.userid";
 }
+
 $where = "";
 $page = $nv_Request->get_int( 'page', 'get', 0 );
 $checkss = $nv_Request->get_string( 'checkss', 'get', '' );
+
 if ( $checkss == md5( session_id() ) )
 {
     if ( in_array( $stype, $array_in_rows ) and ! empty( $q ) )
@@ -152,42 +180,39 @@ if ( ! defined( 'NV_IS_ADMIN_MODULE' ) )
 }
 
 $link_i = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=Other";
+
 $global_array_cat[0] = array( 
-    "catid" => 0, "parentid" => 0, "title" => "Other", "alias" => "Other", "link" => $link_i, "viewcat" => "viewcat_page_new", "subcatid" => 0, "numlinks" => 3, "description" => "", "keywords" => "" 
+    "catid" => 0, 
+	"parentid" => 0, 
+	"title" => "Other", 
+	"alias" => "Other", 
+	"link" => $link_i, 
+	"viewcat" => "viewcat_page_new", 
+	"subcatid" => 0, "numlinks" => 3, 
+	"description" => "", 
+	"keywords" => "" 
 );
 
-$contents = "";
-$contents .= "<form action=\"" . NV_BASE_ADMINURL . "index.php\" method=\"get\">";
-$contents .= "<input type=\"hidden\" name =\"" . NV_NAME_VARIABLE . "\"value=\"" . $module_name . "\" />";
-$contents .= "<input type=\"hidden\" name =\"" . NV_OP_VARIABLE . "\"value=\"" . $op . "\" />";
-$contents .= "<label>" . $lang_module['search_cat'] . ": </label>\n";
-$contents .= "<select name=\"catid\">\n";
-$contents .= $val_cat_content;
-$contents .= "</select> \n";
-$contents .= " <label>" . $lang_module['search_type'] . ": </label>\n";
-$contents .= "<select name=\"stype\">\n";
+$search_type = array();
 foreach ( $array_search as $key => $val )
 {
-    $contents .= "<option value=\"" . $key . "\"" . ( ( $key == $stype ) ? " selected=\"selected\"" : "" ) . ">" . $val . "</option>\n";
+	$search_type[] = array(
+		"key" => $key,
+		"value" => $val,
+		"selected" => ( $key == $stype ) ? " selected=\"selected\"" : ""
+	);
 }
-$contents .= "</select>";
 
 $i = 5;
-$contents .= " <label>" . $lang_module['search_per_page'] . ": </label>\n";
-$contents .= "<select name=\"per_page\">\n";
+$search_per_page = array();
 while ( $i <= 1000 )
 {
-    $contents .= "<option value=\"" . $i . "\"" . ( ( $i == $per_page ) ? " selected=\"selected\"" : "" ) . ">" . $i . "</option>\n";
-    $i = $i + 5;
+	$search_per_page[] = array (
+		"page" => $i,
+		"selected" => ( $i == $per_page ) ? " selected=\"selected\"" : ""
+	);
+	$i = $i + 5;
 }
-$contents .= "</select>";
-$contents .= "<br />\n";
-
-$contents .= "" . $lang_module['search_key'] . ": <input type=\"text\" value=\"" . $q . "\" maxlength=\"64\" name=\"q\" style=\"width: 265px\" />\n";
-$contents .= "<input type=\"submit\" value=\"" . $lang_module['search'] . "\" /><br />\n";
-$contents .= "<input type=\"hidden\" name =\"checkss\"value=\"" . md5( session_id() ) . "\" />";
-$contents .= "<label><em>" . $lang_module['search_note'] . "</em></label>\n";
-$contents .= "</form>\n";
 
 $a = 0;
 $order2 = ( $order == "asc" ) ? "desc" : "asc";
@@ -196,21 +221,10 @@ $base_url_name = "" . NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" .
 $base_url_publtime = "" . NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $op . "&amp;per_page=" . $per_page . "&amp;catid=" . $catid . "&amp;stype=" . $stype . "&amp;q=" . $q . "&amp;checkss=" . $checkss . "&amp;ordername=publtime&amp;order=" . $order2 . "&amp;page=" . $page;
 $base_url_exptime = "" . NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $op . "&amp;per_page=" . $per_page . "&amp;catid=" . $catid . "&amp;stype=" . $stype . "&amp;q=" . $q . "&amp;checkss=" . $checkss . "&amp;ordername=exptime&amp;order=" . $order2 . "&amp;page=" . $page;
 
-$contents .= "<form name=\"block_list\" action=\"\">";
-$contents .= "<table summary=\"\" class=\"tab1\">\n";
-$contents .= "<thead>";
-$contents .= "<tr>\n";
-$contents .= "<td align=\"center\"><input name=\"check_all[]\" type=\"checkbox\" value=\"yes\" onclick=\"nv_checkAll(this.form, 'idcheck[]', 'check_all[]',this.checked);\" /></td>\n";
-$contents .= "<td><a href=\"" . $base_url_name . "\">" . $lang_module['name'] . "</a></td>\n";
-$contents .= "<td align=\"center\"><a href=\"" . $base_url_publtime . "\">" . $lang_module['content_publ_date'] . "</a></td>\n";
-$contents .= "<td align=\"center\">" . $lang_module['status'] . "</td>\n";
-$contents .= "<td>" . $lang_module['content_admin'] . "</td>\n";
-$contents .= "<td></td>\n";
-$contents .= "</tr>\n";
-$contents .= "</thead>";
-
 $base_url = "" . NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $op . "&amp;per_page=" . $per_page . "&amp;catid=" . $catid . "&amp;stype=" . $stype . "&amp;q=" . $q . "&amp;checkss=" . $checkss . "&amp;ordername=" . $ordername . "&amp;order=" . $order;
+
 $ord_sql = "ORDER BY r." . $ordername . " " . $order . "";
+
 $sql = "SELECT SQL_CALC_FOUND_ROWS r.id, r.listcatid, r.admin_id, r.title, r.alias, r.status , r.publtime, r.exptime, u.username  FROM " . $from . " " . $where . " " . $ord_sql . " LIMIT " . $page . "," . $per_page;
 $result = $db->sql_query( $sql );
 
@@ -218,6 +232,7 @@ $result_all = $db->sql_query( "SELECT FOUND_ROWS()" );
 list( $numf ) = $db->sql_fetchrow( $result_all );
 $all_page = ( $numf ) ? $numf : 1;
 
+$data = array();
 while ( list( $id, $listcatid, $post_id, $title, $alias, $status, $publtime, $exptime, $username ) = $db->sql_fetchrow( $result ) )
 {
     if ( $status == 0 )
@@ -306,53 +321,103 @@ while ( list( $id, $listcatid, $post_id, $title, $alias, $status, $publtime, $ex
             $check_permission_delete = true;
         }
     }
+	
     $admin_funcs = array();
     if ( $check_permission_edit ) $admin_funcs[] = nv_link_edit_page( $id );
     if ( $check_permission_delete ) $admin_funcs[] = nv_link_delete_page( $id );
     
     $link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $global_array_cat[$catid_i]['alias'] . "/" . $alias . "-" . $id;
-    $contents .= "<tbody" . $class . ">";
-    $contents .= "<tr align=\"center\">\n";
-    $contents .= "<td align=\"center\"><input type=\"checkbox\" onclick=\"nv_UncheckAll(this.form, 'idcheck[]', 'check_all[]', this.checked);\" value=\"" . $id . "\" name=\"idcheck[]\" /></td>\n";
-    $contents .= "<td align=\"left\"><a target=\"_blank\" href=\"" . $link . "\">" . $title . "</a></td>\n";
-    $contents .= "<td>" . $publtime . "</td>\n";
-    $contents .= "<td>" . $status . "</td>\n";
-    $contents .= "<td>" . $username . "</td>\n";
-    $contents .= "<td>";
-    $contents .= implode( "&nbsp;-&nbsp;", $admin_funcs );
-    $contents .= "</td>\n";
-	$contents .= "</tr>\n";
-    $contents .= "</tbody>";
+	
+	$data[] = array(
+		"class" => $class,
+		"id" => $id,
+		"link" => $link,
+		"title" => $title,
+		"publtime" => $publtime,
+		"status" => $status,
+		"username" => $username,
+		"feature" => implode( "&nbsp;-&nbsp;", $admin_funcs )
+	);
+		
     $a ++;
 }
 
-$contents .= "<tbody>\n";
-$contents .= "<tr align=\"left\" class=\"tfoot_box\">\n";
-$contents .= "<td colspan=\"7\">\n";
-$contents .= "<select name=\"action\" id=\"action\">\n";
 $array_list_action = array( 
-    'delete' => $lang_global['delete'], 'publtime' => $lang_module['publtime'], 'exptime' => $lang_module['exptime'] 
+    'delete' => $lang_global['delete'], 
+	'publtime' => $lang_module['publtime'], 
+	'exptime' => $lang_module['exptime'] 
 );
 if ( defined( 'NV_IS_ADMIN_MODULE' ) )
 {
     $array_list_action['addtoblock'] = $lang_module['addtoblock'];
     $array_list_action['addtotopics'] = $lang_module['addtotopics'];
 }
+
+$action = array();
 while ( list( $catid_i, $title_i ) = each( $array_list_action ) )
 {
-    $contents .= "<option value=\"" . $catid_i . "\">" . $title_i . "</option>\n";
+	$action[] = array(
+		"value" => $catid_i,
+		"title" => $title_i
+	);
 }
-$contents .= "</select>\n";
-$contents .= "<input type=\"button\" onclick=\"nv_main_action(this.form,'" . md5( $global_config['sitekey'] . session_id() ) . "','" . $lang_module['msgnocheck'] . "')\" value=\"" . $lang_module['action'] . "\" />\n";
-$contents .= "</td>\n";
-$contents .= "</tr>\n";
-$contents .= "</tbody>\n";
-
-$contents .= "</table>\n";
-$contents .= "</form>\n";
 
 $generate_page = nv_generate_page( $base_url, $all_page, $per_page, $page );
-if ( $generate_page != "" ) $contents .= "<br /><p align=\"center\">" . $generate_page . "</p>\n";
+
+$xtpl = new XTemplate( "main.tpl", NV_ROOTDIR . "/themes/" . $global_config['module_theme'] . "/modules/" . $module_file );
+$xtpl->assign( 'LANG', $lang_module );
+$xtpl->assign( 'GLANG', $lang_global );
+$xtpl->assign( 'NV_BASE_ADMINURL', NV_BASE_ADMINURL );
+$xtpl->assign( 'NV_NAME_VARIABLE', NV_NAME_VARIABLE );
+$xtpl->assign( 'MODULE_NAME', $module_name );
+$xtpl->assign( 'OP', $op );
+$xtpl->assign( 'Q', $q );
+$xtpl->assign( 'CHECKSS', md5( session_id() ) );
+$xtpl->assign( 'SITEKEY', md5( $global_config['sitekey'] . session_id() ) );
+
+$xtpl->assign( 'base_url_id', $base_url_id );
+$xtpl->assign( 'base_url_name', $base_url_name );
+$xtpl->assign( 'base_url_publtime', $base_url_publtime );
+$xtpl->assign( 'base_url_exptime', $base_url_exptime );
+
+foreach ( $val_cat_content as $cat_content )
+{
+    $xtpl->assign( 'CAT_CONTENT', $cat_content );
+    $xtpl->parse( 'main.cat_content' );
+}
+
+foreach ( $search_type as $search_t )
+{
+    $xtpl->assign( 'SEARCH_TYPE', $search_t );
+    $xtpl->parse( 'main.search_type' );
+}
+
+foreach ( $search_per_page as $s_per_page )
+{
+    $xtpl->assign( 'SEARCH_PER_PAGE', $s_per_page );
+    $xtpl->parse( 'main.s_per_page' );
+}
+
+foreach ( $data as $row )
+{
+    $xtpl->assign( 'ROW', $row );
+    $xtpl->parse( 'main.loop' );
+}
+
+foreach ( $action as $action1 )
+{
+    $xtpl->assign( 'ACTION', $action1 );
+    $xtpl->parse( 'main.action' );
+}
+
+if ( ! empty( $generate_page ) )
+{
+    $xtpl->assign( 'GENERATE_PAGE', $generate_page );
+    $xtpl->parse( 'main.generate_page' );
+}
+
+$xtpl->parse( 'main' );
+$contents = $xtpl->text( 'main' );
 
 include ( NV_ROOTDIR . "/includes/header.php" );
 echo nv_admin_theme( $contents );
---
+++ nukeviet3/themes/admin_default/modules/news/main.tpl
@@ -0,0 +1,77 @@
+<!-- BEGIN: main -->
+<form action="{NV_BASE_ADMINURL}index.php" method="get">
+	<br />
+	<input type="hidden" name="{NV_NAME_VARIABLE}" value="{MODULE_NAME}" />
+	<input type="hidden" name="{NV_OP_VARIABLE}" value="{OP}" />
+	<label>{LANG.search_cat}: </label>
+	<select name="catid">
+		<!-- BEGIN: cat_content -->
+		<option value="{CAT_CONTENT.value}" {CAT_CONTENT.selected} >{CAT_CONTENT.title}</option>
+		<!-- END: cat_content -->
+	</select>
+	<label>{LANG.search_type}: </label>
+	<select name="stype">
+		<!-- BEGIN: search_type -->
+		<option value="{SEARCH_TYPE.key}" {SEARCH_TYPE.selected} >{SEARCH_TYPE.value}</option>
+		<!-- END: search_type -->
+	</select>
+	<label>{LANG.search_per_page}: </label>
+	<select name="per_page">
+		<!-- BEGIN: s_per_page -->
+		<option value="{SEARCH_PER_PAGE.page}" {SEARCH_PER_PAGE.selected} >{SEARCH_PER_PAGE.page}</option>
+		<!-- END: s_per_page -->
+	</select>
+	<br />
+	<br />
+	{LANG.search_key}: 
+	<input type="text" value="{Q}" maxlength="64" name="q" style="width: 265px" />
+	<input type="submit" value="{LANG.search}" /><br />
+	<input type="hidden" name="checkss" value="{CHECKSS}" />
+	<label><em>{LANG.search_note}</em></label>
+</form>
+
+<form name="block_list" action="">
+	<table summary="" class="tab1">
+		<thead>
+			<tr>
+				<td align="center"><input name="check_all[]" type="checkbox" value="yes" onclick="nv_checkAll(this.form, 'idcheck[]', 'check_all[]',this.checked);" /></td>
+				<td><a href="{base_url_name}">{LANG.name}</a></td>
+				<td align="center"><a href="{base_url_publtime}">{LANG.content_publ_date}</a></td>
+				<td align="center">{LANG.status}</td>
+				<td>{LANG.content_admin}</td>
+				<td></td>
+			</tr>
+		</thead>
+		<!-- BEGIN: loop -->
+		<tbody {ROW.class}>
+			<tr align="center">
+				<td align="center"><input type="checkbox" onclick="nv_UncheckAll(this.form, 'idcheck[]', 'check_all[]', this.checked);" value="{ROW.id}" name="idcheck[]" /></td>
+				<td align="left"><a target="_blank" href="{ROW.link}">{ROW.title}</a></td>
+				<td>{ROW.publtime}</td>
+				<td>{ROW.status}</td>
+				<td>{ROW.username}</td>
+				<td>
+					{ROW.feature}
+				</td>
+			</tr>
+		</tbody>
+		<!-- END: loop -->
+		<tbody>
+			<tr align="left" class="tfoot_box">
+				<td colspan="7">
+					<select name="action" id="action">
+						<!-- BEGIN: action -->
+						<option value="{ACTION.value}">{ACTION.title}</option>
+						<!-- END: action -->
+					</select>
+					<input type="button" onclick="nv_main_action(this.form, '{SITEKEY}', '{LANG.msgnocheck}')" value="{LANG.action}" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</form>
+<!-- BEGIN: generate_page -->
+<br />
+<p align="center">{GENERATE_PAGE}</p>
+<!-- END: generate_page -->
+<!-- END: main -->
