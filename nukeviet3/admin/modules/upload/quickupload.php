8b5b84010636fc68bfc32f5f62511c53a8afaeec
thao@vinades.vn <thao@vinades.vn@a957d178-3835-f50f-e15a-6e012bba7686>
2011-04-18 10:24:53 +0000

    Sửa upload nhanh trong trình soạn thảo

--- nukeviet3/admin/modules/upload/quickupload.php
+++ nukeviet3/admin/modules/upload/quickupload.php
@@ -15,13 +15,13 @@ $check_allow_upload_dir = nv_check_allow_upload_dir( $imgfolder );
 if ( ! isset( $check_allow_upload_dir['upload_file'] ) )
 {
     echo "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $CKEditorFuncNum . ", '', '" . $lang_module['notlevel'] . "');</script>";
-    exit;
+    exit();
 }
 
 if ( ! isset( $_FILES, $_FILES['upload'], $_FILES['upload']['tmp_name'] ) )
 {
     echo "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $CKEditorFuncNum . ", '', '" . $lang_module['errorNotSelectFile'] . "');</script>";
-    exit;
+    exit();
 }
 
 $type = $nv_Request->get_string( 'type', 'post,get' );
@@ -29,15 +29,20 @@ $allow_files_type = array();
 if ( $type == "image" and in_array( 'images', $admin_info['allow_files_type'] ) )
 {
     $allow_files_type = array( 'images' );
-} elseif ( $type == "flash" and in_array( 'flash', $admin_info['allow_files_type'] ) )
+}
+elseif ( $type == "flash" and in_array( 'flash', $admin_info['allow_files_type'] ) )
 {
     $allow_files_type = array( 'flash' );
+}
+elseif ( empty( $type ) )
+{
+    $allow_files_type = $admin_info['allow_files_type'];
 }
 
 if ( empty( $allow_files_type ) )
 {
     echo "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $CKEditorFuncNum . ", '', '" . $lang_module['notlevel'] . "');</script>";
-    exit;
+    exit();
 }
 
 require_once ( NV_ROOTDIR . "/includes/class/upload.class.php" );
@@ -47,13 +52,13 @@ $upload_info = $upload->save_file( $_FILES['upload'], NV_ROOTDIR . '/' . $imgfol
 if ( ! empty( $upload_info['error'] ) )
 {
     echo "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $CKEditorFuncNum . ", '', '" . $upload_info['error'] . "');</script>";
-    exit;
+    exit();
 }
 
 nv_filesList( $imgfolder, false, $upload_info['basename'] );
 nv_insert_logs( NV_LANG_DATA, $module_name, $lang_module['upload_file'], $imgfolder . "/" . $upload_info['basename'], $admin_info['userid'] );
 
 echo "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $CKEditorFuncNum . ", '" . NV_BASE_SITEURL . $imgfolder . "/" . $upload_info['basename'] . "', '');</script>";
-exit;
+exit();
 
 ?>
