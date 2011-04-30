b3ba2da83b31d32929c31bb5f3af41f44bd052bd
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2011-04-30 17:23:24 +0000

    Cập nhật trình soạn thảo revision 6825

--- nukeviet3/admin/editors/ckeditor/ckeditor_php5.php
+++ nukeviet3/admin/editors/ckeditor/ckeditor_php5.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '3.5.3';
+	const version = '3.5.4 (SVN)';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'B37D54V';
+	const timestamp = 'B3U759L';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "B37D54V";
+	public $timestamp = "B3U759L";
 	/**
 	 * An array that holds event listeners.
 	 */
