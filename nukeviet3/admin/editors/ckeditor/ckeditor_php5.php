89fefde62ae31f9e1a031c672309dbfc2d4de2a1
thao@vinades.vn <thao@vinades.vn@a957d178-3835-f50f-e15a-6e012bba7686>
2011-06-18 03:55:27 +0000

    Cập nhật trình soạn thảo r7071

--- nukeviet3/admin/editors/ckeditor/ckeditor_php5.php
+++ nukeviet3/admin/editors/ckeditor/ckeditor_php5.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '3.6.1 (SVN)';
+	const version = '3.6.2 (SVN)';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'B5C75FJ';
+	const timestamp = 'B5H75GF';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "B5C75FJ";
+	public $timestamp = "B5H75GF";
 	/**
 	 * An array that holds event listeners.
 	 */
