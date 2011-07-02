53d97b6b90775816daa1b3e533e06896452f1069
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2011-07-02 02:22:15 +0000

    module News: Lỗi hiện tiêu đề bài viết chưa đăng trong sitemap

--- nukeviet3/modules/news/funcs/Sitemap.php
+++ nukeviet3/modules/news/funcs/Sitemap.php
@@ -13,7 +13,7 @@ if ( ! defined( 'NV_IS_MOD_NEWS' ) )
 }
 
 $url = array();
-$cacheFile = NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . NV_LANG_DATA . "_" . $module_name . "_Sitemap.cache";
+$cacheFile = NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . NV_LANG_DATA . "_" . $module_data . "_Sitemap.cache";
 $pa = NV_CURRENTTIME - 7200;
 
 if ( ( $cache = nv_get_cache( $cacheFile ) ) != false and filemtime( $cacheFile ) >= $pa )
@@ -22,7 +22,7 @@ if ( ( $cache = nv_get_cache( $cacheFile ) ) != false and filemtime( $cacheFile 
 }
 else
 {
-    $sql = "SELECT `id`, `listcatid`, `publtime`, `alias` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_rows` WHERE `publtime` < " . NV_CURRENTTIME . " AND (`exptime`=0 OR `exptime` >" . NV_CURRENTTIME . ") ORDER BY `publtime` DESC LIMIT 1000";
+    $sql = "SELECT `id`, `listcatid`, `publtime`, `alias` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_rows` WHERE `status`=1 AND `publtime` < " . NV_CURRENTTIME . " AND (`exptime`=0 OR `exptime` >" . NV_CURRENTTIME . ") ORDER BY `publtime` DESC LIMIT 1000";
     $result = $db->sql_query( $sql );
     $url = array();
 
