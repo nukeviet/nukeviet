01e12adfa037fe0f0819ebc961b64c790c18c1fc
anhtunguyen71 <anhtunguyen71@a957d178-3835-f50f-e15a-6e012bba7686>
2011-11-24 10:11:21 +0000

    Add option "site timezone by user's PC timezone" (2)

--- NukeViet3.3/language/vi/admin_settings.php
+++ NukeViet3.3/language/vi/admin_settings.php
@@ -205,7 +205,7 @@ $lang_module['robots_type'] = "Chế độ";
 $lang_module['robots_type_0'] = "Cấm truy cập";
 $lang_module['robots_type_1'] = "Không hiển thị trong file robots.txt";
 $lang_module['robots_error_writable'] = "Lỗi: Hệ thống không ghi được file robots.txt";
-$lang_module['searchEngineUniqueID'] = "Google search Engine ID<br />(Có dạng 000329275761967753447:sr7yxqgv294 , <a href=\"http://www.google.com/cse/\" target=\"_blank\">xem chi tiết</a>)";
+$lang_module['searchEngineUniqueID'] = "Google search Engine ID<br />(Có dạng 000329275761967753447:sr7yxqgv294 , <a href=\"http://nukeviet.vn/vi/faq/Su-dung-Google-Custom-Search-tren-NukeViet/\" target=\"_blank\">xem chi tiết</a>)";
 $lang_module['timezoneAuto'] = "Theo PC người dùng";
 
 ?>
