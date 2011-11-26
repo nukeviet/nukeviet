5a5aff9e5d1e19c6b5de5ebb5516adcdd69b2657
thao@vinades.vn <thao@vinades.vn@a957d178-3835-f50f-e15a-6e012bba7686>
2011-11-26 05:48:12 +0000

    Module News: config block news

--- NukeViet3.3/modules/news/blocks/module.block_news.php
+++ NukeViet3.3/modules/news/blocks/module.block_news.php
@@ -7,76 +7,137 @@
  * @Createdate 3/9/2010 23:25
  */
 
-if ( ! defined( 'NV_IS_MOD_NEWS' ) ) die( 'Stop!!!' );
+if (!defined('NV_MAINFILE'))
+    die('Stop!!!');
 
-global $global_config, $module_name, $module_data, $module_file, $global_array_cat, $module_config, $module_info;
-
-$blockwidth = $module_config[$module_name]['blockwidth'];
-$array_block_news = array();
-
-$cache_file = NV_LANG_DATA . "_" . $module_name . "_block_news_" . NV_CACHE_PREFIX . ".cache";
-if ( ( $cache = nv_get_cache( $cache_file ) ) != false )
+if (!nv_function_exists('nv_news_block_news'))
 {
-    $array_block_news = unserialize( $cache );
-}
-else
-{
-    $sql = "SELECT id, catid, publtime, exptime, title, alias, homeimgthumb, homeimgfile FROM `" . NV_PREFIXLANG . "_" . $module_data . "_rows` WHERE `status`= 1 ORDER BY `publtime` DESC LIMIT 0 , 20";
-    $result = $db->sql_query( $sql );
-    while ( list( $id, $catid, $publtime, $exptime, $title, $alias, $homeimgthumb, $homeimgfile ) = $db->sql_fetchrow( $result ) )
+    function nv_block_config_news($module, $data_block, $lang_block)
     {
-        $link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $global_array_cat[$catid]['alias'] . "/" . $alias . "-" . $id;
-        
-        if ( ! empty( $homeimgthumb ) )
+        global $db, $language_array;
+        $html = "";
+        $html .= "<tr>";
+        $html .= "	<td>" . $lang_block['numrow'] . "</td>";
+        $html .= "	<td><input type=\"text\" name=\"config_numrow\" size=\"5\" value=\"" . $data_block['numrow'] . "\"/></td>";
+        $html .= "</tr>";
+        return $html;
+    }
+
+    function nv_block_config_news_submit($module, $lang_block)
+    {
+        global $nv_Request;
+        $return = array();
+        $return['error'] = array();
+        $return['config'] = array();
+        $return['config']['numrow'] = $nv_Request->get_int('config_numrow', 'post', 0);
+        return $return;
+    }
+
+    function nv_news_block_news($block_config, $mod_data)
+    {
+        global $module_array_cat, $module_info, $db, $module_config;
+
+        $module = $block_config['module'];
+        $blockwidth = $module_config[$module]['blockwidth'];
+        $array_block_news = array();
+
+        $cache_file = NV_LANG_DATA . "_" . $module . "_block_news_" . NV_CACHE_PREFIX . ".cache";
+        if (($cache = nv_get_cache($cache_file)) != false)
         {
-            $array_img = explode( "|", $homeimgthumb );
+            $array_block_news = unserialize($cache);
         }
         else
         {
-            $array_img = array( 
-                "", "" 
-            );
+            $numrow = (isset($block_config['numrow'])) ? $block_config['numrow'] : 20;
+            $sql = "SELECT id, catid, publtime, exptime, title, alias, homeimgthumb, homeimgfile FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_rows` WHERE `status`= 1 ORDER BY `publtime` DESC LIMIT 0 , " . $numrow;
+            $result = $db->sql_query($sql);
+            while (list($id, $catid, $publtime, $exptime, $title, $alias, $homeimgthumb, $homeimgfile) = $db->sql_fetchrow($result))
+            {
+                $link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module . "&amp;" . NV_OP_VARIABLE . "=" . $module_array_cat[$catid]['alias'] . "/" . $alias . "-" . $id;
+
+                if (!empty($homeimgthumb))
+                {
+                    $array_img = explode("|", $homeimgthumb);
+                }
+                else
+                {
+                    $array_img = array("", "");
+                }
+                if ($array_img[0] != "" and file_exists(NV_ROOTDIR . '/' . NV_FILES_DIR . '/' . $module . '/' . $array_img[0]))
+                {
+                    $imgurl = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module . '/' . $array_img[0];
+                }
+                elseif (nv_is_url($homeimgfile))
+                {
+                    $imgurl = $homeimgfile;
+                }
+                elseif ($homeimgfile != "" and file_exists(NV_UPLOADS_REAL_DIR . '/' . $module . '/' . $homeimgfile))
+                {
+                    $imgurl = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module . '/' . $homeimgfile;
+                }
+                else
+                {
+                    $imgurl = "";
+                }
+
+                $array_block_news[] = array('id' => $id, 'title' => $title, 'link' => $link, 'imgurl' => $imgurl, 'width' => $blockwidth);
+            }
+            $cache = serialize($array_block_news);
+            nv_set_cache($cache_file, $cache);
         }
-        if ( $array_img[0] != "" and file_exists( NV_ROOTDIR . '/' . NV_FILES_DIR . '/' . $module_name . '/' . $array_img[0] ) )
+
+        if (file_exists(NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/news/block_news.tpl"))
         {
-            $imgurl = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module_name . '/' . $array_img[0];
-        }
-        elseif ( nv_is_url( $homeimgfile ) )
-        {
-            $imgurl = $homeimgfile;
-        }
-        elseif ( $homeimgfile != "" and file_exists( NV_UPLOADS_REAL_DIR . '/' . $module_name . '/' . $homeimgfile ) )
-        {
-            $imgurl = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_name . '/' . $homeimgfile;
+            $block_theme = $module_info['template'];
         }
         else
         {
-            $imgurl = "";
+            $block_theme = "default";
         }
-        
-        $array_block_news[] = array( 
-            'id' => $id, 'title' => $title, 'link' => $link, 'imgurl' => $imgurl, 'width' => $blockwidth 
-        );
+        $xtpl = new XTemplate("block_news.tpl", NV_ROOTDIR . "/themes/" . $block_theme . "/modules/news/");
+        $a = 1;
+        foreach ($array_block_news as $array_news)
+        {
+            $xtpl->assign('blocknews', $array_news);
+            if (!empty($array_news['imgurl']))
+            {
+                $xtpl->parse('main.newloop.imgblock');
+            }
+            $xtpl->parse('main.newloop');
+            $xtpl->assign('BACKGROUND', ($a % 2) ? 'bg ' : '');
+            ++$a;
+        }
+
+        $xtpl->parse('main');
+        return $xtpl->text('main');
     }
-    $cache = serialize( $array_block_news );
-    nv_set_cache( $cache_file, $cache );
+
 }
 
-$xtpl = new XTemplate( "block_news.tpl", NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/" . $module_file );
-$a = 1;
-foreach ( $array_block_news as $array_news )
+if (defined('NV_SYSTEM'))
 {
-    $xtpl->assign( 'blocknews', $array_news );
-    if ( ! empty( $array_news['imgurl'] ) )
+    global $site_mods, $module_name, $module_array_cat, $module_array_cat;
+    $module = $block_config['module'];
+    if (isset($site_mods[$module]))
     {
-        $xtpl->parse( 'main.newloop.imgblock' );
+        $mod_data = $site_mods[$module]['module_data'];
+        if ($module == $module_name)
+        {
+            $module_array_cat = $global_array_cat;
+            unset($module_array_cat[0]);
+        }
+        else
+        {
+            $module_array_cat = array();
+            $sql = "SELECT catid, parentid, title, alias, viewcat, subcatid, numlinks, description, inhome, keywords, who_view, groups_view FROM `" . NV_PREFIXLANG . "_" . $mod_data . "_cat` ORDER BY `order` ASC";
+            $list = nv_db_cache($sql, 'catid', $module);
+            foreach ($list as $l)
+            {
+                $module_array_cat[$l['catid']] = $l;
+                $module_array_cat[$l['catid']]['link'] = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module . "&amp;" . NV_OP_VARIABLE . "=" . $l['alias'];
+            }
+        }
+        $content = nv_news_block_news($block_config, $mod_data);
     }
-    $xtpl->parse( 'main.newloop' );
-    $xtpl->assign( 'BACKGROUND', ( $a % 2 ) ? 'bg ' : '' );
-    ++$a;
 }
-
-$xtpl->parse( 'main' );
-$content = $xtpl->text( 'main' );
-
 ?>
---
+++ NukeViet3.3/modules/news/language/block.module.block_news_en.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* @Project NUKEVIET 3.0
+* @Author VINADES.,JSC (contact@vinades.vn)
+* @Copyright (C) 2010 VINADES.,JSC. All rights reserved
+* @Language English
+* @Createdate Nov 08, 2011, 05:58:06 PM
+*/
+
+ if (!defined( 'NV_MAINFILE' )) {
+ die('Stop!!!');
+}
+
+$lang_translator['author'] ="";
+$lang_translator['createdate'] ="";
+$lang_translator['copyright'] ="";
+$lang_translator['info'] ="";
+$lang_translator['langtype'] ="lang_module";
+
+$lang_block['numrow'] = "Number of posts displayed";
+
+?>
