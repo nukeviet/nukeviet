2175c5544c50874a3dc218774a4e38f9bb90e313
phantandung92 <phantandung92@a957d178-3835-f50f-e15a-6e012bba7686>
2012-01-09 04:47:44 +0000

    Module Download: Fix global search modules/download/search.php

--- nukeviet/modules/download/search.php
+++ nukeviet/modules/download/search.php
@@ -9,36 +9,38 @@
 
 if ( ! defined( 'NV_IS_MOD_SEARCH' ) ) die( 'Stop!!!' );
 
-/**
- * nv_list_cats()
- * 
- * @param mixed $module_data
- * @return
- */
-function nv_list_cats( $module_data )
+if( ! nv_function_exists( 'nv_sdown_cats' ) )
 {
-    global $db;
+	/**
+	 * nv_sdown_cats()
+	 * 
+	 * @param mixed $module_data
+	 * @return
+	 */
+	function nv_sdown_cats( $module_data )
+	{
+		global $db;
 
-    $sql = "SELECT `id`, `title`, `alias`, `who_view`, `groups_view` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_categories` WHERE `status`=1";
-    $result = $db->sql_query( $sql );
+		$sql = "SELECT `id`, `title`, `alias`, `who_view`, `groups_view` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_categories` WHERE `status`=1";
+		$result = $db->sql_query( $sql );
 
-    $list = array();
-    while ( $row = $db->sql_fetchrow( $result ) )
-    {
-        if ( nv_set_allow( $row['who_view'], $row['groups_view'] ) )
-        {
-            $list[$row['id']] = array( //
-                'id' => ( int )$row['id'], //
-                'title' => $row['title'], //
-                'alias' => $row['alias'] //
-                );
-        }
-    }
-
-    return $list;
+		$list = array();
+		while ( $row = $db->sql_fetchrow( $result ) )
+		{
+			if ( nv_set_allow( $row['who_view'], $row['groups_view'] ) )
+			{
+				$list[$row['id']] = array(
+					'id' => ( int )$row['id'], //
+					'title' => $row['title'], //
+					'alias' => $row['alias'] //
+				);
+			}
+		}
+		return $list;
+	}
 }
 
-$list_cats = nv_list_cats( $m_values['module_data'] );
+$list_cats = nv_sdown_cats( $m_values['module_data'] );
 $in = implode( ",", array_keys( $list_cats ) );
 
 $sql = "SELECT SQL_CALC_FOUND_ROWS `alias`,`title`,`description`, `introtext`, `catid` 
@@ -56,18 +58,18 @@ list( $all_page ) = $db->sql_fetchrow( $result );
 
 if ( $all_page )
 {
-    $link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $m_values['module_name'] . '&amp;' . NV_OP_VARIABLE . '=';
+	$link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $m_values['module_name'] . '&amp;' . NV_OP_VARIABLE . '=';
 
-    while ( list( $alias, $tilterow, $content, $introtext, $catid ) = $db->sql_fetchrow( $tmp_re ) )
-    {
-        $content = $content . ' ' . $introtext;
-
-        $result_array[] = array( //
-            'link' => $link . $list_cats[$catid]['alias'] . '/' . $alias, //
-            'title' => BoldKeywordInStr( $tilterow, $key, $logic ), //
-            'content' => BoldKeywordInStr( $content, $key, $logic ) //
-            );
-    }
+	while ( list( $alias, $tilterow, $content, $introtext, $catid ) = $db->sql_fetchrow( $tmp_re ) )
+	{
+		$content = $content . ' ' . $introtext;
+		
+		$result_array[] = array(
+			'link' => $link . $list_cats[$catid]['alias'] . '/' . $alias, //
+			'title' => BoldKeywordInStr( $tilterow, $key, $logic ), //
+			'content' => BoldKeywordInStr( $content, $key, $logic ) //
+		);
+	}
 }
 
 ?>
