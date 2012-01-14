2f65ff73902efd186f718edadd6da0798efb20ba
phantandung92 <phantandung92@a957d178-3835-f50f-e15a-6e012bba7686>
2012-01-14 01:32:21 +0000

    Module Download: Add block global new files

---
+++ nukeviet/modules/download/language/block.global.new_files_vi.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* @Project NUKEVIET 3.0
+* @Author VINADES.,JSC (contact@vinades.vn)
+* @Copyright (C) 2010 VINADES.,JSC. All rights reserved
+* @Language Tiếng Việt
+* @Createdate Jul 06, 2011, 04:38:01 PM
+*/
+
+if (!defined( 'NV_MAINFILE' )) die('Stop!!!');
+
+$lang_translator['author'] ="VINADES.,JSC (contact@vinades.vn)";
+$lang_translator['createdate'] ="22/06/2010, 09:22";
+$lang_translator['copyright'] ="@Copyright (C) 2010 VINADES.,JSC. All rights reserved";
+$lang_translator['info'] ="";
+$lang_translator['langtype'] ="lang_block";
+
+$lang_block['title_length'] = "Độ dài tiêu đề";
+$lang_block['numrow'] = "Số files hiển thị";
+$lang_block['class_name'] = "Tên lớp (class) cho thẻ &lt;ul&gt;";
+$lang_block['img_bullet'] = "Ảnh dạng liệt kê";
+
+?>
---
+++ nukeviet/themes/default/modules/download/block_new_files.tpl
@@ -0,0 +1,6 @@
+<!-- BEGIN: main -->
+<ul class="{CONFIG.class_name}">
+	<!-- BEGIN: loop --><li><!-- BEGIN: bullet --><img style="vertical-align:middle" src="{CONFIG.img_bullet}" alt="{ROW.title}"/> <!-- END: bullet --><a href="{ROW.link}" title="{ROW.title}">{ROW.stitle}</a> - <span style="color:#666;font-size:11px">{ROW.updatetime}</span></li>
+	<!-- END: loop -->
+</ul>
+<!-- END: main -->
