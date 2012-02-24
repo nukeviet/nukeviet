a9fdf70d4a89635568a23b6fcd633293f7e2c7d1
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2012-02-24 06:56:42 +0000

    CKEditor 3.6.3  revision 7365

--- nukeviet/editors/ckeditor/ckeditor_php5.php
+++ nukeviet/editors/ckeditor/ckeditor_php5.php
@@ -24,7 +24,7 @@ class CKEditor
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'BAP85NK';
+	const timestamp = 'C1N85OL';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "BAP85NK";
+	public $timestamp = "C1N85OL";
 	/**
 	 * An array that holds event listeners.
 	 */
