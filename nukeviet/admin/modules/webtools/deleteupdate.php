df9216490d4d6d3b7968bbcb0973b40730fedb3f
phantandung92 <phantandung92@a957d178-3835-f50f-e15a-6e012bba7686>
2012-04-02 07:02:38 +0000

    Update function delete update package.

--- nukeviet/admin/modules/webtools/deleteupdate.php
+++ nukeviet/admin/modules/webtools/deleteupdate.php
@@ -17,23 +17,43 @@ if( $checksess ==  md5( $global_config['sitekey'] . session_id() ) and file_exis
 	$contents = "";
 	$list_file_docs = nv_scandir( NV_ROOTDIR . '/install', "/^update_docs_([a-z]{2})\.html$/" );
 
+	// Xoa cac file docs
 	foreach( $list_file_docs as $docsfile )
 	{
-		nv_deletefile( NV_ROOTDIR . '/install/' . $docsfile );
+		$check_del = nv_deletefile( NV_ROOTDIR . '/install/' . $docsfile );
+		
+		if( $check_del[0] == 0 )
+		{
+			$contents .= $check_del[1] . ' ' . $lang_module['update_manual_delete'];
+		}
 	}
 	
+	// Xoa file du lieu nang cap
 	$check_delete_file = nv_deletefile( NV_ROOTDIR . '/install/update_data.php' );
 	if( $check_delete_file[0] == 0 )
 	{
 		$contents .= $check_delete_file[1] . ' ' . $lang_module['update_manual_delete'];
 	}
 	
+	// Xoa thu muc file thay doi
 	if( file_exists( NV_ROOTDIR . '/install/update' ) )
 	{
 		$check_delete_dir = nv_deletefile( NV_ROOTDIR . '/install/update', true );
 		if( $check_delete_dir[0] == 0 )
 		{
 			$contents .= $check_delete_dir[1] . ' ' . $lang_module['update_manual_delete'];
+		}
+	}
+	
+	// Xoa file log
+	$list_file_logs = nv_scandir( NV_ROOTDIR . '/' . NV_DATADIR, "/^config_update_NVUD([0-9]+)\.php$/" );
+	foreach( $list_file_logs as $logsfile )
+	{
+		$check_del = nv_deletefile( NV_ROOTDIR . '/' . NV_DATADIR . '/' . $logsfile );
+		
+		if( $check_del[0] == 0 )
+		{
+			$contents .= $check_del[1] . ' ' . $lang_module['update_manual_delete'];
 		}
 	}
 	
