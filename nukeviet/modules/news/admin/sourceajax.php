3d725e8ca47294c4d75fe67afa36bfa9c8b22059
thangbv@vinades.vn <thangbv@vinades.vn@a957d178-3835-f50f-e15a-6e012bba7686>
2012-04-08 09:33:36 +0000

    Update article source.

--- nukeviet/modules/news/admin/sourceajax.php
+++ nukeviet/modules/news/admin/sourceajax.php
@@ -6,18 +6,19 @@
  * @Copyright (C) 2010 VINADES.,JSC. All rights reserved
  * @Createdate 2-9-2010 14:43
  */
-
-if( ! defined( 'NV_IS_FILE_ADMIN' ) ) die( 'Stop!!!' );
+ 
+if ( ! defined( 'NV_IS_FILE_ADMIN' ) ) die( 'Stop!!!' );
 
 $q = filter_text_input( 'q', 'get', "", 1 );
-if( empty( $q ) ) return;
+if ( empty( $q ) ) return;
 
-$sql = "SELECT link FROM `" . NV_PREFIXLANG . "_" . $module_data . "_sources` WHERE  `title` LIKE '%" . $db->dblikeescape( $q ) . "%' OR `link` LIKE '%" . $db->dblikeescape( $q ) . "%' ORDER BY `weight` ASC";
+$sql = "SELECT title, link FROM `" . NV_PREFIXLANG . "_" . $module_data . "_sources` WHERE  `title` LIKE '%" . $db->dblikeescape( $q ) . "%' OR `link` LIKE '%" . $db->dblikeescape( $q ) . "%' ORDER BY `weight` ASC";
 $result = $db->sql_query( $sql );
 
-while( list( $link ) = $db->sql_fetchrow( $result ) )
+while ( list( $title, $link ) = $db->sql_fetchrow( $result ) )
 {
-	echo "" . $link . "\n";
+    echo "" . $title . "\n";
+    echo "" . $link . "\n";
 }
 
 ?>
