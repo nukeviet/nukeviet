bb7fadae9a8d6df4aeea2ac1c34b4f9b1021ba06
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2012-04-23 14:58:54 +0000

    Module Users: fix group when delete user

--- nukeviet/modules/users/admin/del.php
+++ nukeviet/modules/users/admin/del.php
@@ -26,7 +26,7 @@ if ( $numrows )
     die( "NO" );
 }
 
-$sql = "SELECT `username`, `full_name`, `email`, `photo` FROM `" . NV_USERS_GLOBALTABLE . "` WHERE `userid`=" . $userid;
+$sql = "SELECT `username`, `full_name`, `email`, `photo`, `in_groups` FROM `" . NV_USERS_GLOBALTABLE . "` WHERE `userid`=" . $userid;
 $query = $db->sql_query( $sql );
 $numrows = $db->sql_numrows( $query );
 if ( $numrows != 1 )
@@ -34,7 +34,7 @@ if ( $numrows != 1 )
     die( "NO" );
 }
 
-list( $username, $full_name, $email, $photo ) = $db->sql_fetchrow( $query );
+list( $username, $full_name, $email, $photo, $in_groups ) = $db->sql_fetchrow( $query );
 
 $userdelete = ( ! empty( $full_name ) ) ? $full_name . " (" . $username . ")" : $username;
 
@@ -48,6 +48,19 @@ if ( ! $result )
 
 $sql = "DELETE FROM `" . NV_USERS_GLOBALTABLE . "_openid` WHERE `userid`=" . $userid;
 $result = $db->sql_query( $sql );
+
+if ( !empty($in_groups) )
+{
+    $result = $db->sql_query("SELECT `group_id`, `users` FROM `" . NV_GROUPS_GLOBALTABLE . "` WHERE `group_id` IN (" . $in_groups . ")");
+    while ( list( $group_id, $users ) = $db->sql_fetchrow($result) )
+    {
+        $users = "," . $users . ",";
+        $users = str_replace("," . $userid . ",", ",", $users);
+        $users = trim($users, ",");
+        $db->sql_query("UPDATE `" . NV_GROUPS_GLOBALTABLE . "` SET `users` = '" . $users . "' WHERE `group_id`=" . $group_id);
+    }
+}
+
 nv_insert_logs( NV_LANG_DATA, $module_name, 'log_del_user', "userid ".$userid, $admin_info['userid'] );
 
 if ( ! empty( $photo ) and is_file( NV_ROOTDIR . '/' . $photo ) )
