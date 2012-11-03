85210b0e1dbca9cad16b7d5eed7c9c983b4a1962
thangbv@vinades.vn <thangbv@vinades.vn@a957d178-3835-f50f-e15a-6e012bba7686>
2012-11-03 02:58:58 +0000

    Fix jwplayer play in Google chrome

--- nukeviet/editors/ckeditor/config.js
+++ nukeviet/editors/ckeditor/config.js
@@ -1,11 +1,28 @@
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 CKEDITOR.editorConfig = function( config )
 {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+	//config.language = 'vi';
 	// config.uiColor = '#AADC6E';
+    config.enterMode = CKEDITOR.ENTER_BR;
+	config.extraPlugins='jwplayer';
+	config.toolbar=	[['Source','-','Save','NewPage','Preview','-','Templates'],
+    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+   
+    ['BidiLtr', 'BidiRtl'], ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+    '/',
+    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+    ['Link','Unlink','Anchor'],
+    ['Image','jwplayer','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+    '/',
+    ['Styles','Format','Font','FontSize'],
+    ['TextColor','BGColor'],
+    ['Maximize', 'ShowBlocks','-','About']];
+
 };
