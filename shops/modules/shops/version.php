ade32ea1c8923fcfa2395702527752ae133fe57c
phantandung92 <phantandung92@c1f94b17-b2a6-59d8-c773-3aa5bca23cdc>
2012-11-18 14:26:25 +0000

    Module shops 3.4 RC1.
    git-svn-id: https://nukeviet-shop.googlecode.com/svn/trunk@84 c1f94b17-b2a6-59d8-c773-3aa5bca23cdc

--- shops/modules/shops/version.php
+++ shops/modules/shops/version.php
@@ -14,8 +14,8 @@ $module_version = array(
 	"modfuncs" => "main,viewcat,detail,search,cart,order,payment,complete,history,group,search_result,myproduct,profile,post", // Cac function co block
 	"is_sysmod" => 0, // 1:0 => Co phai la module he thong hay khong
 	"virtual" => 1, // 1:0 => Co cho phep ao hao module hay khong
-	"version" => "3.4.01", // Phien ban cua module
-	"date" => "Sun, 11 Nov 2012 00:00:00 GMT", // Ngay phat hanh phien ban
+	"version" => "3.4.02", // Phien ban cua module
+	"date" => "Sun, 18 Nov 2012 00:00:00 GMT", // Ngay phat hanh phien ban
 	"author" => "VINADES (contact@vinades.vn)", // Tac gia
 	"note" => "", // Ghi chu
 	"uploads_dir" => array(
