6f461f12f82fb968b7aa86406765d2554952aa96
VuThao <vuthao27@gmail.com>
2013-01-06 23:58:43 +0700

    Module banner: remover data not user

--- modules/banners/admin/del_client.php
+++ modules/banners/admin/del_client.php
@@ -22,19 +22,14 @@ $numrows = $db->sql_numrows( $result );
 if( $numrows != 1 ) die( 'Stop!!!' );
 
 $banners_id = array();
-$sql = "SELECT `id`, `file_name`,`file_name_tmp` FROM `" . NV_BANNERS_ROWS_GLOBALTABLE . "` WHERE `clid`=" . $id;
+$sql = "SELECT `id`, `file_name` FROM `" . NV_BANNERS_ROWS_GLOBALTABLE . "` WHERE `clid`=" . $id;
 $result = $db->sql_query( $sql );
 
 while( $row = $db->sql_fetchrow( $result ) )
 {
-	if( ! empty( $row['file_name'] ) and is_file( NV_ROOTDIR . '/' . $row['file_name'] ) )
+	if( ! empty( $row['file_name'] ) and is_file( NV_UPLOADS_REAL_DIR . '/' . NV_BANNER_DIR . '/' . $row['file_name'] ) )
 	{
-		@nv_deletefile( NV_ROOTDIR . '/' . $row['file_name'] );
-	}
-
-	if( ! empty( $row['file_name_tmp'] ) and is_file( NV_ROOTDIR . '/' . $row['file_name_tmp'] ) )
-	{
-		@nv_deletefile( NV_ROOTDIR . '/' . $row['file_name_tmp'] );
+		@nv_deletefile( NV_UPLOADS_REAL_DIR . '/' . NV_BANNER_DIR . '/' . $row['file_name'] );
 	}
 	$banners_id[] = $row['id'];
 }
