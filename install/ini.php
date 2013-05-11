4cd763ab173f28533112b6ee56594679e56a7800
Vu Thao <vuthao27@gmail.com>
2013-05-11 21:38:48 +0700

    check rewrite when server install ISAPI_Rewrite

--- install/ini.php
+++ install/ini.php
@@ -77,11 +77,15 @@ if( function_exists( 'apache_get_modules' ) )
 		$sys_info['supports_rewrite'] = "rewrite_mode_apache";
 	}
 }
-elseif( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS/7.' ) !== false )
+elseif( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) !== false )
 {
 	if( isset( $_SERVER['IIS_UrlRewriteModule'] ) && ( php_sapi_name() == 'cgi-fcgi' ) && class_exists( 'DOMDocument' ) )
 	{
 		$sys_info['supports_rewrite'] = "rewrite_mode_iis";
+	}
+	elseif( isset( $_SERVER['HTTP_X_REWRITE_URL'] )  )
+	{
+			$sys_info['supports_rewrite'] = "rewrite_mode_apache";
 	}
 }
 
