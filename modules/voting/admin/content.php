b5595382bc4d8108755e6b363529aef0a9b698e6
VuThao <vuthao27@gmail.com>
2014-02-08 13:39:51 +0700

    module voting: fix javascript

--- modules/voting/admin/content.php
+++ modules/voting/admin/content.php
@@ -192,29 +192,16 @@ else
 	else
 	{
 		$rowvote = array(
-			"who_view" => 0,
-			"groups_view" => "",
-			"publ_time" => NV_CURRENTTIME,
-			"exp_time" => "",
-			"acceptcm" => 1,
-			"question" => "",
-			"link" => ""
+			'who_view' => 0,
+			'groups_view' => '',
+			'publ_time' => NV_CURRENTTIME,
+			'exp_time' => '',
+			'acceptcm' => 1,
+			'question' => '',
+			'link' => ''
 		);
 	}
 }
-
-$my_head = "<link type=\"text/css\" href=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.core.css\" rel=\"stylesheet\" />\n";
-$my_head .= "<link type=\"text/css\" href=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.theme.css\" rel=\"stylesheet\" />\n";
-$my_head .= "<link type=\"text/css\" href=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.datepicker.css\" rel=\"stylesheet\" />\n";
-
-$my_footer = "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.core.min.js\"></script>\n";
-$my_footer .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.datepicker.min.js\"></script>\n";
-$my_footer .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/language/jquery.ui.datepicker-" . NV_LANG_INTERFACE . ".js\"></script>\n";
-
-$my_footer .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/jquery/jquery.validate.min.js\"></script>\n";
-$my_footer .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/language/jquery.validator-" . NV_LANG_INTERFACE . ".js\"></script>\n";
-
-$my_footer .= "<script type=\"text/javascript\">\$(document).ready(function(){\$(\"#votingcontent\").validate();});</script>";
 
 $xtpl = new XTemplate( 'content.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file );
 $xtpl->assign( 'LANG', $lang_module );
