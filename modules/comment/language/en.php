5786b43c1744832150008391336dde4d446655c8
hongoctrien <hongoctrien@2mit.org>
2014-03-11 11:17:40 +0700

    module comment: Translate english laguage

--- modules/comment/language/en.php
+++ modules/comment/language/en.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * @Project NUKEVIET 4.x
- * @Author VINADES.,JSC (contact@vinades.vn)
- * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
- * @Language English
- * @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
- * @Createdate Mar 04, 2010, 08:22:00 AM
- */
+* @Project NUKEVIET 4.x
+* @Author VINADES.,JSC (contact@vinades.vn)
+* @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+* @Language English
+* @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
+* @Createdate Mar 04, 2010, 08:22:00 AM
+*/
 
 if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
@@ -30,5 +30,13 @@ $lang_module['comment_content'] = 'Content';
 $lang_module['comment_seccode'] = 'Security code';
 $lang_module['comment_submit'] = 'Submit';
 $lang_module['comment_timeout'] = 'You\'ve voted, please wait for %s minutes later to post a comment';
+$lang_module['sortcomm_0'] = 'Sort by new comments';
+$lang_module['sortcomm_1'] = 'Sort by old comments';
+$lang_module['sortcomm_2'] = 'Sort by likes';
+$lang_module['feedback'] = 'Answer';
+$lang_module['like'] = 'Like';
+$lang_module['dislike'] = 'Dislike';
+$lang_module['delete'] = 'Delete';
+$lang_module['like_unsuccess'] = 'You have already rated this comment earlier';
 
 ?>
