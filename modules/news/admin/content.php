ad032497604b2b401871bf24514cc8c1f915bbe3
hongoctrien <hongoctrien@2mit.org>
2014-03-14 20:12:22 +0700

    Fix "error save" when change cat news

--- modules/news/admin/content.php
+++ modules/news/admin/content.php
@@ -657,16 +657,17 @@ if( $nv_Request->get_int( 'save', 'post' ) == 1 )
 				}
 				foreach( $array_cat_new as $catid )
 				{
-					$ct_query[] = $db->exec( 'DELETE FROM ' . NV_PREFIXLANG . '_' . $module_data . '_' . $catid . ' WHERE id = ' . $rowcontent['id'] );
+					$db->exec( 'DELETE FROM ' . NV_PREFIXLANG . '_' . $module_data . '_' . $catid . ' WHERE id = ' . $rowcontent['id'] );
 					$ct_query[] = $db->exec( 'INSERT INTO ' . NV_PREFIXLANG . '_' . $module_data . '_' . $catid . ' SELECT * FROM ' . NV_PREFIXLANG . '_' . $module_data . '_rows WHERE id=' . $rowcontent['id'] );
 				}
 
 				$sth = $db->prepare( 'UPDATE ' . NV_PREFIXLANG . '_' . $module_data . '_bodytext SET bodytext=:bodytext WHERE id =' . $rowcontent['id'] );
 				$sth->bindParam( ':bodytext', $rowcontent['bodytext'], PDO::PARAM_STR, strlen( $rowcontent['bodytext'] ) );
 				$ct_query[] = (int) $sth->execute();
-
+				
 				if( array_sum( $ct_query ) != sizeof( $ct_query ) )
 				{
+					echo 1; exit;
 					$error[] = $lang_module['errorsave'];
 				}
 			}
