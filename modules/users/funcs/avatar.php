7229c7aca451e6d075acf9cceae893721181c41c
hongoctrien <hongoctrien@2mit.org>
2014-03-15 10:14:58 +0700

    Change area select avatar on input images

--- modules/users/funcs/avatar.php
+++ modules/users/funcs/avatar.php
@@ -15,13 +15,20 @@ $array['success'] = false;
 
 if( isset( $_FILES['image_file'] ) and is_uploaded_file( $_FILES['image_file']['tmp_name'] ) )
 {
-	$avatar_width = $nv_Request->get_int( 'avatar_width', 'post', 120);
-	$avatar_height = $nv_Request->get_int( 'avatar_height', 'post', 120);
+	$max_width = $nv_Request->get_int( 'avatar_width', 'post', $global_config['avatar_width'] );
+	$max_height = $nv_Request->get_int( 'avatar_height', 'post', $global_config['avatar_height'] );
+	$avatar_width = $nv_Request->get_int( 'w', 'post', $global_config['avatar_width'] );
+	$avatar_height = $nv_Request->get_int( 'h', 'post', $global_config['avatar_height'] );
 	$x1 = $nv_Request->get_int( 'x1', 'post', 0);
 	$y1 = $nv_Request->get_int( 'y1', 'post', 0);
+	$old_images = $nv_Request->get_title( 'old_images', 'post', '');
+
+	if( ! empty( $old_images ) and is_file( $old_images ) )
+	{
+		@nv_deletefile( $old_images );
+	}
 	
 	require_once NV_ROOTDIR . '/includes/class/upload.class.php' ;
-
 	$upload = new upload( array( 'images' ), $global_config['forbid_extensions'], $global_config['forbid_mimes'] );
 	$upload_info = $upload->save_file( $_FILES['image_file'], NV_ROOTDIR . '/' . SYSTEM_UPLOADS_DIR . '/' . $module_name, false );
 
@@ -41,6 +48,11 @@ if( isset( $_FILES['image_file'] ) and is_uploaded_file( $_FILES['image_file']['
 		$file_name = NV_ROOTDIR . '/' . SYSTEM_UPLOADS_DIR . '/' . $module_name . '/' . $basename;
 		if( file_exists( NV_ROOTDIR . '/' . SYSTEM_UPLOADS_DIR . '/' . $module_name . '/' . $basename ) )
 		{
+			//resize
+			$_image = new image( $file_name, $max_width, $max_height );
+			$_image->resizeXY( $max_width, $max_height );
+			$_image->save( NV_ROOTDIR . '/' . SYSTEM_UPLOADS_DIR . '/' . $module_name, $basename );
+			
 			//@chmod($file_name, 0644);
 			$file_name = str_replace( NV_ROOTDIR . '/', '', $file_name );
 			@nv_deletefile( $upload_info['name'] );
