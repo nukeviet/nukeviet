87a75d91e8d2e2c709409c8746df10ed61bb71e7
VuThao <vuthao27@gmail.com>
2014-03-20 16:48:32 +0700

    ckeditor: deleted language is not used, add Plugin IFrame Dialog

--- editors/ckeditor/build-config.js
+++ editors/ckeditor/build-config.js
@@ -3,27 +3,6 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-/**
- * This file was added automatically by CKEditor builder.
- * You may re-use it at any time to build CKEditor again.
- *
- * If you would like to build CKEditor online again
- * (for example to upgrade), visit one the following links:
- *
- * (1) http://ckeditor.com/builder
- *     Visit online builder to build CKEditor from scratch.
- *
- * (2) http://ckeditor.com/builder/1053eaceffb4039df6cb97e053246c8e
- *     Visit online builder to build CKEditor, starting with the same setup as before.
- *
- * (3) http://ckeditor.com/builder/download/1053eaceffb4039df6cb97e053246c8e
- *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
- *
- * NOTE:
- *    This file is not used by CKEditor, you may remove it.
- *    Changing this file will not change your CKEditor configuration.
- */
-
 var CKBUILDER_CONFIG = {
 	skin: 'moono',
 	preset: 'full',
@@ -64,6 +43,7 @@ var CKBUILDER_CONFIG = {
 		'googledocs' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
+		'iframe' : 1,
 		'image2' : 1,
 		'indent' : 1,
 		'indentblock' : 1,
@@ -103,15 +83,7 @@ var CKBUILDER_CONFIG = {
 		'youtube' : 1
 	},
 	languages : {
-		'ar' : 1,
-		'cs' : 1,
-		'de' : 1,
 		'en' : 1,
-		'fr' : 1,
-		'ja' : 1,
-		'ru' : 1,
-		'tr' : 1,
 		'vi' : 1,
-		'zh-cn' : 1
 	}
 };
