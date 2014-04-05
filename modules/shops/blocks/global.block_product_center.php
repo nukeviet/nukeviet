9a1a11e2fb08dcd9e240f538dd864b1732cc147b
hongoctrien <hongoctrien@2mit.org>
2014-04-05 09:40:12 +0700

    Update global, module block product center for responsive theme

--- modules/shops/blocks/global.block_product_center.php
+++ modules/shops/blocks/global.block_product_center.php
@@ -41,10 +41,10 @@ if( ! nv_function_exists( 'nv_global_product_center' ) )
 
 		$html .= "	</select></td>\n";
 		$html .= "</tr>";
-
+		
 		$html .= "<tr>";
-		$html .= "	<td>" . $lang_block['numslide'] . "</td>";
-		$html .= "	<td><input type=\"text\" name=\"config_numslide\" size=\"5\" value=\"" . $data_block['numslide'] . "\"/></td>";
+		$html .= "	<td>" . $lang_block['numget'] . "</td>";
+		$html .= "	<td><input type=\"text\" name=\"config_numget\" size=\"5\" value=\"" . $data_block['numget'] . "\"/></td>";
 		$html .= "</tr>";
 
 		$html .= "<tr>";
@@ -70,7 +70,7 @@ if( ! nv_function_exists( 'nv_global_product_center' ) )
 		$return['config'] = array();
 		$return['config']['blockid'] = $nv_Request->get_int( 'config_blockid', 'post', 0 );
 		$return['config']['numrow'] = $nv_Request->get_int( 'config_numrow', 'post', 0 );
-		$return['config']['numslide'] = $nv_Request->get_int( 'config_numslide', 'post', 0 );
+		$return['config']['numget'] = $nv_Request->get_int( 'config_numget', 'post', 0 );
 		return $return;
 	}
 
@@ -82,19 +82,15 @@ if( ! nv_function_exists( 'nv_global_product_center' ) )
 	 */
 	function nv_global_product_center( $block_config )
 	{
-		global $site_mods, $global_config, $module_name, $global_array_cat, $db_config, $my_head;
+		global $site_mods, $global_config, $module_name, $global_array_cat, $db_config, $my_head, $db, $pro_config;
 
 		$module = $block_config['module'];
 		$mod_data = $site_mods[$module]['module_data'];
 		$mod_file = $site_mods[$module]['module_file'];
 		$array_cat_shops = $global_array_cat;
-		$num_slide = $block_config['numslide'];
 		$num_view = $block_config['numrow'];
-		$num = $num_slide * $num_view;
+		$num_get = $block_config['numget'];
 
-		$i = 1;
-		$j = 1;
-		$page_i = "";
 		if( file_exists( NV_ROOTDIR . "/themes/" . $global_config['site_theme'] . "/modules/" . $mod_file . "/block.product_center.tpl" ) )
 		{
 			$block_theme = $global_config['site_theme'];
@@ -145,6 +141,8 @@ if( ! nv_function_exists( 'nv_global_product_center' ) )
 
 		$xtpl = new XTemplate( "block.product_center.tpl", NV_ROOTDIR . "/themes/" . $block_theme . "/modules/" . $module );
 		$xtpl->assign( 'THEME_TEM', NV_BASE_SITEURL . "themes/" . $block_theme );
+		$xtpl->assign( 'NUMVIEW', $num_view );
+		$xtpl->assign( 'WIDTH', $pro_config['homewidth'] );
 
 		$db->sqlreset()
 			->select( "t1.id, t1.listcatid, t1." . NV_LANG_DATA . "_title AS title, t1." . NV_LANG_DATA . "_alias AS alias, t1.homeimgfile, t1.homeimgthumb , t1.homeimgalt" )
@@ -152,7 +150,7 @@ if( ! nv_function_exists( 'nv_global_product_center' ) )
 			->join( "INNER JOIN " . $db_config['prefix'] . "_" . $mod_data . "_block t2 ON t1.id = t2.id" )
 			->where( "t2.bid= " . $block_config['blockid'] . " AND t1.status =1" )
 			->order( 't1.id DESC' )
-			->limit( $num );
+			->limit( $num_get );
 
 		$list = nv_db_cache( $db->sql(), '', $module );
 
@@ -174,31 +172,15 @@ if( ! nv_function_exists( 'nv_global_product_center' ) )
 			}
 			else //no image
 			{
-				$src_img = NV_BASE_SITEURL . 'themes/' . $global_config['site_theme'] . '/images/no_image.gif';
+				$src_img = NV_BASE_SITEURL . 'themes/' . $global_config['site_theme'] . '/images/shops/no-image.jpg';
 			}
 
 			$xtpl->assign( 'LINK', $link );
 			$xtpl->assign( 'TITLE', $row['title'] );
 			$xtpl->assign( 'TITLE0', nv_clean60( $row['title'], 30 ) );
 			$xtpl->assign( 'SRC_IMG', $src_img );
-			$xtpl->parse( 'main.loop.items' );
-
-			if( $i % $num_view == 0 )
-			{
-				$page_i .= "<li><a href=\"#\">" . $j . "</a></li>";
-				++$j;
-				$xtpl->parse( 'main.loop' );
-			}
-			++$i;
+			$xtpl->parse( 'main.items' );
 		}
-
-		if( $i > $num_view and ( $i - 1 ) % $num_view != 0 )
-		{
-			$page_i .= "<li><a href=\"#\">" . $j . "</a></li>";
-			$xtpl->parse( 'main.loop' );
-		}
-
-		$xtpl->assign( 'page', $page_i );
 
 		$xtpl->parse( 'main' );
 		return $xtpl->text( 'main' );
