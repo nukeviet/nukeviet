942cf7c83e98a41ab5cbe997f812eaf4331a8426
VuThao <vuthao27@gmail.com>
2014-04-28 23:04:45 +0700

    change config theme default

--- themes/default/config_default.php
+++ themes/default/config_default.php
@@ -5,66 +5,123 @@
  * @Author VINADES.,JSC (contact@vinades.vn)
  * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
  * @License GNU/GPL version 2 or any later version
- * @Createdate 31/05/2010, 00:36
+ * @Createdate Thu, 17 Apr 2014 04:03:46 GMT
  */
 
-if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+if( !defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
-function SetProperties( $tag, $property_array )
-{
-    $css = $line = '';
-    if( empty( $tag ) ) return '';
-    
-    if( is_array( $property_array ) )
-    {
-        foreach( $property_array as $property => $value )
-        {
-            if( $property != 'customcss' )
-            {
-                if( ! empty( $property ) and ! empty( $value ) )
-                {
-                    $property = str_replace( '_', '-', $property );
-                    if( $property == 'background-image' ) $value = "url('" . $value . "')";
-                    $css .= $property . ':' . $value . ';';
-                }
-            }
-            elseif( ! empty( $value ) )
-            {
-                $value = substr(trim($value), -1) == ';' ? $value : $value . ';';
-                $css .= $value;
-            }
-        }
-        $line .= $css == '' ? '' : $tag . '{' . $css . '}';
-    }
-    else
-    {
-        $css .= $property_array;
-        $line .= $css == '' ? '' : $css;
-    }
-
-    return $line;
-}
-
-function CustomStyle()
-{
-    global $module_config, $global_config;
-    $property = '';
-    
-    if( isset( $module_config['themes'][$global_config['site_theme']] ) ) 
-    {
-         $config_theme = unserialize( $module_config['themes'][$global_config['site_theme']] );
-    }
-
-    $property .= SetProperties( 'body', $config_theme['body'] );
-    $property .= SetProperties( 'a, a:link, a:active, a:visited', $config_theme['a_link'] );
-    $property .= SetProperties( 'a:hover', $config_theme['a_link_hover'] );
-    $property .= SetProperties( '#wraper', $config_theme['content'] );
-    $property .= SetProperties( '#header', $config_theme['header'] );
-    $property .= SetProperties( '#footer', $config_theme['footer'] );
-	$property .= SetProperties( '.panel, .well, .nv-block-banners', $config_theme['block'] );
-	$property .= SetProperties( '.panel-default>.panel-heading', $config_theme['block_heading'] );
-	// Không nên thay đổi "generalcss"
-    $property .= SetProperties( 'generalcss', $config_theme['generalcss'] );
-    
-    return $property;
-}
+$config_theme = array(
+	'body' => array(
+		'color' => '',
+		'font_size' => '',
+		'font_family' => '',
+		'font_weight' => '',
+		'font_style' => '',
+		'background_color' => '',
+		'background_image' => '',
+		'background_repeat' => '',
+		'background_position' => '',
+		'margin' => '',
+		'margin_top' => '',
+		'margin_bottom' => '',
+		'margin_left' => '',
+		'margin_right' => '',
+		'padding' => '',
+		'padding_top' => '',
+		'padding_bottom' => '',
+		'padding_left' => '',
+		'padding_right' => '',
+		'customcss' => '',
+	),
+	'a_link' => array(
+		'color' => '',
+		'font_weight' => '',
+		'font_style' => '',
+	),
+	'a_link_hover' => array(
+		'color' => '',
+		'font_weight' => '',
+		'font_style' => '',
+	),
+	'content' => array(
+		'margin' => '',
+		'margin_top' => '',
+		'margin_bottom' => '',
+		'margin_left' => '',
+		'margin_right' => '',
+		'padding' => '',
+		'padding_top' => '',
+		'padding_bottom' => '',
+		'padding_left' => '',
+		'padding_right' => '',
+		'width' => '',
+		'height' => '',
+		'customcss' => '',
+	),
+	'header' => array(
+		'background_color' => '',
+		'background_image' => '',
+		'background_repeat' => '',
+		'background_position' => '',
+		'margin' => '',
+		'margin_top' => '',
+		'margin_bottom' => '',
+		'margin_left' => '',
+		'margin_right' => '',
+		'padding' => '',
+		'padding_top' => '',
+		'padding_bottom' => '',
+		'padding_left' => '',
+		'padding_right' => '',
+		'width' => '',
+		'height' => '',
+		'customcss' => '',
+	),
+	'footer' => array(
+		'background_color' => '',
+		'background_image' => '',
+		'background_repeat' => '',
+		'background_position' => '',
+		'margin' => '',
+		'margin_top' => '',
+		'margin_bottom' => '',
+		'margin_left' => '',
+		'margin_right' => '',
+		'padding' => '',
+		'padding_top' => '',
+		'padding_bottom' => '',
+		'padding_left' => '',
+		'padding_right' => '',
+		'width' => '',
+		'height' => '',
+		'customcss' => '',
+	),
+	'block' => array(
+		'background_color' => '',
+		'background_image' => '',
+		'background_repeat' => '',
+		'background_position' => '',
+		'margin' => '',
+		'margin_top' => '',
+		'margin_bottom' => '',
+		'margin_left' => '',
+		'margin_right' => '',
+		'padding' => '',
+		'padding_top' => '',
+		'padding_bottom' => '',
+		'padding_left' => '',
+		'padding_right' => '',
+		'border_color' => '',
+		'border_style' => '',
+		'border_width' => '',
+		'border_radius' => '',
+		'customcss' => '',
+	),
+	'block_heading' => array(
+		'background_color' => '',
+		'background_image' => '',
+		'background_repeat' => '',
+		'background_position' => '',
+	),
+	'generalcss' => '',
+);
