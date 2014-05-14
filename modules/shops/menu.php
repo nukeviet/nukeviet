38fd50cc33f0ed7ac50c3c6ce05db2d02aa45a9d
VuThao <vuthao27@gmail.com>
2014-05-14 08:12:52 +0700

    module shops: update menu for admin

--- modules/shops/menu.php
+++ modules/shops/menu.php
@@ -10,25 +10,15 @@
 
 if( ! defined( 'NV_IS_FILE_ADMIN' ) ) die( 'Stop!!!' );
 
-$sql = 'SELECT * FROM ' . $db_config['prefix'] . '_' . $module_d . '_catalogs ORDER BY sort ASC';
+$sql = 'SELECT * FROM ' . $db_config['prefix'] . '_' . $mod_data . '_catalogs ORDER BY sort ASC';
 $result = $db->query( $sql );
-
 while( $row = $result->fetch() )
 {
-	$t_sp = '';
-
-	if( $row['lev'] > 0 )
-	{
-		for( $i = 1; $i <= $row['lev']; ++$i )
-		{
-			$t_sp .= '&nbsp;&nbsp;&nbsp;&nbsp;';
-		}
-	}
-
-	$arr_cat[$row['catid']] = array(
-		'module' => $module,
+	$array_item[$row['catid']] = array(
+		'parentid' => $row['parentid'],
+		'groups_view' => $row['groups_view'],
 		'key' => $row['catid'],
-		'title' => $t_sp . $row[NV_LANG_DATA . '_title'],
+		'title' => $row[NV_LANG_DATA . '_title'],
 		'alias' => $row[NV_LANG_DATA . '_alias'],
 	);
 }
