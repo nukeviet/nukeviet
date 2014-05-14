755af5fc598e03cbfbfb060a28752c7a477931d0
VuThao <vuthao27@gmail.com>
2014-05-14 08:40:15 +0700

    module shops: update data setup

--- modules/shops/action_mysql.php
+++ modules/shops/action_mysql.php
@@ -182,7 +182,7 @@ $sql_create_module[] = "CREATE TABLE IF NOT EXISTS " . $db_config['prefix'] . "_
  imgposition tinyint(1) NOT NULL DEFAULT '1',
  copyright tinyint(1) unsigned NOT NULL DEFAULT '0',
  inhome tinyint(1) unsigned NOT NULL DEFAULT '0',
- allowed_comm tinyint(1) unsigned NOT NULL DEFAULT '0',
+ allowed_comm varchar(255) NOT NULL DEFAULT '6',
  allowed_rating tinyint(1) unsigned NOT NULL DEFAULT '0',
  ratingdetail varchar(255) NOT NULL DEFAULT '',
  allowed_send tinyint(1) unsigned NOT NULL DEFAULT '0',
