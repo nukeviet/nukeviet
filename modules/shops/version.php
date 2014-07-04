ae82228b9173a2cdb67fd838f68615aaae0f661b
Hồ Ngọc Triển <hongoctrien@vinades.vn>
2014-07-04 10:05:46 +0700

    Format code

--- modules/shops/version.php
+++ modules/shops/version.php
@@ -14,7 +14,7 @@ $module_version = array(
 	'name' => 'Shops', // Tieu de module
 	'modfuncs' => 'main,viewcat,detail,search,cart,order,payment,complete,history,group,search_result,compare', // Cac function co block
 	'is_sysmod' => 0, // 1:0 => Co phai la module he thong hay khong
-	'virtual' => 1, // 1:0 => Co cho phep ao hao module hay khong 
+	'virtual' => 1, // 1:0 => Co cho phep ao hao module hay khong
 	'version' => '4.0.00', // Phien ban cua module
 	'date' => 'Sun, 21 Jun 2013 00:50:00 GMT', // Ngay phat hanh phien ban
 	'author' => 'VINADES (contact@vinades.vn)', // Tac gia
