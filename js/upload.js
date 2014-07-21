dc728169805388f9a003aff86510263644e371bb
Phan Tan Dung <phantandung92@gmail.com>
2014-07-21 17:48:06 +0700

    Disable select text

--- js/upload.js
+++ js/upload.js
@@ -1857,3 +1857,14 @@ var NVCMENU = {
 KEYPR.init();
 RRT.init();
 NVCMENU.init();
+
+// Disable select text
+$('#imglist').attr('unselectable','on').css({
+	'-moz-user-select':'-moz-none',
+	'-moz-user-select':'none',
+	'-o-user-select':'none',
+	'-khtml-user-select':'none',
+	'-webkit-user-select':'none',
+	'-ms-user-select':'none',
+	'user-select':'none'
+}).bind('selectstart', function(){ return false; });
