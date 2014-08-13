3ee926f594e0526ca64adca9a2c813e4d219ebee
VuThao <vuthao27@gmail.com>
2014-08-13 19:01:39 +0700

    module news: update menu

--- modules/news/admin.menu.php
+++ modules/news/admin.menu.php
@@ -77,7 +77,10 @@ else
 $allow_func = array( 'main', 'view', 'exptime', 'publtime', 'waiting', 'declined', 're-published', 'content', 'rpc', 'del_content', 'alias', 'topicajax', 'sourceajax', 'tagsajax', 'cat', 'change_cat', 'list_cat', 'del_cat', 'blocksajax' );
 
 $submenu['cat'] = $lang_module['categories'];
-$submenu['content'] = $lang_module['content_add'];
+if( ! isset( $site_mods['cms'] ) )
+{
+	$submenu['content'] = $lang_module['content_add'];
+}
 
 if( $NV_IS_ADMIN_MODULE )
 {
