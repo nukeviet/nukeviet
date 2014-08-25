8b40ff50d262359ed23f729e4ed2f1ef5d40194f
Phan Tan Dung <phantandung92@gmail.com>
2014-08-26 00:56:50 +0700

    Fix block headline

--- modules/news/blocks/module.block_headline.php
+++ modules/news/blocks/module.block_headline.php
@@ -84,7 +84,7 @@ if( ! nv_function_exists( 'nv_block_headline' ) )
 					->from( NV_PREFIXLANG . '_' . $module_data . '_rows t1' )
 					->join( 'INNER JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_block t2 ON t1.id = t2.id' )
 					->where( 't2.bid=' . $array_bid['bid'] )
-					->order( 't2.weight DESC' )
+					->order( 't2.weight ASC' )
 					->limit( $array_bid['number'] );
 	
 				$result = $db->query( $db->sql() );
