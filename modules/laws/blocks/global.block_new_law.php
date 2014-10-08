2475ac5d5db4a088bdb740f1b68f3d639eef5d81
hongoctrien <hongoctrien@vinades.vn>
2014-10-08 08:59:23 +0700

    Update block new laws

--- modules/laws/blocks/global.block_new_law.php
+++ modules/laws/blocks/global.block_new_law.php
@@ -12,14 +12,41 @@ if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
 if ( ! nv_function_exists( 'nv_law_block_newg' ) )
 {
+	function nv_block_config_new_laws( $module, $data_block, $lang_block )
+	{
+		$html = '';
+        $html .= '<tr>';
+		$html .= '<td>' . $lang_block['numrow'] . '</td>';
+		$html .= '<td><input type="text" class="form-control w200" name="config_numrow" value="' . $data_block['numrow'] . '" /></td>';
+        $html .= '</tr>';
+        $html .= '<tr>';
+		$html .= '<td>' . $lang_block['title_length'] . '</td>';
+		$html .= '<td><input type="text" class="form-control w200" name="config_title_length" value="' . $data_block['title_length'] . '" /><span class="help-block">' . $lang_block['title_note'] . '</span></td>';
+        $html .= '</tr>';
+		
+		return $html;
+	}
+
+	function nv_block_config_new_laws_submit( $module, $lang_block )
+	{
+		global $nv_Request;
+		$return = array();
+		$return['error'] = array();
+		$return['config'] = array();
+		$return['config']['numrow'] = $nv_Request->get_int( 'config_numrow', 'post', 0 );
+		$return['config']['title_length'] = $nv_Request->get_int( 'config_title_length', 'post', 0 );
+		return $return;
+	}
+
     function nv_law_block_newg ( $block_config )
     {
         global $module_info, $lang_module, $site_mods, $db, $my_head, $module_name;
+		
         $module = $block_config['module'];
         $data = $site_mods[$module]['module_data'];
         $modfile = $site_mods[$module]['module_file'];
 		
-		$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $data . "_row WHERE status=1 ORDER BY addtime DESC LIMIT 0,10";
+		$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $data . "_row WHERE status=1 ORDER BY addtime DESC LIMIT 0," . $block_config['numrow'];
 		$result = $db->query( $sql );
 		$numrow = $result->rowCount();
 		
@@ -35,7 +62,6 @@ if ( ! nv_function_exists( 'nv_law_block_newg' ) )
             }
 			
             $xtpl = new XTemplate( "block_new_law.tpl", NV_ROOTDIR . "/themes/" . $block_theme . "/modules/" . $modfile );
-			$title_length = 34;
 			
 			if( $module_name != $module )
 			{
@@ -57,7 +83,15 @@ if ( ! nv_function_exists( 'nv_law_block_newg' ) )
 			{
 				$link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module . "&amp;" . NV_OP_VARIABLE . "=detail/" . change_alias( $row['title'] . "-" . $row['id'] );
 				$row['link'] = $link;
-				$row['stitle'] = nv_clean60( $row['title'], $title_length );
+				
+				if( ! empty( $block_config['title_length'] ) )
+				{
+					$row['stitle'] = nv_clean60( $row['title'], $block_config['title_length'] );
+				}
+				else
+				{
+					$row['stitle'] = $row['title'];
+				}
 				
 				$xtpl->assign( 'ROW', $row );			
 				$xtpl->parse( 'main.loop' );
