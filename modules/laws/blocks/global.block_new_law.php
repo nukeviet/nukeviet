6a5c54a04e9a6f7d0e118310d8ea47276a5af20c
VuThao <vuthao27@gmail.com>
2014-10-09 08:32:21 +0700

    Sửa lỗi block khi không cấu hình

--- modules/laws/blocks/global.block_new_law.php
+++ modules/laws/blocks/global.block_new_law.php
@@ -23,7 +23,7 @@ if ( ! nv_function_exists( 'nv_law_block_newg' ) )
 		$html .= '<td>' . $lang_block['title_length'] . '</td>';
 		$html .= '<td><input type="text" class="form-control w200" name="config_title_length" value="' . $data_block['title_length'] . '" /><span class="help-block">' . $lang_block['title_note'] . '</span></td>';
         $html .= '</tr>';
-		
+
 		return $html;
 	}
 
@@ -41,15 +41,17 @@ if ( ! nv_function_exists( 'nv_law_block_newg' ) )
     function nv_law_block_newg ( $block_config )
     {
         global $module_info, $lang_module, $site_mods, $db, $my_head, $module_name;
-		
+
         $module = $block_config['module'];
         $data = $site_mods[$module]['module_data'];
         $modfile = $site_mods[$module]['module_file'];
-		
-		$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $data . "_row WHERE status=1 ORDER BY addtime DESC LIMIT 0," . $block_config['numrow'];
+		$numrow = ( isset( $block_config['numrow'] ) ) ? $block_config['numrow'] : 10;
+		$title_length = ( isset( $block_config['title_length'] ) ) ? $block_config['title_length'] : 0;
+
+		$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $data . "_row WHERE status=1 ORDER BY addtime DESC LIMIT 0," . $numrow;
 		$result = $db->query( $sql );
 		$numrow = $result->rowCount();
-		
+
         if ( ! empty( $numrow ) )
         {
             if ( file_exists( NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/" . $modfile . "/block_new_law.tpl" ) )
@@ -60,9 +62,9 @@ if ( ! nv_function_exists( 'nv_law_block_newg' ) )
             {
                 $block_theme = "default";
             }
-			
+
             $xtpl = new XTemplate( "block_new_law.tpl", NV_ROOTDIR . "/themes/" . $block_theme . "/modules/" . $modfile );
-			
+
 			if( $module_name != $module )
 			{
 				$my_head .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . NV_BASE_SITEURL . "themes/" . $block_theme . "/css/laws.css\" />";
@@ -76,27 +78,27 @@ if ( ! nv_function_exists( 'nv_law_block_newg' ) )
 			{
 				$lang_block_module = $lang_module;
 			}
-			
+
 			$xtpl->assign( 'LANG', $lang_block_module );
 
 			while( $row = $result->fetch() )
 			{
 				$link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module . "&amp;" . NV_OP_VARIABLE . "=detail/" . change_alias( $row['title'] . "-" . $row['id'] );
 				$row['link'] = $link;
-				
+
 				if( ! empty( $block_config['title_length'] ) )
 				{
-					$row['stitle'] = nv_clean60( $row['title'], $block_config['title_length'] );
+					$row['stitle'] = nv_clean60( $row['title'], $title_length );
 				}
 				else
 				{
 					$row['stitle'] = $row['title'];
 				}
-				
-				$xtpl->assign( 'ROW', $row );			
+
+				$xtpl->assign( 'ROW', $row );
 				$xtpl->parse( 'main.loop' );
             }
-			
+
             $xtpl->parse( 'main' );
             return $xtpl->text( 'main' );
         }
