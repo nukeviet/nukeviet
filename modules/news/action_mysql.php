8274a7167c722a1872193007fa9c3ccd17bc247f
vuthao <thao@vinades.vn>
2014-10-24 10:50:39 +0700

    module news: fix file action

--- modules/news/action_mysql.php
+++ modules/news/action_mysql.php
@@ -11,11 +11,28 @@
 if( ! defined( 'NV_IS_FILE_MODULES' ) ) die( 'Stop!!!' );
 
 $sql_drop_module = array();
-
-$result = $db->query( 'SHOW TABLE STATUS LIKE ' . $db->quote( $db_config['prefix'] . '\_' . $lang . '\_' . $module_data . '\_%' ) );
-while( $item = $result->fetch() )
+$array_table = array(
+	'admins',
+	'block',
+	'block_cat',
+	'bodytext',
+	'cat',
+	'config_post',
+	'rows',
+	'sources',
+	'tags',
+	'tags_id',
+	'topics'
+);
+$table = $db_config['prefix'] . '_' . $lang . '_' . $module_data;
+$result = $db->query( 'SHOW TABLE STATUS LIKE ' . $db->quote( $table . '_%' ) );
+while( $item = $result->fetch( ) )
 {
-	$sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $item['name'];
+	$name = substr( $item['name'], strlen( $table ) + 1 );
+	if( preg_match( '/^' . $db_config['prefix'] . '\_' . $lang . '\_' . $module_data . '\_/', $item['name'] ) and ( preg_match( '/^([0-9]+)$/', $name ) or in_array( $name, $array_table ) or preg_match( '/^bodyhtml\_([0-9]+)$/', $name ) ) )
+	{
+		$sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $item['name'];
+	}
 }
 
 $result = $db->query( "SHOW TABLE STATUS LIKE '" . $db_config['prefix'] . "\_" . $lang . "\_comments'" );
