5d96e7dea439d9ded826517baaf161b382dc8443
VuThao <vuthao27@gmail.com>
2014-11-01 14:26:55 +0700

    blocks: fix reset order

--- admin/themes/blocks_reset_order.php
+++ admin/themes/blocks_reset_order.php
@@ -84,20 +84,18 @@ if( ! empty( $theme ) and $checkss == md5( $theme . $global_config['sitekey'] . 
 	// Kiem tra va cap nhat lai weight tung function
 	$array_position = array_unique( $array_position );
 
-	$sth = $db->prepare( 'SELECT t1.bid, t1.func_id
-		FROM ' . NV_BLOCKS_TABLE . '_weight t1
-		INNER JOIN ' . NV_BLOCKS_TABLE . '_groups t2 ON t1.bid = t2.bid
-		WHERE t2.theme= :theme AND t2.position = :position
-		ORDER BY t1.func_id ASC, t1.weight ASC' );
-    $sth->bindParam( ':theme', $theme, PDO::PARAM_STR );
-
 	foreach( $array_position as $position )
 	{
 		$func_id_old = $weight = 0;
 
-		$sth->bindParam( ':position', $position, PDO::PARAM_STR );
+		$sth = $db->prepare( 'SELECT t1.bid, t1.func_id
+			FROM ' . NV_BLOCKS_TABLE . '_weight t1
+			INNER JOIN ' . NV_BLOCKS_TABLE . '_groups t2 ON t1.bid = t2.bid
+			WHERE t2.theme= :theme AND t2.position = :position
+			ORDER BY t1.func_id ASC, t1.weight ASC' );
+	    $sth->bindParam( ':theme', $theme, PDO::PARAM_STR );
+	    $sth->bindParam( ':position', $position, PDO::PARAM_STR );
 		$sth->execute();
-
 		while( list( $bid_i, $func_id_i ) = $sth->fetch( 3 ) )
 		{
 			if( $func_id_i == $func_id_old )
