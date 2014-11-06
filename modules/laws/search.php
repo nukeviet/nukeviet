47ede7555f721ac6ab10029a52738330d43d21ba
VuThao <vuthao27@gmail.com>
2014-11-07 06:21:38 +0700

    Sửa lỗi tìm kiếm

--- modules/laws/search.php
+++ modules/laws/search.php
@@ -10,29 +10,27 @@
 
 if ( ! defined( 'NV_IS_MOD_SEARCH' ) ) die( 'Stop!!!' );
 
-$sql = "SELECT SQL_CALC_FOUND_ROWS id,title,alias,bodytext 
-FROM " . NV_PREFIXLANG . "_" . $m_values['module_data'] . "_rows 
-WHERE status=1 AND (" . nv_like_logic( 'title', $dbkeyword, $logic ) . " 
-OR " . nv_like_logic( 'bodytext', $dbkeyword, $logic ) . ") 
-LIMIT " . $pages . "," . $limit;
+$sql = 'SELECT SQL_CALC_FOUND_ROWS id,title,alias,bodytext
+FROM ' . NV_PREFIXLANG . '_' . $m_values['module_data'] . '_row
+WHERE status=1 AND (' . nv_like_logic( 'title', $dbkeyword, $logic ) . '
+OR ' . nv_like_logic( 'bodytext', $dbkeyword, $logic ) . ')
+LIMIT ' . ( $page - 1 ) * $limit . ',' . $limit;
 
 $tmp_re = $db->query( $sql );
 
-$result = $db->query( "SELECT FOUND_ROWS()" );
-$all_page = $result->fetchColumn();
+$result = $db->query( 'SELECT FOUND_ROWS()' );
+$num_items = $result->fetchColumn();
 
-if ( $all_page )
+if ( $num_items )
 {
-    $link = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $m_values['module_name'] . '&amp;' . NV_OP_VARIABLE . '=';
+    $link = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $m_values['module_name'] . '&amp;' . NV_OP_VARIABLE . '=';
 
     while ( list( $id, $tilterow, $alias, $content ) = $tmp_re->fetch( 3 ) )
     {
-        $url = $link . $alias . "-" . $id;
-
-        $result_array[] = array( //
-            'link' => $url, //
-            'title' => BoldKeywordInStr( $tilterow, $key, $logic ), //
-            'content' => BoldKeywordInStr( $content, $key, $logic ) //
-            );
+        $result_array[] = array(
+            'link' => $link . $alias . '-' . $id,
+            'title' => BoldKeywordInStr( $tilterow, $key, $logic ),
+            'content' => BoldKeywordInStr( $content, $key, $logic )
+        );
     }
 }
