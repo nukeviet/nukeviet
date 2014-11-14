0ced5d1ee45d61d1b102f63e56e524ddcc821175
vuthao <thao@vinades.vn>
2014-11-14 11:35:04 +0700

    Module news: fix delete content form site

--- modules/news/js/user.js
+++ modules/news/js/user.js
@@ -9,15 +9,15 @@
 function sendrating(id, point, newscheckss) {
 	if (point == 1 || point == 2 || point == 3 || point == 4 || point == 5) {
 		$.post(nv_siteroot + 'index.php?' + nv_lang_variable + '=' + nv_sitelang + '&' + nv_name_variable + '=' + nv_module_name + '&' + nv_fc_variable + '=rating&nocache=' + new Date().getTime(), 'id=' + id + '&checkss=' + newscheckss + '&point=' + point, function(res) {
-			$("#stringrating").html(res);
+			$('#stringrating').html(res);
 		});
 	}
 }
 
 function nv_del_content(id, checkss, base_adminurl) {
 	if (confirm(nv_is_del_confirm[0])) {
-		$.post(nv_siteroot + 'index.php?' + nv_lang_variable + '=' + nv_sitelang + '&' + nv_name_variable + '=' + nv_module_name + '&' + nv_fc_variable + '=del_content&nocache=' + new Date().getTime(), 'id=' + id + '&checkss=' + checkss, function(res) {
-			var r_split = res.split("_");
+		$.post(base_adminurl + 'index.php?' + nv_lang_variable + '=' + nv_sitelang + '&' + nv_name_variable + '=' + nv_module_name + '&' + nv_fc_variable + '=del_content&nocache=' + new Date().getTime(), 'id=' + id + '&checkss=' + checkss, function(res) {
+			var r_split = res.split('_');
 			if (r_split[0] == 'OK') {
 				window.location.href = strHref;
 			} else if (r_split[0] == 'ERR') {
