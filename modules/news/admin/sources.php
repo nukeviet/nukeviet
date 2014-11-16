de3ed53bf5b89a5f01c480c4a7d3c720f80b3de2
vuthao <thao@vinades.vn>
2014-11-16 10:56:23 +0700

    module News: fix deleted when deleting logo image

--- modules/news/admin/sources.php
+++ modules/news/admin/sources.php
@@ -32,21 +32,30 @@ if( ! empty( $savecat ) )
 		$link = '';
 	}
 
-	$logo_old = $db->query( "SELECT logo FROM " . NV_PREFIXLANG . "_" . $module_data . "_sources WHERE sourceid =" . $sourceid )->fetchColumn();
+	$logo_old = $db->query( 'SELECT logo FROM ' . NV_PREFIXLANG . '_' . $module_data . '_sources WHERE sourceid =' . $sourceid )->fetchColumn();
 
 	$logo = $nv_Request->get_title( 'logo', 'post', '' );
 	if( ! nv_is_url( $logo ) and file_exists( NV_DOCUMENT_ROOT . $logo ) )
 	{
-		$lu = strlen( NV_BASE_SITEURL . NV_UPLOADS_DIR . "/" . $module_name . "/source/" );
+		$lu = strlen( NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_name . '/source/' );
 		$logo = substr( $logo, $lu );
 	}
 	elseif( ! nv_is_url( $logo ) )
 	{
 		$logo = $logo_old;
 	}
+
 	if( ( $logo != $logo_old ) and ! empty( $logo_old ) )
 	{
-		@unlink( NV_UPLOADS_REAL_DIR . "/" . $module_name . "/source/" . $logo_old );
+		$_count = $db->query( 'SELECT COUNT(*) FROM ' . NV_PREFIXLANG . '_' . $module_data . '_sources WHERE sourceid != ' . $sourceid .' AND logo =' . $db->quote( basename( $logo_old ) ) )->fetchColumn();
+		if( empty( $_count ) )
+		{
+			@unlink( NV_ROOTDIR . '/' . NV_UPLOADS_DIR . '/' . $module_name . '/source/' . $logo_old );
+			@unlink( NV_ROOTDIR . '/' . NV_FILES_DIR . '/' . $module_name . '/source/' . $logo_old );
+	
+			$_did = $db->query( 'SELECT did FROM ' . NV_UPLOAD_GLOBALTABLE . '_dir WHERE dirname=' . $db->quote( dirname( NV_UPLOADS_DIR . '/' . $module_name . '/source/' . $logo_old ) ) )->fetchColumn();
+			$db->query( 'DELETE FROM ' . NV_UPLOAD_GLOBALTABLE . '_file WHERE did = ' . $_did . ' AND title=' . $db->quote( basename( $logo_old ) ) );
+		}
 	}
 	if( empty( $title ) )
 	{
@@ -54,9 +63,9 @@ if( ! empty( $savecat ) )
 	}
 	elseif( $sourceid == 0 )
 	{
-		$weight = $db->query( "SELECT max(weight) FROM " . NV_PREFIXLANG . "_" . $module_data . "_sources" )->fetchColumn();
+		$weight = $db->query( 'SELECT max(weight) FROM ' . NV_PREFIXLANG . '_' . $module_data . '_sources' )->fetchColumn();
 		$weight = intval( $weight ) + 1;
-		$sql = "INSERT INTO " . NV_PREFIXLANG . "_" . $module_data . "_sources (title, link, logo, weight, add_time, edit_time) VALUES ( :title, :link, :logo, :weight, " . NV_CURRENTTIME . ", " . NV_CURRENTTIME . ")";
+		$sql = 'INSERT INTO ' . NV_PREFIXLANG . '_' . $module_data . '_sources (title, link, logo, weight, add_time, edit_time) VALUES ( :title, :link, :logo, :weight, ' . NV_CURRENTTIME . ', ' . NV_CURRENTTIME . ')';
 		$data_insert = array();
 		$data_insert['title'] = $title;
 		$data_insert['link'] = $link;
@@ -65,7 +74,7 @@ if( ! empty( $savecat ) )
 		
 		if( $db->insert_id( $sql, 'sourceid', $data_insert ) )
 		{
-			nv_insert_logs( NV_LANG_DATA, $module_name, 'log_add_source', " ", $admin_info['userid'] );
+			nv_insert_logs( NV_LANG_DATA, $module_name, 'log_add_source', ' ', $admin_info['userid'] );
 			Header( 'Location: ' . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=' . $op );
 			die();
 		}
@@ -76,13 +85,13 @@ if( ! empty( $savecat ) )
 	}
 	else
 	{
-		$stmt = $db->prepare( "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_sources SET title= :title, link = :link, logo= :logo, edit_time=" . NV_CURRENTTIME . " WHERE sourceid =" . $sourceid );
+		$stmt = $db->prepare( 'UPDATE ' . NV_PREFIXLANG . '_' . $module_data . '_sources SET title= :title, link = :link, logo= :logo, edit_time=' . NV_CURRENTTIME . ' WHERE sourceid =' . $sourceid );
 		$stmt->bindParam( ':title', $title, PDO::PARAM_STR );
 		$stmt->bindParam( ':link', $link, PDO::PARAM_STR );
 		$stmt->bindParam( ':logo', $logo, PDO::PARAM_STR );
 		if( $stmt->execute() )
 		{
-			nv_insert_logs( NV_LANG_DATA, $module_name, 'log_edit_source', "sourceid " . $sourceid, $admin_info['userid'] );
+			nv_insert_logs( NV_LANG_DATA, $module_name, 'log_edit_source', 'sourceid ' . $sourceid, $admin_info['userid'] );
 			Header( 'Location: ' . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=' . $op );
 			die();
 		}
@@ -96,13 +105,13 @@ if( ! empty( $savecat ) )
 $sourceid = $nv_Request->get_int( 'sourceid', 'get', 0 );
 if( $sourceid > 0 )
 {
-	list( $sourceid, $title, $link, $logo ) = $db->query( "SELECT sourceid, title, link, logo FROM " . NV_PREFIXLANG . "_" . $module_data . "_sources where sourceid=" . $sourceid )->fetch( 3 );
+	list( $sourceid, $title, $link, $logo ) = $db->query( 'SELECT sourceid, title, link, logo FROM ' . NV_PREFIXLANG . '_' . $module_data . '_sources where sourceid=' . $sourceid )->fetch( 3 );
 	$lang_module['add_topic'] = $lang_module['edit_topic'];
 }
 
 if( ! empty( $logo ) )
 {
-	$logo = NV_BASE_SITEURL . NV_UPLOADS_DIR . "/" . $module_name . "/source/" . $logo;
+	$logo = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_name . '/source/' . $logo;
 }
 
 $xtpl = new XTemplate( 'sources.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file );
