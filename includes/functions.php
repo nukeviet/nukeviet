caafbe6b1ffffd0c8e2a9d76b85c972ad4eb0aab
VuThao <vuthao27@gmail.com>
2015-07-05 08:06:24 +0700

    module users: Rewrite for function changequestion

--- includes/functions.php
+++ includes/functions.php
@@ -1705,7 +1705,7 @@ function nv_site_mods( $module_name = '' )
 		{
 			if( defined( 'NV_IS_USER' ) )
 			{
-				$user_ops = array( 'main', 'logout', 'changepass', 'openid', 'editinfo', 'regroups', 'avatar' );
+				$user_ops = array( 'main', 'logout', 'changepass', 'openid', 'editinfo', 'changequestion', 'regroups', 'avatar' );
 			}
 			else
 			{
--- modules/users/funcs/memberlist.php
+++ modules/users/funcs/memberlist.php
@@ -5,7 +5,7 @@
  * @Author VINADES.,JSC (contact@vinades.vn)
  * @Copyright (C) 2010 - 2014 VINADES.,JSC. All rights reserved
  * @License GNU/GPL version 2 or any later version
- * @Createdate Sun, 08 Apr 2012 00:00:00 GMT GMT
+ * @Createdate Sun, 08 Apr 2012 00:00:00 GMT
  */
 
 if( ! defined( 'NV_IS_MOD_USER' ) ) die( 'Stop!!!' );
