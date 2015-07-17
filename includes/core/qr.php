5586da8a29e54fae5030f4c77784ba1b322c4aa2
Nguyen Anh Tu <anhtu@vinades.vn>
2015-07-18 00:22:23 +0700

    Thank you for new block "QR-code"

--- includes/core/qr.php
+++ includes/core/qr.php
@@ -10,14 +10,15 @@
 
 if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
-require NV_ROOTDIR . '/includes/class/qrcode.class.php';
+//Khong can doan nay vi da co AUTOLOAD
+//require NV_ROOTDIR . '/includes/class/qrcode.class.php';
 
 $url = $nv_Request->get_string( 'u', 'get', '' );
 $level = $nv_Request->get_title( 'l', 'get', '' );
 $pixel_per_point = $nv_Request->get_int( 'ppp', 'get', 4 );
 $outer_frame = $nv_Request->get_int( 'of', 'get', 1 );
 
-if( ! empty( $url ) and in_array( $level, array( 'L', 'M', 'Q', 'H' ) ) and ( $pixel_per_point > 0 and $pixel_per_point < 15 ) and ( $outer_frame > 0 and $outer_frame < 6 ) )
+if( ! empty( $url ) and in_array( $level, array( 'L', 'M', 'Q', 'H' ) ) and ( $pixel_per_point > 0 and $pixel_per_point < 13 ) and ( $outer_frame > 0 and $outer_frame < 6 ) )
 {
 	QRcode::png( $url, false, $level, $pixel_per_point, $outer_frame );
 }
