f208c4874f42e21ca907d5a1aabafe007e78dd7f
Nguyen Anh Tu <anhtu@vinades.vn>
2015-07-21 08:52:19 +0700

    Update theme Default 3
    Van con tiep tuc

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -211,36 +211,36 @@ function nv_site_theme( $contents, $full = true )
 		$xtpl->assign( 'THEME_STAT_IMG', $theme_stat_img );
 
 		// Change theme types
-		$mobile_theme = empty( $module_info['mobile'] ) ?  $global_config['mobile_theme'] : $module_info['mobile'];
-		if( empty( $mobile_theme ) or empty( $global_config['switch_mobi_des'] ))
-		{
-			$array_theme_type  = array_diff( $global_config['array_theme_type'], array( 'm' ) );
-		}
-		else
-		{
-			$array_theme_type = $global_config['array_theme_type'];
-		}
-		$num_theme_type = sizeof( $array_theme_type ) - 1;
-		foreach( $array_theme_type as $i => $theme_type )
-		{
-			$xtpl->assign( 'STHEME_TYPE', NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;nv' . NV_LANG_DATA . 'themever=' . $theme_type . '&amp;nv_redirect=' . nv_base64_encode( $client_info['selfurl'] ) );
-			$xtpl->assign( 'STHEME_TITLE', $lang_global['theme_type_' . $theme_type] );
-			$xtpl->assign( 'STHEME_INFO', sprintf( $lang_global['theme_type_chose'], $lang_global['theme_type_' . $theme_type] ) );
+        $mobile_theme = empty( $module_info['mobile'] ) ? $global_config['mobile_theme'] : $module_info['mobile'];
+        if ( empty( $mobile_theme ) or empty( $global_config['switch_mobi_des'] ) )
+        {
+            $array_theme_type = array_diff( $global_config['array_theme_type'], array( 'm' ) );
+        }
+        else
+        {
+            $array_theme_type = $global_config['array_theme_type'];
+        }
+        
+        $icons = array('r' => 'random', 'd' => 'desktop', 'm' => 'mobile');
+        foreach ( $array_theme_type as $theme_type )
+        {
+            $xtpl->assign( 'STHEME_TYPE', NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;nv' . NV_LANG_DATA . 'themever=' . $theme_type . '&amp;nv_redirect=' . nv_base64_encode( $client_info['selfurl'] ) );
+            $xtpl->assign( 'STHEME_TITLE', $lang_global['theme_type_' . $theme_type] );
+            $xtpl->assign( 'STHEME_INFO', sprintf( $lang_global['theme_type_chose'], $lang_global['theme_type_' . $theme_type] ) );
+            $xtpl->assign( 'STHEME_ICON', $icons[$theme_type] );
 
-			if( $theme_type == $global_config['current_theme_type'] )
-			{
-				$xtpl->parse( 'main.theme_type.loop.current' );
-			}
-			else
-			{
-				$xtpl->parse( 'main.theme_type.loop.other' );
-			}
+            if ( $theme_type == $global_config['current_theme_type'] )
+            {
+                $xtpl->parse( 'main.theme_type.loop.current' );
+            }
+            else
+            {
+                $xtpl->parse( 'main.theme_type.loop.other' );
+            }
 
-			if( $i < $num_theme_type ) $xtpl->parse( 'main.theme_type.loop.space' );
-
-			$xtpl->parse( 'main.theme_type.loop' );
-		}
-		$xtpl->parse( 'main.theme_type' );
+            $xtpl->parse( 'main.theme_type.loop' );
+        }
+        $xtpl->parse( 'main.theme_type' );
 	}
 
 	$xtpl->parse( 'main' );
