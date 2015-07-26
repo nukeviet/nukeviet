0dc78beb9546e43650b9eab84028c45a99774ae4
Phan Tan Dung <phantandung92@gmail.com>
2015-07-27 01:09:11 +0700

    Check cronjob, block exists before delete

--- admin/extensions/manage.php
+++ admin/extensions/manage.php
@@ -638,11 +638,34 @@ foreach( $array_langs as $lang )
 	}
 }
 
-// Array themes exists
-
+// Array themes exists - Unnecessary
 // Array blocks exists
+$array_blocks_exists = array();
+
+foreach( $array_langs as $lang )
+{
+	$sql = 'SELECT DISTINCT file_name FROM ' . $db_config['prefix'] . '_' . $lang . '_blocks_groups';
+	$result = $db->query( $sql );
+
+	while( $row = $result->fetch() )
+	{
+		$array_blocks_exists[$row['file_name']] = $row['file_name'];
+	}
+}
 
 // Array crons exists
+$array_crons_exists = array();
+
+foreach( $array_langs as $lang )
+{
+	$sql = 'SELECT DISTINCT run_file FROM ' . NV_CRONJOBS_GLOBALTABLE;
+	$result = $db->query( $sql );
+
+	while( $row = $result->fetch() )
+	{
+		$array_crons_exists[$row['run_file']] = $row['run_file'];
+	}
+}
 
 // List extensions
 $sql = 'SELECT * FROM ' . $db_config['prefix'] . '_setup_extensions WHERE title=basename';
@@ -666,6 +689,14 @@ while( $row = $result->fetch() )
 		$row['delete_allowed'] = false;
 	}
 	elseif( $row['type'] == 'theme' and ( $global_config['site_theme'] == $row['basename'] or $row['basename'] == 'default' ) )
+	{
+		$row['delete_allowed'] = false;
+	}
+	elseif( $row['type'] == 'block' and isset( $array_blocks_exists[$row['basename']] ) )
+	{
+		$row['delete_allowed'] = false;
+	}
+	elseif( $row['type'] == 'cronjob' and isset( $array_crons_exists[$row['basename']] ) )
 	{
 		$row['delete_allowed'] = false;
 	}
@@ -722,6 +753,7 @@ if( $selecttype == '' or $selecttype == 'theme' )
 		);
 	}
 }
+
 foreach( $array_parse as $row )
 {
 	$xtpl->assign( 'ROW', $row );
