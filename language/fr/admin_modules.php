7ebc42954bafdf5ab7239049e0f2a09e89221014
VuThao <vuthao27@gmail.com>
2015-07-27 14:35:02 +0700

    update language France for http://translate.nukeviet.vn/

---
+++ language/fr/admin_modules.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+* @Project NUKEVIET 4.x
+* @Author VINADES.,JSC (contact@vinades.vn)
+* @Copyright (C) 2015 VINADES.,JSC. All rights reserved
+* @Language Français
+* @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
+* @Createdate Jun 21, 2010, 10:30:00 AM
+*/
+
+if( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+
+$lang_translator['author'] = 'Phạm Chí Quang';
+$lang_translator['createdate'] = '21/6/2010, 17:30';
+$lang_translator['copyright'] = '@Copyright (C) 2010 VINADES.,JSC. Tous droits réservés.';
+$lang_translator['info'] = 'Langue française pour NukeViet 3';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['modules'] = 'Installation';
+$lang_module['blocks'] = 'Configuration des blocks';
+$lang_module['language'] = 'Installation de langues';
+$lang_module['setup'] = 'Installer';
+$lang_module['main'] = 'Liste des modules';
+$lang_module['edit'] = 'Éditer le module &ldquo;%s&rdquo;';
+$lang_module['edit_error_update_theme'] = 'La mise a jour a détecté que le thème  %s est invalide ou erroné, merci de vérifier';
+$lang_module['caption_actmod'] = 'Liste des modules actifs';
+$lang_module['caption_deactmod'] = 'Liste de modules inactifs';
+$lang_module['caption_badmod'] = 'Liste des modules inactifs par erreurs';
+$lang_module['caption_newmod'] = 'Liste des modules pas encore installés';
+$lang_module['custom_title'] = 'Titre';
+$lang_module['admin_title'] = 'Titre de la section d\'administration';
+$lang_module['weight'] = 'Ordre';
+$lang_module['in_menu'] = 'Menu en haut';
+$lang_module['submenu'] = 'Menu secondaire';
+$lang_module['settime'] = 'Date d\'installation';
+$lang_module['author'] = 'Auteur';
+$lang_module['mobile'] = 'Theme mobile';
+$lang_module['theme_default'] = 'Défaut';
+$lang_module['keywords'] = 'Mots clés';
+$lang_module['keywords_info'] = 'Séparer par virgule';
+$lang_module['funcs_list'] = 'Liste des fonctions de module &ldquo;%s&rdquo;';
+$lang_module['funcs_title'] = 'Fonction';
+$lang_module['funcs_custom_title'] = 'Titre';
+$lang_module['funcs_layout'] = 'Layout utilisé';
+$lang_module['funcs_subweight'] = 'Ordre';
+$lang_module['activate_rss'] = 'Activer la fonction RSS';
+$lang_module['module_sys'] = 'Les Modules du système';
+$lang_module['vmodule'] = 'Modules virtuels';
+$lang_module['vmodule_add'] = 'Ajout de mod. virtuel';
+$lang_module['vmodule_name'] = 'Nom du nouveau module';
+$lang_module['vmodule_file'] = 'Module original';
+$lang_module['vmodule_note'] = 'Description';
+$lang_module['vmodule_select'] = 'Choisir un module';
+$lang_module['vmodule_blockquote'] = 'Note: Nom du nouveau module se combine de lettres latines, chiffres et/ou tiret.';
