94a1d0b11a7b8854cb120c49a12d5ea0dd5b3222
Nguyen Anh Tu <anhtu@vinades.vn>
2015-07-28 11:09:16 +0700

    Update module Contact

--- modules/contact/blocks/global.contact_default.php
+++ modules/contact/blocks/global.contact_default.php
@@ -49,21 +49,82 @@ if ( ! nv_function_exists( 'nv_contact_default_info' ) )
 
         if ( ! empty( $row['phone'] ) )
         {
+            $nums = array_map( "trim", explode( "|", nv_unhtmlspecialchars( $row['phone'] ) ) );
+            $mainphone = array();
+            foreach ( $nums as $k => $num )
+            {
+                unset( $m );
+                if ( preg_match( "/^(.*)\s*\[([0-9\+\.\,\;\*\#]+)\]$/", $num, $m ) )
+                {
+                    $phone = array( 'number' => nv_htmlspecialchars( $m[1] ), 'href' => $m[2] );
+                    $xtpl->assign( 'PHONE', $phone );
+                    $xtpl->parse( 'main.phone.item.href' );
+                    $xtpl->parse( 'main.phone.item.href2' );
+                    
+                    if ( $k == 0 ) $mainphone = $phone;
+                }
+                else
+                {
+                    
+                    $num = preg_replace( "/\[[^\]]*\]/", "", $num );
+                    $phone = array( 'number' => nv_htmlspecialchars( $num ) );
+                    $xtpl->assign( 'PHONE', $phone );
+                    
+                    if ( $k == 0 ) $mainphone = $phone;
+                }
+                if ( $k ) $xtpl->parse( 'main.phone.item.comma' );
+                $xtpl->parse( 'main.phone.item' );
+            }
+            
             $xtpl->parse( 'main.phone' );
+            
+            if( !empty( $mainphone ) )
+            {
+                $xtpl->assign( 'MAINPHONE', $mainphone );
+                if( isset( $mainphone['href'] ) )
+                {
+                    $xtpl->parse( 'main.mainphone.href' );
+                    $xtpl->parse( 'main.mainphone.href2' );
+                }
+                $xtpl->parse( 'main.mainphone' );
+            }
         }
 
         if ( ! empty( $row['email'] ) )
         {
+            $emails = array_map( "trim", explode( ",", $row['email'] ) );
+
+            foreach ( $emails as $k => $email )
+            {
+                $xtpl->assign( 'EMAIL', $email );
+                if ( $k ) $xtpl->parse( 'main.email.item.comma' );
+                $xtpl->parse( 'main.email.item' );
+            }
+
             $xtpl->parse( 'main.email' );
         }
 
         if ( ! empty( $row['yahoo'] ) )
         {
+            $ys = array_map( "trim", explode( ",", $row['yahoo'] ) );
+            foreach ( $ys as $k => $y )
+            {
+                $xtpl->assign( 'YAHOO', $y );
+                if ( $k ) $xtpl->parse( 'main.yahoo.item.comma' );
+                $xtpl->parse( 'main.yahoo.item' );
+            }                        
             $xtpl->parse( 'main.yahoo' );
         }
 
         if ( ! empty( $row['skype'] ) )
         {
+            $ss = array_map( "trim", explode( ",", $row['skype'] ) );
+            foreach ( $ss as $k => $s )
+            {
+                $xtpl->assign( 'SKYPE', $s );
+                if ( $k ) $xtpl->parse( 'main.skype.item.comma' );
+                $xtpl->parse( 'main.skype.item' );
+            }
             $xtpl->parse( 'main.skype' );
         }
         $xtpl->parse( 'main' );
