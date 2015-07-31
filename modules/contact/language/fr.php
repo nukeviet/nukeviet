c5649897ffc02d182aae21bcade1422097ce1614
VuThao <vuthao27@gmail.com>
2015-07-31 18:38:33 +0700

    update Language Français. Thank Nguyễn Phú Thành

--- modules/contact/language/fr.php
+++ modules/contact/language/fr.php
@@ -14,7 +14,7 @@ if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 $lang_translator['author'] = 'Phạm Chí Quang';
 $lang_translator['createdate'] = '21/6/2010, 19:30';
 $lang_translator['copyright'] = '@Copyright (C) 2010 VINADES.,JSC. Tous droits réservés.';
-$lang_translator['info'] = 'Langue française pour NukeViet 3';
+$lang_translator['info'] = 'Langue française pour NukeViet 4';
 $lang_translator['langtype'] = 'lang_module';
 
 $lang_module['fullname'] = 'Nom complet';
@@ -33,3 +33,10 @@ $lang_module['sendcontactok'] = 'Merci de votre message! Nous allons étudier et
 $lang_module['sendinfo'] = 'Ce message a été envoyé par l\'outil Contact du site %1$s.<br />Expéditeur<br /><strong>Nom complet</strong>: %2$s<br /><strong>E-mail</strong>: %3$s<br /><strong>IP</strong>: %4$s<br /><strong>Destinataire</strong>: %5$s';
 $lang_module['sendinfo2'] = 'Ce message est envoyé par la fonction "Contact" du site %1$s.<br />Coordonnées de l’expéditeur<br /><strong>Nom complet</strong>: %2$s<br /><strong>E-mail</strong>: %3$s<br /><strong>Télephone</strong>: %4$s<br /><strong>IP</strong>: %5$s<br /><strong>Destinataire</strong>: %6$s';
 $lang_module['phone'] = 'Téléphone';
+$lang_module['note'] = 'Remplir toutes les informations en dessous avant d\'envoyer les mail pour qu\'on puisse vous répondre facilement';
+$lang_module['yahoo'] = 'Yahoo';
+$lang_module['skype'] = 'Skype';
+$lang_module['reset'] = 'Re-Saisir';
+$lang_module['fax'] = 'Fax';
+$lang_module['note_s'] = 'Note';
+$lang_module['department'] = 'Département';
--- modules/seek/language/fr.php
+++ modules/seek/language/fr.php
@@ -6,15 +6,15 @@
 * @Copyright (C) 2015 VINADES.,JSC. All rights reserved
 * @Language Français
 * @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
-* @Createdate Jul 27, 2015, 03:22:34 AM
+* @Createdate Jul 31, 2015, 09:30:00 AM
 */
 
 if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
-$lang_translator['author'] = '';
-$lang_translator['createdate'] = '';
-$lang_translator['copyright'] = '';
-$lang_translator['info'] = 'Language translated from http://translate.nukeviet.vn';
+$lang_translator['author'] = 'Nguyễn Phú Thành';
+$lang_translator['createdate'] = '31/07/2015, 16:30';
+$lang_translator['copyright'] = 'phuthanh.nguyen215@gmail.com';
+$lang_translator['info'] = '';
 $lang_translator['langtype'] = 'lang_module';
 
 $lang_module['main_title'] = 'Recherche';
@@ -37,3 +37,4 @@ $lang_module['logic_and'] = 'Cette expression exacte';
 $lang_module['logic_or'] = 'Au moins un mot';
 $lang_module['searchQueryError'] = 'Mots clés doit avoir au minimum %s caractères';
 $lang_module['chooseModule'] = 'Sélectionnez la section de recherche';
+$lang_module['pages_title'] = 'Page';
