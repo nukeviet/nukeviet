933e34594ff7e60a9b982fe5354d0d581b0fa7a1
Hồ Ngọc Triển <hongoctrien@vinades.vn>
2015-08-22 16:19:14 +0700

    Fix search generate page

--- modules/shops/funcs/search_result.php
+++ modules/shops/funcs/search_result.php
@@ -209,14 +209,17 @@ $table_exchange = " LEFT JOIN " . $db_config['prefix'] . "_" . $module_data . "_
 $table_exchange1 = " INNER JOIN " . $db_config['prefix'] . "_" . $module_data . "_catalogs t3 ON t3.catid = t1.listcatid";
 
 // Fetch Limit
-$db->sqlreset()->select( 'COUNT(*)' )->from( $table_search . " " . $table_exchange . " " . $table_exchange1 )->where( "t1.status =1 " . $search . " " . $show_price );
+$db->sqlreset()->select( 'DISTINCT COUNT(*)' )->from( $table_search . " " . $table_exchange . " " . $table_exchange1 )->where( "t1.status =1 " . $search . " " . $show_price );
+$sth = $db->prepare( $db->sql( ) );
+$sth->execute( );
+
+$num_items = $sth->fetchColumn( );
 
 $db->select( "DISTINCT t1.id, t1.listcatid, t1.publtime, t1." . NV_LANG_DATA . "_title, t1." . NV_LANG_DATA . "_alias, t1." . NV_LANG_DATA . "_hometext, t1.homeimgalt, t1.homeimgfile, t1.homeimgthumb, t1.product_number, t1.product_price, t1.discount_id, t1.money_unit, t1.showprice, t1." . NV_LANG_DATA . "_gift_content, t1.gift_from, t1.gift_to, t3.newday, t2.exchange " . $sql_i )
 	->order( $order_by )
 	->limit( $per_page )
 	->offset( ( $page - 1 ) * $per_page );
 $result = $db->query( $db->sql() );
-$num_items = $result->rowCount();
 
 $base_url = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=search_result&keyword=' . $keyword . '&price1=' . $price1 . '&price2=' . $price2 . '&typemoney=' . $typemoney . '&cata=' . $cataid . $url;
 $html_pages = nv_generate_page( $base_url, $num_items, $per_page, $page );
