d060b82000142845e4203d67383164d81fa84fa6
Hồ Ngọc Triển <hongoctrien@vinades.vn>
2015-10-19 14:06:50 +0700

    Bổ sung code php kiểm tra trùng người ký khi thêm văn bản mới

--- modules/laws/admin/main.php
+++ modules/laws/admin/main.php
@@ -133,8 +133,16 @@ else
 	        $post['sgid'] = $nv_Request->get_title( 'sgid', 'post', '' );
 			if( !is_numeric( $post['sgid'] ) and !empty( $post['sgid'] ) )
 			{
-				$sql = 'INSERT INTO ' . NV_PREFIXLANG . '_' . $module_data . '_signer(title, addtime) VALUES(' . $db->quote( $post['sgid'] ) . ', ' . NV_CURRENTTIME . ')';
-				$post['sgid'] = $db->insert_id( $sql );
+				$result = $db->query( "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_signer WHERE title=" . $db->quote( $post['title'] ) . " AND offices='' AND positions=''" );
+				if ( $result->rowCount() == 0 )
+				{
+					$sql = 'INSERT INTO ' . NV_PREFIXLANG . '_' . $module_data . '_signer(title, addtime) VALUES(' . $db->quote( $post['sgid'] ) . ', ' . NV_CURRENTTIME . ')';
+					$post['sgid'] = $db->insert_id( $sql );
+				}
+				else
+				{
+					$post['sgid'] = $result->fetchColumn();
+				}
 			}
 			else
 			{
