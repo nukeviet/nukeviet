7e085a9f5ff2c6137a9be76a9d2d76ce0b4f0da0
Hồ Ngọc Triển <hongoctrien@vinades.vn>
2016-02-01 16:10:46 +0700

    Ckeditor: Update bvtdownload plugin

--- assets/editors/ckeditor/plugins/tbvdownload/lang/vi.js
+++ assets/editors/ckeditor/plugins/tbvdownload/lang/vi.js
@@ -9,7 +9,7 @@ CKEDITOR.plugins.setLang('tbvdownload', 'vi', {
 		toolbar	: 'Dán HTMl để tải ảnh về máy chủ',
         tooltip : 'Dán bài viết để tải hình ảnh về máy chủ',
 		content_html: 'Nội dung cần tải hình ảnh',
-		url_path_save: 'Đường dẫn lưu trữ hình ảnh (Đường dẫn lưu trữ sẽ ở phía trong phần module)',
+		url_path_save: 'Đường dẫn lưu trữ hình ảnh',
         pasteMsg: 'Hãy dán nội dung vào trong khung bên dưới, sử dụng tổ hợp phím (<STRONG>Ctrl/Cmd+V</STRONG>) và nhấn vào nút <STRONG>Đồng ý</STRONG>.',
         info_action : 'Hãy dán nội dung vào trong khung bên dưới, sử dụng tổ hợp phím (Ctrl/Cmd+V) và nhấn vào nút Đồng ý.'
 });
