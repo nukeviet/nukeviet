b35c1b40a6d5070b80ef2126ae00ef752630731e
Vu Thao <vuthao27@gmail.com>
2016-02-21 11:11:22 +0700

    Fix setup for  webserver Apache 8.1.0
    Run SQL to update database:
    ALTER TABLE `nv4_setup_extensions` CHANGE `virtual` `is_virtual`
    TINYINT(1) NOT NULL DEFAULT '0';

--- admin/modules/vmodule.php
+++ admin/modules/vmodule.php
@@ -39,7 +39,7 @@ if ($nv_Request->get_title('checkss', 'post') == md5(session_id() . 'addmodule')
         $module_data = preg_replace('/(\W+)/i', '_', $title);
         if (empty($array_site_cat_module) or in_array($modfile, $array_site_cat_module)) {
             try {
-                $sth = $db->prepare('INSERT INTO ' . $db_config['prefix'] . '_setup_extensions (type, title, is_sys, virtual, basename, table_prefix, version, addtime, author, note) VALUES ( \'module\', :title, 0, 0, :basename, :table_prefix, :version, ' . NV_CURRENTTIME . ', :author, :note)');
+                $sth = $db->prepare('INSERT INTO ' . $db_config['prefix'] . '_setup_extensions (type, title, is_sys, is_virtual, basename, table_prefix, version, addtime, author, note) VALUES ( \'module\', :title, 0, 0, :basename, :table_prefix, :version, ' . NV_CURRENTTIME . ', :author, :note)');
                 $sth->bindParam(':title', $title, PDO::PARAM_STR);
                 $sth->bindParam(':basename', $modfile, PDO::PARAM_STR);
                 $sth->bindParam(':table_prefix', $module_data, PDO::PARAM_STR);
@@ -57,7 +57,6 @@ if ($nv_Request->get_title('checkss', 'post') == md5(session_id() . 'addmodule')
         }
     }
 }
-
 
 $page_title = $lang_module['vmodule_add'];
 
@@ -78,7 +77,7 @@ $xtpl->assign('CHECKSS', md5(session_id() . 'addmodule'));
 $xtpl->assign('TITLE', $title);
 $xtpl->assign('NOTE', $note);
 
-$sql = 'SELECT title FROM ' . $db_config['prefix'] . '_setup_extensions WHERE virtual=1 AND type=\'module\' ORDER BY addtime ASC';
+$sql = 'SELECT title FROM ' . $db_config['prefix'] . '_setup_extensions WHERE is_virtual=1 AND type=\'module\' ORDER BY addtime ASC';
 $result = $db->query($sql);
 
 while (list($modfile_i) = $result->fetch(3)) {
