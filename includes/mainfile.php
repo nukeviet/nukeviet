726069a8af9d78392b33dba0f1a3b9a16f1c2c6a
Nguyen Anh Tu <anhtu@vinades.vn>
2016-04-20 15:36:35 +0700

    Update mainfile.php

--- includes/mainfile.php
+++ includes/mainfile.php
@@ -121,6 +121,11 @@ require NV_ROOTDIR . '/includes/core/filesystem_functions.php';
 require NV_ROOTDIR . '/includes/functions.php';
 require NV_ROOTDIR . '/includes/core/theme_functions.php';
 
+if ($global_config['cached'] == 'memcached') {
+    ini_set('session.save_handler', 'memcached');
+    ini_set('session.save_path', NV_MEMCACHED_HOST . ':' . NV_MEMCACHED_PORT);
+}
+
 // IP Ban
 if (nv_is_banIp(NV_CLIENT_IP)) {
     trigger_error('Hi and Good-bye!!!', 256);
@@ -182,8 +187,6 @@ if (!in_array(NV_SERVER_NAME, $domains)) {
 }
 // Ket noi Cache
 if ($global_config['cached'] == 'memcached') {
-    ini_set('session.save_handler', 'memcached');
-    ini_set('session.save_path', NV_MEMCACHED_HOST . ':' . NV_MEMCACHED_PORT);
     $nv_Cache = new NukeViet\Cache\Memcacheds(NV_MEMCACHED_HOST, NV_MEMCACHED_PORT, NV_LANG_DATA, NV_CACHE_PREFIX);
 } else {
     $nv_Cache = new NukeViet\Cache\Files(NV_ROOTDIR . '/' . NV_CACHEDIR, NV_LANG_DATA, NV_CACHE_PREFIX);
