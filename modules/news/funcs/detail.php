a17284967468f09ad9b7574fb5d7c0e51b270de6
Vu Thao <vuthao27@gmail.com>
2016-05-16 01:17:46 +0700

    news:  database optimization update: https://github.com/nukeviet/nukeviet/wiki/Ghi-ch%C3%BA-ph%C3%A1t-tri%E1%BB%83n-NukeViet-4.0.29#c%E1%BA%ADp-nh%E1%BA%ADt-csdl-module-tin-t%E1%BB%A9c

--- modules/news/funcs/detail.php
+++ modules/news/funcs/detail.php
@@ -31,7 +31,7 @@ if (nv_user_in_groups($global_array_cat[$catid]['groups_view'])) {
             $canonicalUrl = $base_url_rewrite;
         }
 
-        $body_contents = $db_slave->query('SELECT bodyhtml as bodytext, sourcetext, imgposition, copyright, allowed_send, allowed_print, allowed_save, gid FROM ' . NV_PREFIXLANG . '_' . $module_data . '_bodyhtml_' . ceil($news_contents['id'] / 2000) . ' where id=' . $news_contents['id'])->fetch();
+        $body_contents = $db_slave->query('SELECT bodyhtml, sourcetext, imgposition, copyright, allowed_send, allowed_print, allowed_save, gid FROM ' . NV_PREFIXLANG . '_' . $module_data . '_detail where id=' . $news_contents['id'])->fetch();
         $news_contents = array_merge($news_contents, $body_contents);
         unset($body_contents);
 
