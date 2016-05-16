27bf11e5327dfda12d6837fc1e7c238c08efaa93
Phan Tan Dung <phantandung92@gmail.com>
2016-05-16 09:48:19 +0700

    Fix theme admin

--- themes/admin_default/js/main.js
+++ themes/admin_default/js/main.js
@@ -2,114 +2,117 @@ var myTimerPage = '';
 var myTimersecField = '';
 
 function timeoutsesscancel() {
-	clearInterval(myTimersecField);
-	$.ajax({
-		url : nv_base_siteurl + 'index.php?second=statimg',
-		cache : false
-	}).done(function() {
-		$("#timeoutsess").hide();
-		myTimerPage = setTimeout(function() {
-			timeoutsessrun();
-		}, nv_check_pass_mstime);
-	});
+    clearInterval(myTimersecField);
+    $.ajax({
+        url: nv_base_siteurl + 'index.php?second=statimg',
+        cache: false
+    }).done(function() {
+        $("#timeoutsess").hide();
+        myTimerPage = setTimeout(function() {
+            timeoutsessrun();
+        }, nv_check_pass_mstime);
+    });
 }
 
 function timeoutsessrun() {
-	clearInterval(myTimerPage);
-	var Timeout = 60;
-	document.getElementById('secField').innerHTML = Timeout;
-	jQuery("#timeoutsess").show();
-	var msBegin = new Date().getTime();
-	myTimersecField = setInterval(function() {
-		var msCurrent = new Date().getTime();
-		var ms = Timeout - Math.round((msCurrent - msBegin) / 1000);
-		if (ms >= 0) {
-			document.getElementById('secField').innerHTML = ms;
-		} else if (ms < -3) {
-			clearInterval(myTimersecField);
-			$(window).unbind();
-			window.location.reload();
-		}
-	}, 1000);
+    clearInterval(myTimerPage);
+    var Timeout = 60;
+    document.getElementById('secField').innerHTML = Timeout;
+    jQuery("#timeoutsess").show();
+    var msBegin = new Date().getTime();
+    myTimersecField = setInterval(function() {
+        var msCurrent = new Date().getTime();
+        var ms = Timeout - Math.round((msCurrent - msBegin) / 1000);
+        if (ms >= 0) {
+            document.getElementById('secField').innerHTML = ms;
+        } else if (ms < -3) {
+            clearInterval(myTimersecField);
+            $(window).unbind();
+            window.location.reload();
+        }
+    }, 1000);
 }
 
 // ModalShow
 function modalShow(a, b) {
-	"" == a && (a = "&nbsp;");
-	$("#sitemodal").find(".modal-title").html(a);
-	$("#sitemodal").find(".modal-body").html(b);
-	$("#sitemodal").modal()
+    "" == a && (a = "&nbsp;");
+    $("#sitemodal").find(".modal-title").html(a);
+    $("#sitemodal").find(".modal-body").html(b);
+    $("#sitemodal").modal()
 }
 
 var NV = {
-	menuBusy: false,
-	menuTimer: null,
-	menu: null,
-	openMenu: function(menu){
-		this.menuBusy = true;
-		this.menu = $(menu);
-		this.menuTimer = setTimeout( function(){ NV.menu.addClass('open'); }, 300 );
-	},
-	closeMenu: function(menu){
-		clearTimeout( this.menuTimer );
-		this.menuBusy = false;
-		this.menu = $(menu).removeClass('open');
-	},
-	fixContentHeight: function(){
-		var wrap = $('.nvwrap');
-		var vmenu = $('#left-menu');
+    menuBusy: false,
+    menuTimer: null,
+    menu: null,
+    openMenu: function(menu) {
+        this.menuBusy = true;
+        this.menu = $(menu);
+        this.menuTimer = setTimeout(function() {
+            NV.menu.addClass('open');
+        }, 300);
+    },
+    closeMenu: function(menu) {
+        clearTimeout(this.menuTimer);
+        this.menuBusy = false;
+        this.menu = $(menu).removeClass('open');
+    },
+    fixContentHeight: function() {
+        var wrap = $('.nvwrap');
+        var vmenu = $('#left-menu');
 
-		if( wrap.length > 0 ){
-			if( wrap.height() < vmenu.height() + vmenu.offset().top && vmenu.is(':visible') ){
-				wrap.css('min-height', ( vmenu.height() + vmenu.offset().top ) + 'px' )
-			}else{
-				wrap.css('min-height', '100%');
-			}
-		}
-	}
+        if (wrap.length > 0) {
+            wrap.css('min-height', '100%');
+            if (wrap.height() < vmenu.height() + vmenu.offset().top && vmenu.is(':visible')) {
+                wrap.css('min-height', (vmenu.height() + vmenu.offset().top) + 'px')
+            } else {
+                wrap.css('min-height', '100%');
+            }
+        }
+    }
 };
 
-$(document).ready(function(){
-	// Control content height
-	NV.fixContentHeight();
-	$(window).resize(function(){
-		NV.fixContentHeight();
-	});
+$(document).ready(function() {
+    // Control content height
+    NV.fixContentHeight();
+    $(window).resize(function() {
+        NV.fixContentHeight();
+    });
 
-	// Show submenu
-	$('#menu-horizontal .dropdown, #left-menu .dropdown:not(.active)').hover(function(){
-		NV.openMenu(this);
-	}, function(){
-		NV.closeMenu(this);
-	});
+    // Show submenu
+    $('#menu-horizontal .dropdown, #left-menu .dropdown:not(.active)').hover(function() {
+        NV.openMenu(this);
+    }, function() {
+        NV.closeMenu(this);
+    });
 
-	// Left menu handle
-	$('#left-menu-toggle').click(function(){
-		if( $('#left-menu').is(':visible') ){
-			$('#left-menu, #left-menu-bg, #container, #footer').removeClass('open');
-		}else{
-			$('#left-menu, #left-menu-bg, #container, #footer').addClass('open');
-		}
-		NV.fixContentHeight();
-	});
+    // Left menu handle
+    $('#left-menu-toggle').click(function() {
+        if ($('#left-menu').is(':visible')) {
+            $('#left-menu, #left-menu-bg, #container, #footer').removeClass('open');
+        } else {
+            $('#left-menu, #left-menu-bg, #container, #footer').addClass('open');
+        }
+        NV.fixContentHeight();
+    });
 
-	// Show admin confirm
-	myTimerPage = setTimeout(function() {
-		timeoutsessrun();
-	}, nv_check_pass_mstime);
+    // Show admin confirm
+    myTimerPage = setTimeout(function() {
+        timeoutsessrun();
+    }, nv_check_pass_mstime);
 
-	// Show confirm message on leave, reload page
-	$('form.confirm-reload').change(function() {
-		$(window).bind('beforeunload', function() {
-			return nv_msgbeforeunload;
-		});
-	});
+    // Show confirm message on leave, reload page
+    $('form.confirm-reload').change(function() {
+        $(window).bind('beforeunload', function() {
+            return nv_msgbeforeunload;
+        });
+    });
 
-	// Disable confirm message on submit form
-	$('form').submit(function() {
-		$(window).unbind();
-	});
+    // Disable confirm message on submit form
+    $('form').submit(function() {
+        $(window).unbind();
+    });
 
-	// Bootstrap tooltip
-	$('[data-toggle="tooltip"]').tooltip();
+    // Bootstrap tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 });
