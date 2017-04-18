2f4f03680b05cfc8876a1283d6462820aacf79d0
VINADES.,JSC <contact@vinades.vn>
2017-04-18 15:42:28 +0700

    page: thay đổi cơ sở dữ liệu cho cấu hình copy bài viết, câu lệnh thêm mới INSERT INTO `nv4_vi_about_config` (`config_name`, `config_value`) VALUES ('copy_page', '0');

--- modules/page/action_mysql.php
+++ modules/page/action_mysql.php
@@ -55,7 +55,8 @@ $sql_create_module[] = "INSERT INTO " . $db_config['prefix'] . "_" . $lang . "_"
 ('facebookapi', ''),
 ('per_page', '20'),
 ('news_first', '0'),
-('related_articles', '5')";
+('related_articles', '5'),
+('copy_page', '0')";
 
 // Comments
 $sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $module_name . "', 'auto_postcomm', '1')";
@@ -69,4 +70,3 @@ $sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module,
 $sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $module_name . "', 'captcha', '1')";
 $sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $module_name . "', 'perpagecomm', '5')";
 $sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $module_name . "', 'timeoutcomm', '360')";
-$sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $module_name . "', 'copy_page', '0')";
