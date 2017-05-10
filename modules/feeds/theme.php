6e3a1c0e6bb6465a2287788dfc5ae69ad470a80f
VINADES.,JSC <contact@vinades.vn>
2017-05-10 14:53:49 +0700

    Avoid unused local variables such as '$module_name'.

--- modules/feeds/theme.php
+++ modules/feeds/theme.php
@@ -20,7 +20,7 @@ if (!defined('NV_IS_MOD_RSS')) {
  */
 function nv_rss_main_theme($array)
 {
-    global $module_name, $module_info;
+    global $module_info;
 
     $array .= '<div class="tree well"><ul>';
     $array .= nv_get_rss_link();
