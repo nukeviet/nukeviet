1639acfdf63fa04337848033b455b36bdf5a6508
VINADES.,JSC <contact@vinades.vn>
2017-05-11 09:24:33 +0700

    Avoid unused local variables such as '$module_name'.

--- themes/mobile_default/modules/news/theme.php
+++ themes/mobile_default/modules/news/theme.php
@@ -166,7 +166,7 @@ function viewcat_list_new($array_catpage, $catid, $page, $generate_page)
  */
 function viewcat_page_new($array_catpage, $array_cat_other, $generate_page)
 {
-    global $global_array_cat, $module_name, $module_upload, $lang_module, $module_config, $module_info, $global_array_cat, $catid, $page;
+    global $global_array_cat, $module_upload, $lang_module, $module_config, $module_info, $global_array_cat, $catid, $page;
 
     $xtpl = new XTemplate('viewcat_page.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_info['module_theme']);
     $xtpl->assign('LANG', $lang_module);
