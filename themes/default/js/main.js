650d93811f05423e82147220a4f00d536f4ff07b
Nguyen Anh Tu <anhtu@vinades.vn>
2017-06-10 23:15:25 +0700

    Preload: link rel="prefetch", link rel="prerender"
    https://www.smashingmagazine.com/2016/02/preload-what-is-it-good-for/

--- themes/default/js/main.js
+++ themes/default/js/main.js
@@ -444,6 +444,14 @@ function showSubBreadcrumbs(a, b) {
     });
 }
 
+function add_hint(type, url) {
+	if (!type || !url) return;
+	var el = document.createElement("link");
+	el.setAttribute("rel", type);
+	el.setAttribute("href", url);
+	document.getElementsByTagName("head")[0].appendChild(el)
+}
+
 var reCaptchaLoadCallback = function() {
     for (i = 0, j = nv_recaptcha_elements.length; i < j; i++) {
         var ele = nv_recaptcha_elements[i];
@@ -607,7 +615,14 @@ $(function() {
     //Change Localtion
     $("[data-location]").on("click", function() {
         locationReplace($(this).data("location"))
-    })
+    });
+    //Add preload: link rel="prefetch", link rel="prerender"
+    $(document).bind("mousemove", function(e) {
+    	if (!e.target.href || e.target.href.indexOf(location.host) == -1 || e.target.hintAdded) return;
+        add_hint("prefetch", e.target.href);
+    	add_hint("prerender", e.target.href);
+    	e.target.hintAdded = true
+    });
 });
 // Fix bootstrap multiple modal
 $(document).on({
