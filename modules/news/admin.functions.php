5bee0d6030daffe668f8fcdd00d72c5741ea5b88
VINADES.,JSC <contact@vinades.vn>
2017-08-03 15:10:28 +0700

    add link document moduel users

--- modules/news/admin.functions.php
+++ modules/news/admin.functions.php
@@ -51,6 +51,7 @@ $array_allowed_comm = array(
 define('NV_IS_FILE_ADMIN', true);
 require_once NV_ROOTDIR . '/modules/' . $module_file . '/global.functions.php';
 
+//Document
 $array_url_instruction['main'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:news';
 $array_url_instruction['cat'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:news#quản_ly_chuyen_mục';
 $array_url_instruction['content'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:news#them_bai_viet';
