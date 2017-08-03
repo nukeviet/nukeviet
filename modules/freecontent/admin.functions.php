03ed22432a9792a8df09e36c695df3c6902acc79
VINADES.,JSC <contact@vinades.vn>
2017-08-03 15:29:20 +0700

    add link document module freecontent

--- modules/freecontent/admin.functions.php
+++ modules/freecontent/admin.functions.php
@@ -15,3 +15,7 @@ if (! defined('NV_ADMIN') or ! defined('NV_MAINFILE') or ! defined('NV_IS_MODADM
 $allow_func = array( 'main', 'list', 'manager' );
 
 define('NV_IS_FILE_ADMIN', true);
+
+//Document
+$array_url_instruction['main'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:freecontent';
+$array_url_instruction['list'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:freecontent:list';
