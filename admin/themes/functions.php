d570e3353d284daf5540ff79c994bfa8f532ad35
VINADES.,JSC <contact@vinades.vn>
2017-08-04 11:10:22 +0700

    add link document themes

--- admin/themes/functions.php
+++ admin/themes/functions.php
@@ -20,3 +20,11 @@ $menu_top = array(
 );
 
 define('NV_IS_FILE_THEMES', true);
+
+//Document
+$array_url_instruction['main'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:themes';
+$array_url_instruction['config'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:themes:config';
+$array_url_instruction['setuplayout'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:themes:setuplayout';
+$array_url_instruction['blocks'] = 'https://wiki.nukeviet.vn/nukeviet4:admin:themes:blocks';
+$array_url_instruction['xcopyblock'] = 'https://wiki.nukeviet.vn/themes:xcopyblock';
+$array_url_instruction['package_theme_module'] = 'https://wiki.nukeviet.vn/themes:package_theme_module';
