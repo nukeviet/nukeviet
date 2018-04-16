310356a9efb72268e3a2b3baa28ca39d8cb5cfea
VINADES.,JSC <contact@vinades.vn>
2018-04-16 17:17:34 +0700

    Browse to file location from ckeditor

--- assets/editors/ckeditor/nv.php
+++ assets/editors/ckeditor/nv.php
@@ -28,9 +28,71 @@ function nv_aleditor($textareaname, $width = '100%', $height = '450px', $val = '
         define('CKEDITOR', true);
         $return .= '<script type="text/javascript" src="' . NV_BASE_SITEURL . NV_EDITORSDIR . '/ckeditor/ckeditor.js?t=' . $global_config['timestamp'] . '"></script>';
         $return .= '<script type="text/javascript">CKEDITOR.timestamp=CKEDITOR.timestamp+' . $global_config['timestamp'] . ';</script>';
+        if (defined('NV_IS_ADMIN')) {
+            $return .= "<script type=\"text/javascript\">
+            CKEDITOR.on('dialogDefinition', function(e) {
+                if (e.data.name == 'image2' || e.data.name == 'video' || e.data.name == 'flash' || e.data.name == 'googledocs' || e.data.name == 'link') {
+                    var contents;
+                    if (e.data.name == 'googledocs') {
+                        contents = e.data.definition.getContents('settingsTab');
+                    } else {
+                        contents = e.data.definition.getContents('info');
+                    }
+                    var dialogID = e.data.definition.dialog.parts.contents.$.id;
+                    var fileButtons = new Array();
+                    var textDOMOffset = -1;
+                    $.each(contents.elements, function(k, v) {
+                        if (v.type == 'text') {
+                            textDOMOffset++;
+                        } else if (v.type == 'vbox1') {
+                            $.each(v.children[0].children, function(sk, sv) {
+                                if (sv.type == 'text') {
+                                    textDOMOffset++;
+                                } else if (sv.type == 'button') {
+                                    fileButtons.push([textDOMOffset, sv]);
+                                }
+                            });
+                        } else if (v.type == 'hbox' || v.type == 'vbox') {
+                            $.each(v.children, function(sk, sv) {
+                                if (sv.type == 'text') {
+                                    textDOMOffset++;
+                                } else if (sv.type == 'button') {
+                                    fileButtons.push([textDOMOffset, sv]);
+                                }
+                                if (typeof sv.children == 'object') {
+                                    $.each(sv.children, function(ssk, ssv) {
+                                        if (ssv.type == 'text') {
+                                            textDOMOffset++;
+                                        } else if (ssv.type == 'button') {
+                                            fileButtons.push([textDOMOffset, ssv]);
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                    $.each(fileButtons, function(k, v) {
+                        var btn = v[1];
+                        if (typeof btn.filebrowser != 'undefined') {
+                            var offset = v[0];
+                            var orgclickevent = btn.onClick;
+                            btn.onClick = function(type, element) {
+                                var textInputs = $('#' + dialogID).find('input[type=text].cke_dialog_ui_input_text');
+                                var input = $(textInputs[offset]);
+                                if (input.length == 1) {
+                                    btn.filebrowser.url = btn.filebrowser.url.replace(/\&currentfile\=.*?$/g, '');
+                                    btn.filebrowser.url = btn.filebrowser.url + '&currentfile=' + encodeURIComponent(input.val());
+                                }
+                                orgclickevent.call(this, type, element);
+                            };
+                        }
+                    });
+                }
+            });
+            </script>";
+        }
     }
-    $return .= "<script type=\"text/javascript\">
-			CKEDITOR.replace( '" . $module_data . "_" . $textareaname . "', {" . (!empty($customtoolbar) ? 'toolbar : "' . $customtoolbar . '",' : '') . " width: '" . $width . "',height: '" . $height . "',";
+    $return .= "<script type=\"text/javascript\">CKEDITOR.replace( '" . $module_data . "_" . $textareaname . "', {" . (!empty($customtoolbar) ? 'toolbar : "' . $customtoolbar . '",' : '') . " width: '" . $width . "',height: '" . $height . "',";
     $return .= "contentsCss: '" . NV_BASE_SITEURL . NV_EDITORSDIR . "/ckeditor/nv.css?t=" . $global_config['timestamp'] . "',";
 
     if (defined('NV_IS_ADMIN')) {
