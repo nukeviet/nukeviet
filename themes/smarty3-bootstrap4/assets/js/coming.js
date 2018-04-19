a5f1ea4d80e3e189f67a99d1f22392aefe2cc80c
VINADES.,JSC <contact@vinades.vn>
2018-04-19 14:54:25 +0700

    Thêm file mục giao diện: assets

---
+++ themes/smarty3-bootstrap4/assets/js/coming.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#ms-countdown").countdown("2018/04/01",function(l){$(this).html(l.strftime('<ul class="coming-date coming-date-black"><li>%D <span>Days</span></li><li class="colon">:</li><li>%H <span>Hours</span></li><li class="colon">:</li><li>%M <span>Minutes</span></li><li class="colon">:</li><li>%S <span>Seconds</span></li></ul>'))})});
---
+++ themes/smarty3-bootstrap4/assets/js/index.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".owl-carousel").owlCarousel({loop:!0,margin:30,autoplay:!0,autoplayHoverPause:!0,slideBy:3,mouseDrag:!1,dotsContainer:".owl-dots",responsive:{0:{items:1},768:{items:2},992:{items:2},1200:{items:3}}})});
