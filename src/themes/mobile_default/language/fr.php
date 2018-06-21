b64c06008dad6a2a51079daa5c8a010e858f9eaa
VINADES.,JSC <contact@vinades.vn>
2018-06-21 14:08:56 +0700

    Update lang theme

--- src/themes/mobile_default/language/fr.php
+++ src/themes/mobile_default/language/fr.php
@@ -9,15 +9,15 @@
 * @Createdate Jan 31, 2018, 01:08:00 AM
 */
 
-$lang_block['qr_level'] = 'Qualité des images';
-$lang_block['qr_pixel_per_point'] = 'Taille de QR-code ';
-$lang_block['qr_outer_frame'] = 'Bord de QR-code';
-$lang_block['cominfo_map_guide_title'] = 'Guide de construire la carte de Google';
-$lang_block['cominfo_map_guide_content'] = '<ul><li>Saisissez l\'adresse dans la case, choisir une parmi les adresse dans la liste des adresses qui est compatible à Google MAP.</li><li>Vous pouvez cliquez sur la carte pour cocher l\'adresse que vous voulez</li><li>Kéo thả chuột trên điểm đánh dấu để di chuyển nó phù hợp với tọa độ địa chỉ công ty của bạn</li><li>Long clique au dessus et deplacer le pointeur de la souris pour deplacer la carte, tourner la roulette pour modifier la taille de la carte</li></ul>';
-$lang_block['cominfo_map_yes'] = 'Activez Google MAP';
-$lang_block['cominfo_map_no'] = 'Désactivew Google MAP';
-$lang_block['cominfo_map_key'] = 'Clé API';
-$lang_block['cominfo_map_key_guide'] = 'Guide pour obtenir la clé API';
+$lang_module['qr_level'] = 'Qualité des images';
+$lang_module['qr_pixel_per_point'] = 'Taille de QR-code ';
+$lang_module['qr_outer_frame'] = 'Bord de QR-code';
+$lang_module['cominfo_map_guide_title'] = 'Guide de construire la carte de Google';
+$lang_module['cominfo_map_guide_content'] = '<ul><li>Saisissez l\'adresse dans la case, choisir une parmi les adresse dans la liste des adresses qui est compatible à Google MAP.</li><li>Vous pouvez cliquez sur la carte pour cocher l\'adresse que vous voulez</li><li>Kéo thả chuột trên điểm đánh dấu để di chuyển nó phù hợp với tọa độ địa chỉ công ty của bạn</li><li>Long clique au dessus et deplacer le pointeur de la souris pour deplacer la carte, tourner la roulette pour modifier la taille de la carte</li></ul>';
+$lang_module['cominfo_map_yes'] = 'Activez Google MAP';
+$lang_module['cominfo_map_no'] = 'Désactivew Google MAP';
+$lang_module['cominfo_map_key'] = 'Clé API';
+$lang_module['cominfo_map_key_guide'] = 'Guide pour obtenir la clé API';
 
 $lang_module['general'] = 'Configuration générale';
 $lang_module['allpages'] = 'Toute la page';
