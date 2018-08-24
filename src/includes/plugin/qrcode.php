3d02094a3ff2f8ccc2d865d5f9f0fbefd95a2859
VINADES.,JSC <contact@vinades.vn>
2018-08-24 14:31:29 +0700

    Fix qrcode

--- src/includes/plugin/qrcode.php
+++ src/includes/plugin/qrcode.php
@@ -7,7 +7,8 @@
  * @License GNU/GPL version 2 or any later version
  * @Createdate 12/29/2009 20:7
  */
-if (! defined('NV_MAINFILE')) {
+
+if (!defined('NV_MAINFILE')) {
     die('Stop!!!');
 }
 
@@ -20,21 +21,23 @@ nv_add_hook($module_name, 'get_qr_code', $priority, function($vars) {
         $outer_frame = $nv_Request->get_int('of', 'get', 1);
 
         $_ErrorCorrection = array(
-            'L' => 'low',
-            'M' => 'medium',
-            'Q' => 'quartile',
-            'H' => 'high'
+            'L' => Endroid\QrCode\ErrorCorrectionLevel::LOW,
+            'M' => Endroid\QrCode\ErrorCorrectionLevel::MEDIUM,
+            'Q' => Endroid\QrCode\ErrorCorrectionLevel::QUARTILE,
+            'H' => Endroid\QrCode\ErrorCorrectionLevel::HIGH
         );
-        if (! empty($url) and isset($_ErrorCorrection[$level]) and ($ModuleSize > 0 and $ModuleSize < 13) and ($outer_frame > 0 and $outer_frame < 6)) {
+        if (!empty($url) and isset($_ErrorCorrection[$level]) and ($ModuleSize > 0 and $ModuleSize < 13) and ($outer_frame > 0 and $outer_frame < 6)) {
             // Readmore: https://github.com/endroid/QrCode and http://www.qrcode.com/en/about/version.html
-            $qrCode = new Endroid\QrCode\QrCode();
+            $qrCode = new Endroid\QrCode\QrCode($url);
 
-            header('Content-type: image/png');
-            $qrCode->setText($url)
-                ->setErrorCorrection($_ErrorCorrection[$level])
-                ->setModuleSize($ModuleSize)
-                ->setImageType('png')
-                ->render();
+            $qrCode->setSize(200);
+            $qrCode->setWriterByName('png');
+            $qrCode->setMargin($ModuleSize);
+            $qrCode->setEncoding('UTF-8');
+            $qrCode->setErrorCorrectionLevel($_ErrorCorrection[$level]);
+
+            header('Content-Type: '. $qrCode->getContentType());
+            echo $qrCode->writeString();
         }
         exit();
     }
