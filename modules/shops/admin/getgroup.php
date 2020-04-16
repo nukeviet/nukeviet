107ae8afe8891a78f85f0e3c35d63b9814e9c5c9
VINADES.,JSC <contact@vinades.vn>
2020-04-16 10:05:37 +0700

    Sửa lỗi lấy nhóm sản phẩm #641

--- modules/shops/admin/getgroup.php
+++ modules/shops/admin/getgroup.php
@@ -8,16 +8,16 @@
  * @Createdate 04/18/2017 09:47
  */
 
-if (! defined('NV_IS_FILE_ADMIN')) {
+if (!defined('NV_IS_FILE_ADMIN')) {
     die('Stop!!!');
 }
 
 function getgroup_ckhtml($subgroupid_i, $array_groupid_in_row)
 {
-    global $module_name, $global_array_group;
+    global $module_name, $global_array_group, $module_upload;
 
     $contents_temp = '';
-    if (! empty($subgroupid_i)) {
+    if (!empty($subgroupid_i)) {
         foreach ($subgroupid_i as $groupid_i) {
             $data_group = $global_array_group[$groupid_i];
             $ch = '';
@@ -26,7 +26,7 @@ function getgroup_ckhtml($subgroupid_i, $array_groupid_in_row)
             }
 
             $image = '';
-            if (! empty($data_group['image']) and file_exists(NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $data_group['image'])) {
+            if (!empty($data_group['image']) and file_exists(NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $data_group['image'])) {
                 $image = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $data_group['image'];
                 $image = '<img src="' . $image . '" style="margin-top: -3px; max-width: 16px; max-height: 16px" alt="' . $groupinfo_i['title'] . '" />';
             }
@@ -43,11 +43,10 @@ $array_groupid_in_row = array();
 if (!empty($inrow)) {
     $inrow = nv_base64_decode($inrow);
     $array_groupid_in_row = unserialize($inrow);
-}
-else if(!empty($id)) {
-    $_result = $db->query('SELECT group_id FROM ' . $db_config['prefix'] . '_' . $module_data . '_group_items WHERE pro_id = '.$id);
-    while($_row = $_result->fetch()) {
-        $array_groupid_in_row[]=$_row['group_id'];
+} else if (!empty($id)) {
+    $_result = $db->query('SELECT group_id FROM ' . $db_config['prefix'] . '_' . $module_data . '_group_items WHERE pro_id = ' . $id);
+    while ($_row = $_result->fetch()) {
+        $array_groupid_in_row[] = $_row['group_id'];
     }
 }
 $contents_temp_cate = '';
@@ -55,29 +54,26 @@ $contents_temp_cate = '';
 if ($cid > 0) {
     $cid = GetParentCatFilter($cid);
 
-	$arr=array();
+    $arr = array();
     $arr_groupid = array();
     $result = $db->query('SELECT t1.groupid FROM ' . $db_config['prefix'] . '_' . $module_data . '_group t1 INNER JOIN ' . $db_config['prefix'] . '_' . $module_data . '_group_cateid t2 ON t1.groupid = t2.groupid WHERE t2.cateid = ' . $cid);
-   while (list($groupid) = $result->fetch(3)) {
-   		if($global_array_group[$groupid]['parentid']==0)
-   		{
-   			$arr_groupid[$groupid] = GetGroupidInParentGroup($groupid, 0, 1,$cid);
-   		}
-		else {
-			$arr[$groupid]=$groupid;
-		}
+    while (list ($groupid) = $result->fetch(3)) {
+        if ($global_array_group[$groupid]['parentid'] == 0) {
+            $arr_groupid[$groupid] = GetGroupidInParentGroup($groupid, 0, 1, $cid);
+        } else {
+            $arr[$groupid] = $groupid;
+        }
     }
     foreach ($arr_groupid as $key => $value) {
-    	$dataarr=array();
+        $dataarr = array();
         foreach ($arr as $keyar => $valuearr) {
-        	if($value[$valuearr]==$valuearr) $dataarr[$valuearr]=$valuearr;
-   		 }
-		if(!empty($dataarr))
-		{
-			$arr_groupid[$key]=$dataarr;continue;
-
-		}
-
+            if ($value[$valuearr] == $valuearr)
+                $dataarr[$valuearr] = $valuearr;
+        }
+        if (!empty($dataarr)) {
+            $arr_groupid[$key] = $dataarr;
+            continue;
+        }
     }
 
     foreach ($arr_groupid as $groupid_i => $subgroupid_i) {
@@ -96,7 +92,6 @@ if ($cid > 0) {
         $contents_temp_cate .= '</div>';
         $contents_temp_cate .= '</div>';
     }
-
 }
 
 include NV_ROOTDIR . '/includes/header.php';
