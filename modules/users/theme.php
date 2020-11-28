b4f385bfc7f409d4607e997da9692949c74c0bfe
nhoxbin <khoaij123@gmail.com>
2020-11-28 14:23:02 +0700

    first commit

--- modules/users/theme.php
+++ modules/users/theme.php
@@ -972,7 +972,7 @@ function user_welcome()
 
     if (!empty($user_info['avata'])) {
         $xtpl->assign('IMG', array(
-            'src' => $user_info['avata'],
+            'src' => NV_BASE_SITEURL . "{$user_info['photo']}",
             'title' => $lang_module['img_size_title']
         ));
     } else {
