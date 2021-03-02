890c8664b053ed8678d5ad910152c0bb5abdd756
VINADES.,JSC <contact@vinades.vn>
2021-03-02 10:22:09 +0700

    Bổ sung cột hiển thị lượt xem trong quản trị của module about

--- modules/page/admin/main.php
+++ modules/page/admin/main.php
@@ -7,7 +7,6 @@
  * @License GNU/GPL version 2 or any later version
  * @Createdate 24-06-2011 10:35
  */
-
 if (!defined('NV_IS_FILE_ADMIN')) {
     die('Stop!!!');
 }
@@ -56,7 +55,7 @@ foreach ($_rows as $row) {
         $xtpl->parse('main.row.status');
     }
 
-    //Chức năng copy
+    // Chức năng copy
     if ($page_config['copy_page'] == 1) {
         $url_copy = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=content&amp;copy=1&amp;id=' . $row['id'];
         $xtpl->assign('URL_COPY', $url_copy);
@@ -65,7 +64,9 @@ foreach ($_rows as $row) {
 
     $is_excdata = 0;
     if ($global_config['idsite'] > 0 and isset($site_mods['excdata']) and isset($push_content['module'][$module_name]) and $row['status'] == 1) {
-        $count = $db->query('SELECT COUNT(*) FROM ' . NV_PREFIXLANG . '_' . $site_mods['excdata']['module_data'] . '_sended WHERE id_content=' . $row['id'] . ' AND module=' . $db->quote($module_name))->fetchColumn(); {
+        $count = $db->query('SELECT COUNT(*) FROM ' . NV_PREFIXLANG . '_' . $site_mods['excdata']['module_data'] . '_sended WHERE id_content=' . $row['id'] . ' AND module=' . $db->quote($module_name))
+            ->fetchColumn();
+        {
             $is_excdata = 1;
             $row['url_send'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=excdata&amp;' . NV_OP_VARIABLE . '=send&amp;module=' . $module_name . '&amp;id=' . $row['id'];
         }
