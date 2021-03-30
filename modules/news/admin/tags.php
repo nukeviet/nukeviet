5076331c4267de3988fa8ce236f93f1f7c150298
VINADES.,JSC <contact@vinades.vn>
2021-03-30 13:03:48 +0700

    Tối ưu lại code

--- modules/news/admin/tags.php
+++ modules/news/admin/tags.php
@@ -34,59 +34,37 @@ function nv_show_tags_list($q = '', $incomplete = false)
         $nv_Request->set_Cookie('per_page_tagadmin_' . $module_data, $per_page, NV_LIVE_COOKIE_TIME);
     }
 
+    $where = [];
+    $base_url = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=' . $op . '&amp;per_page=' . $per_page;
     if (!empty($q)) {
-        $where = "keywords LIKE '%" . $db_slave->dblikeescape($q) . "%'";
-        $db_slave->sqlreset()
-        ->select('COUNT(tid)')
-        ->from(NV_PREFIXLANG . '_' . $module_data . '_tags');
-        if ($incomplete === true) {
-            $where .= "AND description = \'\'";
-        }
-        $db_slave->where($where);
-        $sth = $db_slave->prepare($db_slave->sql());
-    } else {
-        $db_slave->sqlreset()
-        ->select('COUNT(tid)')
-        ->from(NV_PREFIXLANG . '_' . $module_data . '_tags');
-        if ($incomplete === true) {
-            $db_slave->where('description = \'\'');
-        }
-        $sth = $db_slave->prepare($db_slave->sql());
+        $where[] = "keywords LIKE '%" . $db_slave->dblikeescape($q) . "%'";
+        $base_url .= '&amp;q=' . urlencode($q);
+    }
+    if (isset($incomplete) and $incomplete === true) {
+        $where[] = "description = ''";
+        $base_url .= '&amp;incomplete=1';
     }
 
+    $db_slave->sqlreset()
+    ->select('COUNT(tid)')
+    ->from(NV_PREFIXLANG . '_' . $module_data . '_tags')
+    ->where(implode(' AND ', $where));
+
+    $sth = $db_slave->prepare($db_slave->sql());
     $sth->execute();
     $num_items = $sth->fetchColumn();
+
     $db_slave->sqlreset()
     ->select('*')
-    ->from(NV_PREFIXLANG . '_' . $module_data . '_tags');
-    if ($incomplete === true) {
-        $db_slave->where('description = \'\'');
-    }
-    $db_slave->order('alias ASC')
+    ->from(NV_PREFIXLANG . '_' . $module_data . '_tags')
+    ->where(implode(' AND ', $where))
+    ->order('alias ASC')
     ->limit($per_page)
     ->offset(($page - 1) * $per_page);
 
-    if (!empty($q)) {
-        $q = strip_punctuation($q);
-        $db_slave->where('keywords LIKE :keywords');
-    } else {
-        $per_page = $nv_Request->get_int('per_page', 'cookie', $module_config[$module_name]['per_page']);
-        $db_slave->order('alias ASC')->limit($per_page)->offset(($page - 1) * $per_page);
-    }
-
     $sth = $db_slave->prepare($db_slave->sql());
-    if (!empty($q)) {
-        $sth->bindValue(':keywords', '%' . $q . '%', PDO::PARAM_STR);
-    }
     $sth->execute();
 
-    $base_url = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=' . $op . '&amp;per_page=' . $per_page;
-    if ($incomplete === true) {
-        $base_url .= ($incomplete === true ? '&amp;incomplete=1' : '');
-    }
-    if (!empty($q)) {
-        $base_url .= '&amp;q=' . urlencode($q);
-    }
     $xtpl = new XTemplate('tags_lists.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
     $xtpl->assign('LANG', $lang_module);
     $xtpl->assign('GLANG', $lang_global);
@@ -113,7 +91,7 @@ function nv_show_tags_list($q = '', $incomplete = false)
     }
 
     $generate_page = nv_generate_page($base_url, $num_items, $per_page, $page);
-    if (!empty($q) or $incomplete === 1) {
+    if (!empty($q)) {
         $generate_page = nv_generate_page($base_url, $num_items, $per_page, $page, true, true, 'nv_urldecode_ajax', 'module_show_list');
     }
     if (!empty($generate_page)) {
