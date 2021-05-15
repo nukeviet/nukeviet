4f2701094af2a031f24a669806b847cb38034a95
Nguyen Anh Tu <anhtu@vinades.vn>
2021-05-15 13:55:32 +0700

    Fix error in mail.tpl
    Không hiển thị dòng Điện thoại trong phần footer của thư nếu chưa khai báo số điện thoại của site

--- themes/mobile_default/theme.php
+++ themes/mobile_default/theme.php
@@ -43,6 +43,11 @@ function nv_mailHTML($title, $content, $footer='')
     $xtpl->assign('MESSAGE_TITLE', $title);
     $xtpl->assign('MESSAGE_CONTENT', $content);
     $xtpl->assign('MESSAGE_FOOTER', $footer);
+
+    if (!empty($global_config['phonenumber'])) {
+        $xtpl->parse('main.phonenumber');
+    }
+
     $xtpl->parse('main');
     return $xtpl->text('main');
 }
