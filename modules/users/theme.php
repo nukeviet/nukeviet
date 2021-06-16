8f26b462958ce0cdc218a86ce1dfa083d5a7f833
VINADES.,JSC <contact@vinades.vn>
2021-06-16 11:56:03 +0700

    Add redirect url at user login page

--- modules/users/theme.php
+++ modules/users/theme.php
@@ -295,7 +295,7 @@ function user_register($gfx_chk, $checkss, $data_questions, $array_field_config,
  */
 function user_login($is_ajax = false)
 {
-    global $module_info, $global_config, $lang_global, $lang_module, $module_name, $op, $nv_header, $nv_redirect;
+    global $module_info, $global_config, $lang_global, $lang_module, $module_name, $op, $nv_header, $nv_redirect, $canonicalUrl;
 
     if ($is_ajax) {
         $xtpl = new XTemplate('ajax_login.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/users');
@@ -360,10 +360,13 @@ function user_login($is_ajax = false)
             'google' => 'google-plus',
             'facebook' => 'facebook'
         ];
+        $default_redirect = nv_redirect_encrypt($canonicalUrl);
         foreach ($global_config['openid_servers'] as $server) {
             $assigns['href'] = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=oauth&amp;server=' . $server;
             if (!empty($nv_redirect)) {
                 $assigns['href'] .= '&nv_redirect=' . $nv_redirect;
+            } else {
+                $assigns['href'] .= '&nv_redirect=' . $default_redirect;
             }
             $assigns['server'] = $server;
             $assigns['title'] = ucfirst($server);
