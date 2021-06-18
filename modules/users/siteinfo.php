79b3079cc4f220c1d9ffa62eebcf9e2b84f6ac54
Nguyen Anh Tu <anhtu@vinades.vn>
2021-06-18 13:06:40 +0700

    Fix module users

--- modules/users/siteinfo.php
+++ modules/users/siteinfo.php
@@ -16,51 +16,59 @@ $lang_siteinfo = nv_get_lang_module($mod);
 $_mod_table = ($mod_data == 'users') ? NV_USERS_GLOBALTABLE : $db_config['prefix'] . '_' . $mod_data;
 $level = $admin_info['level'];
 
-$_arr_siteinfo = array();
+$_arr_siteinfo = [];
 $cacheFile = 'siteinfo_' . NV_CACHE_PREFIX . '.cache';
-$cacheTTL = 7200;
+$cacheTTL = 1800;
 
 if (($cache = $nv_Cache->getItem($mod, $cacheFile, $cacheTTL)) != false) {
     $_arr_siteinfo = unserialize($cache);
     $access_admin = $_arr_siteinfo['access_admin'];
 } else {
-    $_arr_siteinfo['number_user'] = $db->query('SELECT COUNT(*) FROM ' . $_mod_table)->fetchColumn();
-    $_arr_siteinfo['number_user_reg'] = $db->query('SELECT COUNT(*) FROM ' . $_mod_table . '_reg')->fetchColumn();
+    if ($global_config['idsite'] > 0) {
+        $site_condition = ' WHERE idsite=' . $global_config['idsite'];
+    } else {
+        $site_condition = '';
+    }
+    $_arr_siteinfo['number_user'] = $db->query('SELECT COUNT(*) FROM ' . $_mod_table . $site_condition)->fetchColumn();
+    $_arr_siteinfo['number_user_reg'] = $db->query('SELECT COUNT(*) FROM ' . $_mod_table . '_reg' . $site_condition)->fetchColumn();
     $_arr_siteinfo['number_user_edit'] = $db->query('SELECT COUNT(*) FROM ' . $_mod_table . '_edit')->fetchColumn();
-    $access_admin = $db->query("SELECT content FROM " . $_mod_table . "_config WHERE config='access_admin'")->fetchColumn();
+
+    $access_admin = $db->query('SELECT content FROM ' . $_mod_table . "_config WHERE config='access_admin'")->fetchColumn();
     $access_admin = unserialize($access_admin);
     $_arr_siteinfo['access_admin'] = $access_admin;
+
     $nv_Cache->setItem($mod, $cacheFile, serialize($_arr_siteinfo), $cacheTTL);
 }
-// So thanh vien
+
+// Số thành viên
 if ($_arr_siteinfo['number_user'] > 0) {
-    $siteinfo[] = array(
+    $siteinfo[] = [
         'key' => $lang_siteinfo['siteinfo_user'],
         'value' => number_format($_arr_siteinfo['number_user'])
-    );
+    ];
 }
 
-// So thanh vien doi kich hoat
+// Số thành viên đợi kích hoạt
 if ($_arr_siteinfo['number_user_reg'] > 0) {
-    $pendinginfo[] = array(
+    $pendinginfo[] = [
         'key' => $lang_siteinfo['siteinfo_waiting'],
         'value' => number_format($_arr_siteinfo['number_user_reg']),
         'link' => NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $mod . '&amp;' . NV_OP_VARIABLE . '=user_waiting'
-    );
+    ];
 }
 
 // Số thành viên chờ kiểm duyệt thông tin cá nhân
 if (!empty($_arr_siteinfo['number_user_edit']) and isset($access_admin['access_editcensor'][$level]) and $access_admin['access_editcensor'][$level] == 1) {
-    $pendinginfo[] = array(
+    $pendinginfo[] = [
         'key' => $lang_siteinfo['siteinfo_editcensor'],
         'value' => number_format($_arr_siteinfo['number_user_edit']),
         'link' => NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $mod . '&amp;' . NV_OP_VARIABLE . '=editcensor'
-    );
+    ];
 }
 
-// So thanh vien dang ky vao nhom
+// Số thành viên đăng ký vào nhóm chờ kiểm duyệt
 if (isset($access_admin['access_groups'][$level]) and $access_admin['access_groups'][$level] == 1) {
-    $pending_lists = $group_ids = array();
+    $pending_lists = $group_ids = [];
 
     $sql = 'SELECT COUNT(*) num_users, group_id FROM ' . $_mod_table . '_groups_users WHERE approved = 0 GROUP BY group_id';
     $result = $db->query($sql);
@@ -82,11 +90,11 @@ if (isset($access_admin['access_groups'][$level]) and $access_admin['access_grou
 
     if (!empty($pending_lists)) {
         foreach ($pending_lists as $row) {
-            $pendinginfo[] = array(
+            $pendinginfo[] = [
                 'key' => sprintf($lang_siteinfo['group_user_peding'], $row['title']),
                 'value' => number_format($row['num_users'], 0, ',', '.'),
                 'link' => NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $mod . '&amp;' . NV_OP_VARIABLE . '=groups&userlist=' . $row['group_id']
-            );
+            ];
         }
     }
 }
