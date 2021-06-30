3b3fb6e4ef986b8504ed94c901002fc383e28554
Nguyen Anh Tu <anhtu@vinades.vn>
2021-06-30 16:12:56 +0700

    Update QRcode block

--- src/themes/default/language/vi.php
+++ src/themes/default/language/vi.php
@@ -10,8 +10,8 @@
  */
 
 $lang_block['qr_level'] = 'Chất lượng hình';
-$lang_block['qr_pixel_per_point'] = 'Size của QR-code ';
-$lang_block['qr_outer_frame'] = 'Đường viền của QR-code';
+$lang_block['qr_size'] = 'Size của QR-code ';
+$lang_block['qr_margin'] = 'Đường viền của QR-code';
 
 $lang_block['cominfo_map_yes'] = 'Bật Google MAP';
 $lang_block['cominfo_map_no'] = 'Tắt Google MAP';
