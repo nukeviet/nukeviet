dedf49c4b9d3bf47b9724018d3c68c9fbf241695
Nguyen Anh Tu <anhtu@vinades.vn>
2021-08-22 19:01:23 +0700

    Fix SQLSTATE: Base table not found in News module - part 3

--- modules/news/funcs/search.php
+++ modules/news/funcs/search.php
@@ -113,27 +113,19 @@ $where = '';
 $tbl_src = '';
 if (empty($key) and ($catid == 0) and empty($from_date) and empty($to_date)) {
     $contents .= '<div class="alert alert-danger">' . $lang_module['empty_data_search'] . '</div>';
+} elseif (!empty($catid) and !isset($array_cat_search[$catid])) {
+    $contents .= '<div class="alert alert-danger">' . $lang_module['search_catid_error'] . '</div>';
 } else {
-    $allowed = true;
-    if ($catid) {
-        $res = $db->query("SHOW TABLES LIKE '" . NV_PREFIXLANG . '_' . $module_data . '_' . $catid . "'");
-        $item = $res->fetch(3);
-        if (!$item) {
-            $contents .= '<div class="alert alert-danger">' . $lang_module['search_catid_error'] . '</div>';
-            $allowed = false;
-        }
-    }
-    if ($allowed) {
-        $dbkey = $db_slave->dblikeescape($key);
-        $dbkeyhtml = $db_slave->dblikeescape($keyhtml);
+    $dbkey = $db_slave->dblikeescape($key);
+    $dbkeyhtml = $db_slave->dblikeescape($keyhtml);
 
-        if ($module_config[$module_name]['elas_use'] == 1) {
-            //ket noi den csdl elastic
-            $nukeVietElasticSearh = new NukeViet\ElasticSearch\Functions($module_config[$module_name]['elas_host'], $module_config[$module_name]['elas_port'], $module_config[$module_name]['elas_index']);
+    if ($module_config[$module_name]['elas_use'] == 1) {
+        //ket noi den csdl elastic
+        $nukeVietElasticSearh = new NukeViet\ElasticSearch\Functions($module_config[$module_name]['elas_host'], $module_config[$module_name]['elas_port'], $module_config[$module_name]['elas_index']);
 
-            $dbkeyhtml = nv_EncString($dbkeyhtml);
-            if ($choose == 1) {
-                $search_elastic = [
+        $dbkeyhtml = nv_EncString($dbkeyhtml);
+        if ($choose == 1) {
+            $search_elastic = [
                 'should' => [
                     'multi_match' => [ //dung multi_match:tim kiem theo nhieu truong
                         'query' => $dbkeyhtml, //tim kiem theo t? kh�a
@@ -151,30 +143,30 @@ if (empty($key) and ($catid == 0) and empty($from_date) and empty($to_date)) {
                     ]
                 ]
             ];
-            } elseif ($choose == 2) {
-                //match:tim kiem theo 1 truong
-                $search_elastic = [
+        } elseif ($choose == 2) {
+            //match:tim kiem theo 1 truong
+            $search_elastic = [
                 'should' => [
                     'match' => [
                         'unsigned_author' => $dbkeyhtml
                     ]
                 ]
             ];
-            } elseif ($choose == 3) {
-                $qurl = $key;
-                $url_info = parse_url($qurl);
-                if (isset($url_info['scheme']) and isset($url_info['host'])) {
-                    $qurl = $url_info['scheme'] . '://' . $url_info['host'];
-                }
-                $search_elastic = [
+        } elseif ($choose == 3) {
+            $qurl = $key;
+            $url_info = parse_url($qurl);
+            if (isset($url_info['scheme']) and isset($url_info['host'])) {
+                $qurl = $url_info['scheme'] . '://' . $url_info['host'];
+            }
+            $search_elastic = [
                 'should' => [
                     'match' => [
                         'sourcetext' => $db_slave->dblikeescape($qurl)
                     ]
                 ]
             ];
-            } else {
-                $search_elastic = [
+        } else {
+            $search_elastic = [
                 'should' => [
                     'multi_match' => [ //dung multi_match:tim kiem theo nhieu truong
                         'query' => $dbkeyhtml, //tim kiem theo tu khoa
@@ -193,69 +185,69 @@ if (empty($key) and ($catid == 0) and empty($from_date) and empty($to_date)) {
                     ]
                 ]
             ];
-            }
-            if ($catid > 0) {
-                $search_elastic_catid = [
+        }
+        if ($catid > 0) {
+            $search_elastic_catid = [
                 'filter' => [
                     'term' => [
                         'catid' => $catid
                     ]
                 ]
             ];
-            }
+        }
 
-            $todate_elastic = [];
-            if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['to_date'], $m)) {
-                $todate_elastic = [
+        $todate_elastic = [];
+        if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['to_date'], $m)) {
+            $todate_elastic = [
                 'lte' => mktime(23, 59, 59, $m[2], $m[1], $m[3])
             ];
-            }
-            $fromdate_elastic = [];
-            if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['from_date'], $m)) {
-                $fromdate_elastic = [
+        }
+        $fromdate_elastic = [];
+        if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['from_date'], $m)) {
+            $fromdate_elastic = [
                 'gte' => mktime(0, 0, 0, $m[2], $m[1], $m[3])
             ];
-            }
+        }
 
-            $array_query_elastic = [];
-            $array_query_elastic['query']['bool'] = $search_elastic;
-            $array_query_elastic['size'] = $per_page;
-            $array_query_elastic['from'] = ($page - 1) * $per_page;
+        $array_query_elastic = [];
+        $array_query_elastic['query']['bool'] = $search_elastic;
+        $array_query_elastic['size'] = $per_page;
+        $array_query_elastic['from'] = ($page - 1) * $per_page;
 
-            if ($date_elastic = array_merge($todate_elastic, $fromdate_elastic)) {
-                $array_query_elastic['query']['bool']['must']['range']['publtime'] = $date_elastic;
-                $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
-            } elseif ($todate_elastic) {
-                $array_query_elastic['query']['bool']['must']['range']['publtime'] = $todate_elastic;
-                $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
-            } elseif ($fromdate_elastic) {
-                $array_query_elastic['query']['bool']['must']['range']['publtime'] = $fromdate_elastic;
-                $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
+        if ($date_elastic = array_merge($todate_elastic, $fromdate_elastic)) {
+            $array_query_elastic['query']['bool']['must']['range']['publtime'] = $date_elastic;
+            $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
+        } elseif ($todate_elastic) {
+            $array_query_elastic['query']['bool']['must']['range']['publtime'] = $todate_elastic;
+            $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
+        } elseif ($fromdate_elastic) {
+            $array_query_elastic['query']['bool']['must']['range']['publtime'] = $fromdate_elastic;
+            $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
+        } else {
+            $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
+        }
+        $numRecord = $response['hits']['total'];
+        // Không cho tùy ý đánh số page + xác định trang trước, trang sau
+        betweenURLs($page, ceil($numRecord / $per_page), $base_url, '&page-', $prevPage, $nextPage);
+
+        foreach ($response['hits']['hits'] as $key => $value) {
+            $homeimgthumb = $value['_source']['homeimgthumb'];
+            if ($homeimgthumb == 1) {
+                // image thumb
+                $img_src = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module_upload . '/' . $value['_source']['homeimgfile'];
+            } elseif ($homeimgthumb == 2) {
+                // image file
+                $img_src = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $value['_source']['homeimgfile'];
+            } elseif ($homeimgthumb == 3) {
+                // image url
+                $img_src = $value['_source']['homeimgfile'];
+            } elseif (!empty($show_no_image)) {
+                // no image
+                $img_src = NV_BASE_SITEURL . $show_no_image;
             } else {
-                $response = $nukeVietElasticSearh->search_data(NV_PREFIXLANG . '_' . $module_data . '_rows', $array_query_elastic);
+                $img_src = '';
             }
-            $numRecord = $response['hits']['total'];
-            // Không cho tùy ý đánh số page + xác định trang trước, trang sau
-            betweenURLs($page, ceil($numRecord / $per_page), $base_url, '&page-', $prevPage, $nextPage);
-
-            foreach ($response['hits']['hits'] as $key => $value) {
-                $homeimgthumb = $value['_source']['homeimgthumb'];
-                if ($homeimgthumb == 1) {
-                    // image thumb
-                    $img_src = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module_upload . '/' . $value['_source']['homeimgfile'];
-                } elseif ($homeimgthumb == 2) {
-                    // image file
-                    $img_src = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $value['_source']['homeimgfile'];
-                } elseif ($homeimgthumb == 3) {
-                    // image url
-                    $img_src = $value['_source']['homeimgfile'];
-                } elseif (!empty($show_no_image)) {
-                    // no image
-                    $img_src = NV_BASE_SITEURL . $show_no_image;
-                } else {
-                    $img_src = '';
-                }
-                $array_content[] = [
+            $array_content[] = [
                 'id' => $value['_source']['id'],
                 'title' => $value['_source']['title'],
                 'alias' => $value['_source']['alias'],
@@ -267,82 +259,82 @@ if (empty($key) and ($catid == 0) and empty($from_date) and empty($to_date)) {
                 'sourceid' => $value['_source']['sourceid'],
                 'external_link' => $value['_source']['external_link']
             ];
+        }
+        $contents .= search_result_theme($key, $numRecord, $per_page, $page, $array_content, $catid);
+    } else {
+        if ($choose == 1) {
+            $tbl_src = ' LEFT JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_detail tb2 ON ( tb1.id = tb2.id ) ';
+            $where = "AND ( tb1.title LIKE '%" . $dbkeyhtml . "%' OR tb1.hometext LIKE '%" . $dbkey . "%' OR tb2.bodyhtml LIKE '%" . $dbkey . "%' ) ";
+        } elseif ($choose == 2) {
+            $where = "AND ( tb1.author LIKE '%" . $dbkeyhtml . "%' ) ";
+        } elseif ($choose == 3) {
+            $qurl = $key;
+            $url_info = parse_url($qurl);
+            if (isset($url_info['scheme']) and isset($url_info['host'])) {
+                $qurl = $url_info['scheme'] . '://' . $url_info['host'];
             }
-            $contents .= search_result_theme($key, $numRecord, $per_page, $page, $array_content, $catid);
+            $where = 'AND (tb1.sourceid IN (SELECT sourceid FROM ' . NV_PREFIXLANG . '_' . $module_data . "_sources WHERE title like '%" . $db_slave->dblikeescape($dbkey) . "%' OR link like '%" . $db_slave->dblikeescape($qurl) . "%'))";
         } else {
-            if ($choose == 1) {
-                $tbl_src = ' LEFT JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_detail tb2 ON ( tb1.id = tb2.id ) ';
-                $where = "AND ( tb1.title LIKE '%" . $dbkeyhtml . "%' OR tb1.hometext LIKE '%" . $dbkey . "%' OR tb2.bodyhtml LIKE '%" . $dbkey . "%' ) ";
-            } elseif ($choose == 2) {
-                $where = "AND ( tb1.author LIKE '%" . $dbkeyhtml . "%' ) ";
-            } elseif ($choose == 3) {
-                $qurl = $key;
-                $url_info = parse_url($qurl);
-                if (isset($url_info['scheme']) and isset($url_info['host'])) {
-                    $qurl = $url_info['scheme'] . '://' . $url_info['host'];
-                }
-                $where = 'AND (tb1.sourceid IN (SELECT sourceid FROM ' . NV_PREFIXLANG . '_' . $module_data . "_sources WHERE title like '%" . $db_slave->dblikeescape($dbkey) . "%' OR link like '%" . $db_slave->dblikeescape($qurl) . "%'))";
-            } else {
-                $qurl = $key;
-                $url_info = parse_url($qurl);
-                if (isset($url_info['scheme']) and isset($url_info['host'])) {
-                    $qurl = $url_info['scheme'] . '://' . $url_info['host'];
-                }
-                $tbl_src = ' LEFT JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_detail tb2 ON ( tb1.id = tb2.id )';
-                $where = " AND (( tb1.title LIKE '%" . $dbkeyhtml . "%' OR tb1.hometext LIKE '%" . $dbkey . "%' ";
-                $where .= " OR tb1.author LIKE '%" . $dbkeyhtml . "%' OR tb2.bodyhtml LIKE '%" . $dbkey . "%') OR (tb1.sourceid IN (SELECT sourceid FROM " . NV_PREFIXLANG . "_" . $module_data . "_sources WHERE title like '%" . $db_slave->dblikeescape($dbkey) . "%' OR link like '%" . $db_slave->dblikeescape($qurl) . "%')))";
+            $qurl = $key;
+            $url_info = parse_url($qurl);
+            if (isset($url_info['scheme']) and isset($url_info['host'])) {
+                $qurl = $url_info['scheme'] . '://' . $url_info['host'];
             }
+            $tbl_src = ' LEFT JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_detail tb2 ON ( tb1.id = tb2.id )';
+            $where = " AND (( tb1.title LIKE '%" . $dbkeyhtml . "%' OR tb1.hometext LIKE '%" . $dbkey . "%' ";
+            $where .= " OR tb1.author LIKE '%" . $dbkeyhtml . "%' OR tb2.bodyhtml LIKE '%" . $dbkey . "%') OR (tb1.sourceid IN (SELECT sourceid FROM " . NV_PREFIXLANG . "_" . $module_data . "_sources WHERE title like '%" . $db_slave->dblikeescape($dbkey) . "%' OR link like '%" . $db_slave->dblikeescape($qurl) . "%')))";
+        }
 
-            if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['to_date'], $m)) {
-                $where .= ' AND tb1.publtime <=' . mktime(23, 59, 59, $m[2], $m[1], $m[3]);
-            }
-            if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['from_date'], $m)) {
-                $where .= ' AND tb1.publtime >=' . mktime(0, 0, 0, $m[2], $m[1], $m[3]);
-            }
+        if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['to_date'], $m)) {
+            $where .= ' AND tb1.publtime <=' . mktime(23, 59, 59, $m[2], $m[1], $m[3]);
+        }
+        if (preg_match('/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/', $date_array['from_date'], $m)) {
+            $where .= ' AND tb1.publtime >=' . mktime(0, 0, 0, $m[2], $m[1], $m[3]);
+        }
 
-            if ($catid > 0) {
-                $table_search = NV_PREFIXLANG . '_' . $module_data . '_' . $catid;
-            } else {
-                $table_search = NV_PREFIXLANG . '_' . $module_data . '_rows';
-            }
+        if ($catid > 0) {
+            $table_search = NV_PREFIXLANG . '_' . $module_data . '_' . $catid;
+        } else {
+            $table_search = NV_PREFIXLANG . '_' . $module_data . '_rows';
+        }
 
-            $db_slave->sqlreset()
+        $db_slave->sqlreset()
             ->select('COUNT(*)')
             ->from($table_search . ' as tb1 ' . $tbl_src)
             ->where('tb1.status=1 ' . $where);
 
-            $numRecord = $db_slave->query($db_slave->sql())
+        $numRecord = $db_slave->query($db_slave->sql())
             ->fetchColumn();
-            // Không cho tùy ý đánh số page + xác định trang trước, trang sau
-            betweenURLs($page, ceil($numRecord / $per_page), $base_url, '&page=', $prevPage, $nextPage);
+        // Không cho tùy ý đánh số page + xác định trang trước, trang sau
+        betweenURLs($page, ceil($numRecord / $per_page), $base_url, '&page=', $prevPage, $nextPage);
 
-            $db_slave->select('tb1.id,tb1.title,tb1.alias,tb1.catid,tb1.hometext,tb1.author,tb1.publtime,tb1.homeimgfile, tb1.homeimgthumb,tb1.sourceid,tb1.external_link')
+        $db_slave->select('tb1.id,tb1.title,tb1.alias,tb1.catid,tb1.hometext,tb1.author,tb1.publtime,tb1.homeimgfile, tb1.homeimgthumb,tb1.sourceid,tb1.external_link')
             ->order('tb1.' . $order_articles_by . ' DESC')
             ->limit($per_page)
             ->offset(($page - 1) * $per_page);
 
-            $result = $db_slave->query($db_slave->sql());
+        $result = $db_slave->query($db_slave->sql());
 
-            $array_content = [];
-            $show_no_image = $module_config[$module_name]['show_no_image'];
+        $array_content = [];
+        $show_no_image = $module_config[$module_name]['show_no_image'];
 
-            while (list($id, $title, $alias, $catid, $hometext, $author, $publtime, $homeimgfile, $homeimgthumb, $sourceid, $external_link) = $result->fetch(3)) {
-                if ($homeimgthumb == 1) {
-                    // image thumb
-                    $img_src = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module_upload . '/' . $homeimgfile;
-                } elseif ($homeimgthumb == 2) {
-                    // image file
-                    $img_src = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $homeimgfile;
-                } elseif ($homeimgthumb == 3) {
-                    // image url
-                    $img_src = $homeimgfile;
-                } elseif (!empty($show_no_image)) {
-                    // no image
-                    $img_src = NV_BASE_SITEURL . $show_no_image;
-                } else {
-                    $img_src = '';
-                }
-                $array_content[] = [
+        while (list($id, $title, $alias, $catid, $hometext, $author, $publtime, $homeimgfile, $homeimgthumb, $sourceid, $external_link) = $result->fetch(3)) {
+            if ($homeimgthumb == 1) {
+                // image thumb
+                $img_src = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module_upload . '/' . $homeimgfile;
+            } elseif ($homeimgthumb == 2) {
+                // image file
+                $img_src = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $homeimgfile;
+            } elseif ($homeimgthumb == 3) {
+                // image url
+                $img_src = $homeimgfile;
+            } elseif (!empty($show_no_image)) {
+                // no image
+                $img_src = NV_BASE_SITEURL . $show_no_image;
+            } else {
+                $img_src = '';
+            }
+            $array_content[] = [
                 'id' => $id,
                 'title' => $title,
                 'alias' => $alias,
@@ -354,11 +346,10 @@ if (empty($key) and ($catid == 0) and empty($from_date) and empty($to_date)) {
                 'sourceid' => $sourceid,
                 'external_link' => $external_link
             ];
-            }
         }
-
-        $contents .= search_result_theme($key, $numRecord, $per_page, $page, $array_content, $catid);
     }
+
+    $contents .= search_result_theme($key, $numRecord, $per_page, $page, $array_content, $catid);
 }
 
 if (empty($key)) {
