d6b1f2a96ee28e33642054396e1614ae07a91edd
Nguyen Anh Tu <anhtu@vinades.vn>
2021-10-15 22:27:42 +0700

    Cập nhật QRcode plugin

--- includes/plugin/qrcode.php
+++ includes/plugin/qrcode.php
@@ -22,14 +22,27 @@ nv_add_hook($module_name, 'get_qr_code', $priority, function ($vars) {
             $barcode = new Com\Tecnick\Barcode\Barcode();
             // generate a barcode
             $bobj = $barcode->getBarcodeObj(
-                'QRCODE,H',                     // barcode type and additional comma-separated parameters
-                $url,          // data string to encode
-                160,                             // bar width (use absolute or negative value as multiplication factor)
-                160,                             // bar height (use absolute or negative value as multiplication factor)
-                'black',                        // foreground color
-                array(5, 5, 5, 5)           // padding (use absolute or negative values as multiplication factors)
+                'QRCODE,H',  // barcode type and additional comma-separated parameters
+                $url,        // data string to encode
+                160,         // bar width (use absolute or negative value as multiplication factor)
+                160,         // bar height (use absolute or negative value as multiplication factor)
+                'black',     // foreground color
+                [5, 5, 5, 5] // padding (use absolute or negative values as multiplication factors)
             )->setBackgroundColor('white'); // background color
-            $bobj->getPng();
+            $data = $bobj->getSvgCode();
+            header('Content-Type: image/svg+xml');
+            header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
+            header('Pragma: public');
+            header('Expires: ' . gmdate('D, d M Y H:i:s', time() - 3600) . ' GMT'); // Date in the past
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+            header('Content-Disposition: inline; filename="' . md5($url) . '.svg";');
+            header('access-control-allow-origin: *');
+            header('Vary: Accept-Encoding');
+            if (empty($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+                // the content length may vary if the server is using compression
+                header('Content-Length: ' . strlen($data));
+            }
+            echo $data;
         }
         exit();
     }
