d167e92d87e47d13933abd50b29e7a6e4097ea78
Nguyen Anh Tu <anhtu@vinades.vn>
2021-11-15 12:05:52 +0700

    Fix cảnh báo "iconv_set_encoding đã lỗi thời"
    #3376

--- includes/utf8/iconv_string_handler.php
+++ includes/utf8/iconv_string_handler.php
@@ -13,9 +13,16 @@ if (!defined('NV_MAINFILE')) {
     exit('Stop!!!');
 }
 
-iconv_set_encoding('input_encoding', $global_config['site_charset']);
-iconv_set_encoding('internal_encoding', $global_config['site_charset']);
-iconv_set_encoding('output_encoding', $global_config['site_charset']);
+// https://www.php.net/manual/en/function.iconv-set-encoding.php#119888
+// https://github.com/nukeviet/nukeviet/issues/3376
+if (PHP_VERSION_ID < 50600) {
+    iconv_set_encoding('input_encoding', $global_config['site_charset']);
+    iconv_set_encoding('internal_encoding', $global_config['site_charset']);
+    iconv_set_encoding('output_encoding', $global_config['site_charset']);
+} else {
+    ini_set('default_charset', $global_config['site_charset']);
+}
+
 
 /**
  * nv_internal_encoding()
