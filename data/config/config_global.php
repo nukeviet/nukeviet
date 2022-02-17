f2fb82ebfbe9e792b801e394a3ef9b4df57f016f
Nguyen Anh Tu <anhtu@vinades.vn>
2022-02-17 14:19:27 +0700

    Thêm: Buộc đăng xuất khỏi tài khoản người dùng khi đăng nhập admin
    #3409

--- data/config/config_global.php
+++ data/config/config_global.php
@@ -31,6 +31,7 @@ $global_config['admfirewall'] = 0;
 $global_config['admin_2step_default'] = 'code';
 $global_config['admin_2step_opt'] = 'code';
 $global_config['admin_check_pass_time'] = 1800;
+$global_config['admin_user_logout'] = 0;
 $global_config['allow_null_origin'] = 0;
 $global_config['allow_sitelangs'] = ['vi'];
 $global_config['authors_detail_main'] = 0;
