11f9c3fbc2fdc0a14b29ba0cfe915092cb4d3dc7
Nguyen Anh Tu <anhtu@vinades.vn>
2022-05-05 14:16:24 +0700

    Update config_global.php

--- data/config/config_global.php
+++ data/config/config_global.php
@@ -115,6 +115,7 @@ $global_config['site_timezone'] = 'byCountry';
 $global_config['spadmin_add_admin'] = 1;
 $global_config['str_referer_blocker'] = 0;
 $global_config['timestamp'] = 0;
+$global_config['static_noquerystring'] = 0;
 $global_config['two_step_verification'] = 0;
 $global_config['upload_alt_require'] = 1;
 $global_config['upload_auto_alt'] = 1;
