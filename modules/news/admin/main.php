7bb107a9e7ea711b6e9d0c39197128ce628d6407
Nguyễn Văn Lâm <lamnv@vinades.com.vn>
2022-05-10 16:38:41 +0700

    Cập nhật lại code bộ lọc thời gian

--- modules/news/admin/main.php
+++ modules/news/admin/main.php
@@ -20,10 +20,10 @@ $catid = $nv_Request->get_int('catid', 'get', 0);
 $per_page_old = $nv_Request->get_int('per_page', 'cookie', 50);
 $per_page = $nv_Request->get_int('per_page', 'get', $per_page_old);
 $num_items = $nv_Request->get_int('num_items', 'get', 0);
-$search_type_date = $nv_Request->get_string('type_date', 'get', '-');
+$search_type_date = $nv_Request->get_title('type_date', 'get', '-');
 $search_time_to = $nv_Request->get_title('search_time_to', 'get', '');
 $search_time_from = $nv_Request->get_title('search_time_from', 'get', '');
-$error_date = "";
+$error_date = '';
 
 if ($per_page < 1 or $per_page > 500) {
     $per_page = 50;
@@ -401,138 +401,27 @@ if (($module_config[$module_name]['elas_use'] == 1) and $checkss == NV_CHECK_SES
         }
     }
 
-    if ($search_type_date != "-" && ($search_type_date == "addtime" || $search_type_date == "publtime" || $search_type_date == "exptime")) {
-        if (!empty($search_time_to) && !empty($search_time_from)) {
+    if ($search_type_date == 'addtime' || $search_type_date == 'publtime' || $search_type_date == 'exptime') {
+        if (!empty($search_time_from)) {
             $timestamp_from = "";
             if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_from, $m)) {
                 $timestamp_from = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
             }
 
-            $timestamp_to = "";
-            if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
-                $timestamp_to = mktime(23, 59, 59, $m[2], $m[1], $m[3]);
-            }
-            
-            if (!empty($timestamp_to) && !empty($timestamp_from)) {
-                // Hiển thị lỗi kiểm tra thời gian
-                if ($timestamp_to < $timestamp_from) {
-                    $error_date = $lang_module['error_to_date'];
-                    $timestamp_from = "";
-                } else {
-                    $match = [];
-                    if ($search_type_date == "addtime") {
-                        $match[]['range']['addtime'] = [
-                            "gte" => $timestamp_from,
-                            "lte" => $timestamp_to
-                        ];
+            $match[]['range'][$search_type_date] = [
+                "gte" => $timestamp_from,
+            ];
+        }
 
-                        $match[]['range']['edittime'] = [
-                            "gte" => $timestamp_from,
-                            "lte" => $timestamp_to
-                        ];
-                    } else {
-                        $match[]['range'][$search_type_date] = [
-                            "gte" => $timestamp_from,
-                            "lte" => $timestamp_to
-                        ];
-                    }
-
-                    $search_elastic_user['filter']['or'] = $match;
-                    $search_elastic = array_merge($search_elastic, $search_elastic_user);
-                }
-            }
-        } else if (empty($search_time_to) && empty($search_time_from)){
-            $match = [];
-            $search_time_to = date("d/m/Y");
-            $timestamp_to = "";
-            if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
-                $timestamp_to = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
-            }
-
-            if (!empty($timestamp_to)) {
-                if ($search_type_date == "addtime") {
-                    $match[]['range']['addtime'] = [
-                        "lte" => $timestamp_to
-                    ];
-
-                    $match[]['range']['edittime'] = [
-                        "lte" => $timestamp_to
-                    ];
-                } else {
-                    $match[]['range'][$search_type_date] = [
-                        "lte" => $timestamp_to
-                    ];
-                }
-                
-                $search_elastic_user['filter']['or'] = $match;
-                $search_elastic = array_merge($search_elastic, $search_elastic_user);
-            }
-        } elseif (!empty($search_time_from) && empty($search_time_to)) {
-            // Trường hợp thời gian từ ngày khác rỗng và thời gian đến ngày rỗng
-            $match = [];
-            $timestamp_from = "";
-            if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_from, $m)) {
-                $timestamp_from = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
-            }
-
-            $search_time_to = date('d/m/Y');
+        if (!empty($search_time_to)) {
             $timestamp_to = "";
             if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
                 $timestamp_to = mktime(23, 59, 59, $m[2], $m[1], $m[3]);
             }
 
-            if (!empty($timestamp_to) && !empty($timestamp_from)) { 
-                // Hiển thị lỗi kiểm tra thời gian
-                if ($timestamp_to < $timestamp_from) {
-                    $error_date = $lang_module['error_to_date'];
-                    $timestamp_from = "";
-                } else {
-                    if ($search_type_date == "addtime") {
-                        $match[]['range']['addtime'] = [
-                            "gte" => $timestamp_from,
-                            "lte" => $timestamp_to
-                        ];
-
-                        $match[]['range']['edittime'] = [
-                            "gte" => $timestamp_from,
-                            "lte" => $timestamp_to
-                        ];
-                    } else {
-                        $match[]['range'][$search_type_date] = [
-                            "gte" => $timestamp_from,
-                            "lte" => $timestamp_to
-                        ];
-                    }
-                    $search_elastic_user['filter']['or'] = $match;
-                    $search_elastic = array_merge($search_elastic, $search_elastic_user); 
-                }
-            }
-        } else {
-            $match = [];
-            // Trường hợp thời gian từ ngày rỗng và thời gian đến ngày khác rỗng
-            $timestamp_to = "";
-            if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
-                $timestamp_to = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
-            }
-            
-            if (!empty($timestamp_to)) {
-                if ($search_type_date == "addtime") {
-                    $match[]['range']['addtime'] = [
-                        "lte" => $timestamp_to
-                    ];
-
-                    $match[]['range']['edittime'] = [
-                        "lte" => $timestamp_to
-                    ];
-                } else {
-                   $match[]['range'][$search_type_date] = [
-                        "lte" => $timestamp_to
-                    ]; 
-                }
-                
-                $search_elastic_user['filter']['or'] = $match;
-                $search_elastic = array_merge($search_elastic, $search_elastic_user);
-            }
+            $match[]['range'][$search_type_date] = [
+                "lte" => $timestamp_to
+            ];
         }
     }
 
@@ -739,89 +628,30 @@ if (($module_config[$module_name]['elas_use'] == 1) and $checkss == NV_CHECK_SES
                 OR a.pseudonym LIKE '%" . $db_slave->dblikeescape($qhtml) . "%')";
         }
 
-        if ($search_type_date != "-" && ($search_type_date == "addtime" || $search_type_date == "publtime" || $search_type_date == "exptime")) {
-            if (!empty($search_time_to) && !empty($search_time_from)) {
+        if ($search_type_date == "addtime" || $search_type_date == "publtime" || $search_type_date == "exptime") {
+            if (!empty($search_time_from)) {
                 $timestamp_from = "";
                 if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_from, $m)) {
                     $timestamp_from = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
                 }
 
+                if ($search_type_date == "addtime") {
+                    $where .= "r.addtime >= " . $timestamp_from;
+                    $where .= " OR r.edittime >= " . $timestamp_from;
+                } else {
+                    $column_date = "r." . $search_type_date;
+                    $where = $column_date . ">= " . $timestamp_from;
+                }
+            }
+
+            if (!empty($search_time_to)) {
                 $timestamp_to = "";
                 if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
                     $timestamp_to = mktime(23, 59, 59, $m[2], $m[1], $m[3]);
                 }
 
-                if (!empty($timestamp_to) && !empty($timestamp_from)) {
-                    if ($timestamp_to < $timestamp_from) {
-                        $error_date = $lang_module['error_to_date'];
-                        $timestamp_from = "";
-                    } else {
-                        if ($search_type_date == "addtime") {
-                            $where .= "(r.addtime >= " . $timestamp_from . " AND r.addtime <= " . $timestamp_to . ")";
-                            $where .= " OR (r.edittime >= " . $timestamp_from . " AND r.edittime <= " . $timestamp_to . ")";
-                        } else {
-                            $column_date = "r." . $search_type_date;
-                            $where = "(" . $column_date . ">= " . $timestamp_from . " AND " . $column_date . " <= " . $timestamp_to . ")";
-                        }
-                    }
-                }
-            } else if (empty($search_time_to) && empty($search_time_from)){
-                $search_time_to = date("d/m/Y");
-                $timestamp_to = "";
-                if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
-                    $timestamp_to = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
-                }
-
-                
-                if ($search_type_date == "addtime") {
-                    $where .= " (r.addtime <= " . $timestamp_to . ")";
-                    $where .= " OR (r.edittime <= " . $timestamp_to . ")";
-                } else {
-                    $column_date = "r." . $search_type_date;
-                    $where = "(" . $column_date . " <= " . $timestamp_to . ")"; 
-                }
-            } elseif (!empty($search_time_from) && empty($search_time_to)) {
-                 // Trường hợp thời gian từ ngày khác rỗng và thời gian đến ngày rỗng
-                $timestamp_from = "";
-                if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_from, $m)) {
-                    $timestamp_from = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
-                }
-
-                $search_time_to = date('d/m/Y');
-                $timestamp_to = "";
-                if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
-                    $timestamp_to = mktime(23, 59, 59, $m[2], $m[1], $m[3]);
-                }
-
-                if (!empty($timestamp_to) && !empty($timestamp_from)) { 
-                    if ($timestamp_to < $timestamp_from) {
-                        $error_date = $lang_module['error_to_date'];
-                        $timestamp_from = "";
-                    } else { 
-                        if ($search_type_date == "addtime") {
-                            $where .= "(r.addtime >= " . $timestamp_from . " AND r.addtime <= " . $timestamp_to . ")";
-                            $where .= " OR (r.edittime >= " . $timestamp_from . " AND r.edittime <= " . $timestamp_to . ")";
-                        } else {
-                            $column_date = "r." . $search_type_date;
-                            $where = "(" . $column_date . " >= " . $timestamp_from . " AND " . $column_date . " <= " . $timestamp_to . ")";
-                        }
-                    }
-                }
-
-            } else {
-                // Trường hợp thời gian từ ngày rỗng và thời gian đến ngày khác rỗng
-                $timestamp_to = "";
-                if (preg_match('/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/', $search_time_to, $m)) {
-                    $timestamp_to = mktime(00, 00, 00, $m[2], $m[1], $m[3]);
-                }
-
-                if ($search_type_date == "addtime") {
-                    $where .= " (r.addtime <= " . $timestamp_to . ")";
-                    $where .= " OR (r.edittime <= " . $timestamp_to . ")";
-                } else {
-                    $column_date = "r." . $search_type_date;
-                    $where = "(" . $column_date . " <= " . $timestamp_to . ")";
-                }
+                $column_date = "r." . $search_type_date;
+                $where = $column_date . " <= " . $timestamp_to . ")";
             }
         }
 
@@ -1023,12 +853,12 @@ foreach ($array_search as $key => $val) {
     ];
 }
 
-$arr_search_date = array(
+$arr_search_date = [
     '-' => $lang_module['search_type_date'],
     'addtime' => $lang_module['content_publ_date'],
     'publtime' => $lang_module['search_public_time'],
     'exptime' => $lang_module['content_exp_date'],
-);
+];
 
 $array_select_type_date = [];
 foreach ($arr_search_date as $key => $val) {
