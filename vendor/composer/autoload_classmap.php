db8960d181050fa6dd9aa0b55771a952c98fe38c
VINADES.,JSC <contact@vinades.vn>
2022-06-02 15:46:33 +0700

    PHPMailer 6.6.0

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
     'PclZip' => $vendorDir . '/vinades/pclzip/pclzip.lib.php',
 );
