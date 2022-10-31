823cbf2e30fc0b36354b77d6d935e2593b7acff7
Vũ Thảo <thao@vinades.vn>
2022-10-31 17:02:00 +0700

    Update editors config.js

---
+++ assets/editors/ckeditor/config.js
@@ -0,0 +1,50 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.language = nv_lang_interface;
+    config.allowedContent = true;
+    config.extraPlugins = 'video,eqneditor,switchbar,tbvdownload,cleanlink,googledocs';
+    config.entities = false;
+    config.image2_altRequired = false;
+    config.image2_alignClasses = ['image-left', 'image-center', 'image-right'];
+    config.youtube_width = '640';
+    config.youtube_height = '480';
+    config.youtube_related = false;
+    config.youtube_older = false;
+    config.youtube_privacy = false;
+    config.youtube_autoplay = true;
+    config.codeSnippet_theme = 'github';
+    // Default setting.
+
+    config.toolbarGroups = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'insert', groups: [ 'insert' ] },
+        { name: 'others', groups: [ 'others' ] },
+        '/',
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'links', groups: [ 'links' ] },
+        '/',
+        { name: 'styles', groups: [ 'styles' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'tools', groups: [ 'tools' ] }
+    ];
+
+    config.toolbar_Basic =
+    [
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+        { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+        '/',
+        { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+        { name: 'styles', items : [ 'Font', 'FontSize', 'TextColor', 'BGColor' ] },
+        { name: 'tools', items : ['SwitchBar',  'Maximize'] }
+    ];
+};
