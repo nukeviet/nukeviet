73d7977d48dc7ba799fc45ee175b174eb6557a51
Nguyen Anh Tu <anhtu@vinades.vn>
2023-06-23 10:54:16 +0700

    Không bắt buộc phải có email cho oauth là Facebook
    #3639

--- includes/core/admin_login_facebook.php
+++ includes/core/admin_login_facebook.php
@@ -28,22 +28,15 @@ if (!empty($_GET['code'])) {
     try {
         $facebookService->requestAccessToken($_GET['code']);
         $result = json_decode($facebookService->request('/me?fields=id,name,email,first_name,last_name'), true);
-        if (is_array($result) and !empty($result['id']) and !empty($result['email'])) {
-            $result['email'] = nv_check_valid_email($result['email'], true);
-            if (!empty($result['email'][0])) {
-                // Kiểm tra email hợp lệ
-                $error = $lang_global['admin_oauth_error_email'];
-            } else {
-                // Thành công
-                $attribs = [
-                    'identity' => $result['id'],
-                    'full_identity' => $crypt->hash($result['id']),
-                    'email' => $result['email'][1],
-                    'name' => isset($result['name']) ? $result['name'] : '',
-                    'first_name' => isset($result['first_name']) ? $result['first_name'] : '',
-                    'last_name' => isset($result['last_name']) ? $result['last_name'] : '',
-                ];
-            }
+        if (isset($result['id'])) {
+            $attribs = [
+                'identity' => $result['id'],
+                'full_identity' => $crypt->hash($result['id']),
+                'email' => isset($result['email']) ? $result['email'] : '',
+                'name' => isset($result['name']) ? $result['name'] : '',
+                'first_name' => isset($result['first_name']) ? $result['first_name'] : '',
+                'last_name' => isset($result['last_name']) ? $result['last_name'] : '',
+            ];
         }
     } catch (Exception $e) {
         $error = $e->getMessage();
