568592277412b56e84d588f54fce9ac79ee25416
Nguyen Anh Tu <anhtu@vinades.vn>
2023-07-11 17:38:03 +0700

    Added another way to check support_rewrite

--- includes/vendor/vinades/nukeviet/Core/Sconfig.php
+++ includes/vendor/vinades/nukeviet/Core/Sconfig.php
@@ -868,6 +868,9 @@ class Sconfig
             }
             if (!empty($deny_access_contents)) {
                 $config_contents .= "<IfModule mod_rewrite.c>\n";
+                $config_contents .= "  <IfModule mod_env.c>\n";
+                $config_contents .= "    SetEnv HTTP_SUPPORT_REWRITE on\n";
+                $config_contents .= "  </IfModule>\n\n";
                 $config_contents .= "  RewriteEngine On\n";
                 $config_contents .= '  RewriteBase ' . NV_BASE_SITEURL . "\n";
                 $config_contents .= $deny_access_contents;
