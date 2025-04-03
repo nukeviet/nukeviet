31e9003ad8a35c91f677434f96394a9430e21627
Nguyễn Nam <nguyennam@vinades.vn>
2025-04-03 15:28:55 +0700

    Đẩy plugin admin_future

--- src/includes/plugin/get_global_admin_theme.php
+++ src/includes/plugin/get_global_admin_theme.php
@@ -31,7 +31,7 @@ nv_add_hook($module_name, 'get_global_admin_theme', $priority, function ($vars) 
     if (in_array($module_name, ['upload', 'themes', 'emailtemplates', 'settings', 'seotools', 'modules', 'extensions', 'webtools', 'language', 'siteinfo', 'authors', 'database'])) {
         return $new_theme;
     }
-    if (($module_info['module_file'] ?? '') == 'voting' and in_array($op, ['main'])) {
+    if (($module_info['module_file'] ?? '') == 'voting' and in_array($op, ['main', 'setting'])) {
         return $new_theme;
     }
 
