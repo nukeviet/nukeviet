a68adbac2d173de997b1d73ec2bb54c78c8a9ea2
Nguyễn Duy Trí <nguyentri97@gmail.com>
2026-01-26 11:40:37 +0700

    active block content with bot

--- src/includes/core/user_functions.php
+++ src/includes/core/user_functions.php
@@ -250,6 +250,11 @@ function nv_blocks_content($sitecontent)
                 }
             }
 
+            // Kiem tra hien thi voi bot
+            if ($client_info['is_bot'] and empty($_row['bot_visible'])) {
+                $_active = false;
+            }
+
             // Kiem tra quyen xem block
             if ($_active and in_array($_row['position'], $array_position, true) and nv_user_in_groups($_row['groups_view'])) {
                 $block_config = $_row['block_config'];
