d87c6f56a52546ab8ddc290a2b75935e64f6b004
Vũ Thảo <thao@vinades.vn>
2026-03-18 18:00:12 +0700

    Nâng cấp hàm csrf_create từ md5 lên sha256

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -3851,9 +3851,7 @@ function mload_url_generate($module, $op, $amp = '&amp;', $checkuser = false, $o
  */
 function csrf_create($key)
 {
-    $timestamp = NV_CURRENTTIME;
-
-    return md5(NV_CHECK_SESSION . '_' . $key . '_' . $timestamp) . $timestamp;
+    return hash_hmac('sha256', NV_CHECK_SESSION . '_' . $key . '_' . NV_CURRENTTIME, NV_CACHE_PREFIX) . NV_CURRENTTIME;
 }
 
 /**
@@ -3872,7 +3870,7 @@ function csrf_check($csrf, $key)
     if ($timestamp < (NV_CURRENTTIME - $lifetime) or $timestamp > NV_CURRENTTIME) {
         return false;
     }
-    $expected = md5(NV_CHECK_SESSION . '_' . $key . '_' . $timestamp) . $timestamp;
+    $expected = hash_hmac('sha256', NV_CHECK_SESSION . '_' . $key . '_' . $timestamp, NV_CACHE_PREFIX) . $timestamp;
 
     return hash_equals($expected, $csrf);
 }
