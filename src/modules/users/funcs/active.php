6b89e8bb889a044a8770cc6af0b8ad06c31b81d3
VINADES.,JSC <contact@vinades.vn>
2026-05-07 17:23:49 +0700

    Refactor user registration logic to handle existing emails and improve data integrity

--- src/modules/users/funcs/active.php
+++ src/modules/users/funcs/active.php
@@ -53,90 +53,102 @@ if ($checknum == $row['checknum']) {
             $check_update_user = true;
         }
     } elseif (!defined('NV_IS_USER') and $global_config['allowuserreg'] == 2) {
-        $sql = 'INSERT INTO ' . NV_MOD_TABLE . ' (
-            group_id, username, md5username, password, email, first_name, last_name,
-            gender, photo, birthday, regdate, question, answer,
-            passlostkey, view_mail, remember, in_groups,
-            active, checknum, last_login, last_ip, last_agent, last_openid, idsite,
-            pass_creation_time, pass_reset_request, email_creation_time, email_verification_time,
-            active_obj
-        ) VALUES (
-            :group_id, :username, :md5_username, :password, :email, :first_name, :last_name,
-            :gender, :photo, :birthday, :regdate, :question, :answer,
-            :passlostkey, :view_mail, :remember, :in_groups,
-            1, :checknum, 0, :last_ip, :last_agent, :last_openid, :idsite,
-            :pass_creation_time, 0, :email_creation_time, :email_verification_time, :active_obj
-        )';
+        $stmt = $db->prepare('SELECT userid FROM ' . NV_MOD_TABLE . ' WHERE email=:email LIMIT 1');
+        $stmt->bindValue(':email', $row['email'], PDO::PARAM_STR);
+        $stmt->execute();
+        $existing_user = $stmt->fetch();
+        if (!empty($existing_user)) {
+            $stmt = $db->prepare('DELETE FROM ' . NV_MOD_TABLE . '_reg WHERE userid=:userid');
+            $stmt->bindValue(':userid', $row['userid'], PDO::PARAM_INT);
+            $stmt->execute();
+            $check_update_user = true;
+        } else {
+            $sql = 'INSERT INTO ' . NV_MOD_TABLE . ' (
+                group_id, username, md5username, password, email, first_name, last_name,
+                gender, photo, birthday, regdate, question, answer,
+                passlostkey, view_mail, remember, in_groups,
+                active, checknum, last_login, last_ip, last_agent, last_openid, idsite,
+                pass_creation_time, pass_reset_request, email_creation_time, email_verification_time,
+                active_obj
+            ) VALUES (
+                :group_id, :username, :md5_username, :password, :email, :first_name, :last_name,
+                :gender, :photo, :birthday, :regdate, :question, :answer,
+                :passlostkey, :view_mail, :remember, :in_groups,
+                1, :checknum, 0, :last_ip, :last_agent, :last_openid, :idsite,
+                :pass_creation_time, 0, :email_creation_time, :email_verification_time, :active_obj
+            )';
 
-        $group_id = (!empty($global_users_config['active_group_newusers']) ? 7 : 4);
+            $group_id = (!empty($global_users_config['active_group_newusers']) ? 7 : 4);
 
-        $sth = $db->prepare($sql);
-        $sth->bindValue(':group_id', $group_id, PDO::PARAM_INT);
-        $sth->bindValue(':username', $row['username'], PDO::PARAM_STR);
-        $sth->bindValue(':md5_username', nv_md5safe($row['username']), PDO::PARAM_STR);
-        $sth->bindValue(':password', $row['password'], PDO::PARAM_STR);
-        $sth->bindValue(':email', $row['email'], PDO::PARAM_STR);
-        $sth->bindValue(':first_name', $row['first_name'], PDO::PARAM_STR);
-        $sth->bindValue(':last_name', $row['last_name'], PDO::PARAM_STR);
-        $sth->bindValue(':gender', $row['gender'], PDO::PARAM_STR);
-        $sth->bindValue(':photo', '', PDO::PARAM_STR);
-        $sth->bindValue(':birthday', $row['birthday'], PDO::PARAM_INT);
-        $sth->bindValue(':regdate', $row['regdate'], PDO::PARAM_INT);
-        $sth->bindValue(':question', $row['question'], PDO::PARAM_STR);
-        $sth->bindValue(':answer', $row['answer'], PDO::PARAM_STR);
-        $sth->bindValue(':passlostkey', '', PDO::PARAM_STR);
-        $sth->bindValue(':view_mail', 0, PDO::PARAM_INT);
-        $sth->bindValue(':remember', 1, PDO::PARAM_INT);
-        $sth->bindValue(':in_groups', $group_id, PDO::PARAM_STR);
-        $sth->bindValue(':checknum', '', PDO::PARAM_STR);
-        $sth->bindValue(':last_ip', '', PDO::PARAM_STR);
-        $sth->bindValue(':last_agent', '', PDO::PARAM_STR);
-        $sth->bindValue(':last_openid', '', PDO::PARAM_STR);
-        $sth->bindValue(':idsite', $global_config['idsite'], PDO::PARAM_INT);
-        $sth->bindValue(':pass_creation_time', NV_CURRENTTIME, PDO::PARAM_INT);
-        $sth->bindValue(':email_creation_time', NV_CURRENTTIME, PDO::PARAM_INT);
-        $sth->bindValue(':email_verification_time', NV_CURRENTTIME, PDO::PARAM_INT);
-        $sth->bindValue(':active_obj', 'EMAIL', PDO::PARAM_STR);
-        $sth->execute();
-        $userid = $db->lastInsertId();
-        if ($userid) {
-            $users_info = json_decode($row['users_info'], true);
-            $query_field = [];
-            $query_field['userid'] = $userid;
-            $result_field = $db->query('SELECT * FROM ' . NV_MOD_TABLE . '_field ORDER BY fid ASC');
-            while ($row_f = $result_field->fetch()) {
-                if ($row_f['is_system'] == 1) {
-                    continue;
+            $sth = $db->prepare($sql);
+            $sth->bindValue(':group_id', $group_id, PDO::PARAM_INT);
+            $sth->bindValue(':username', $row['username'], PDO::PARAM_STR);
+            $sth->bindValue(':md5_username', nv_md5safe($row['username']), PDO::PARAM_STR);
+            $sth->bindValue(':password', $row['password'], PDO::PARAM_STR);
+            $sth->bindValue(':email', $row['email'], PDO::PARAM_STR);
+            $sth->bindValue(':first_name', $row['first_name'], PDO::PARAM_STR);
+            $sth->bindValue(':last_name', $row['last_name'], PDO::PARAM_STR);
+            $sth->bindValue(':gender', $row['gender'], PDO::PARAM_STR);
+            $sth->bindValue(':photo', '', PDO::PARAM_STR);
+            $sth->bindValue(':birthday', $row['birthday'], PDO::PARAM_INT);
+            $sth->bindValue(':regdate', $row['regdate'], PDO::PARAM_INT);
+            $sth->bindValue(':question', $row['question'], PDO::PARAM_STR);
+            $sth->bindValue(':answer', $row['answer'], PDO::PARAM_STR);
+            $sth->bindValue(':passlostkey', '', PDO::PARAM_STR);
+            $sth->bindValue(':view_mail', 0, PDO::PARAM_INT);
+            $sth->bindValue(':remember', 1, PDO::PARAM_INT);
+            $sth->bindValue(':in_groups', $group_id, PDO::PARAM_STR);
+            $sth->bindValue(':checknum', '', PDO::PARAM_STR);
+            $sth->bindValue(':last_ip', '', PDO::PARAM_STR);
+            $sth->bindValue(':last_agent', '', PDO::PARAM_STR);
+            $sth->bindValue(':last_openid', '', PDO::PARAM_STR);
+            $sth->bindValue(':idsite', $global_config['idsite'], PDO::PARAM_INT);
+            $sth->bindValue(':pass_creation_time', NV_CURRENTTIME, PDO::PARAM_INT);
+            $sth->bindValue(':email_creation_time', NV_CURRENTTIME, PDO::PARAM_INT);
+            $sth->bindValue(':email_verification_time', NV_CURRENTTIME, PDO::PARAM_INT);
+            $sth->bindValue(':active_obj', 'EMAIL', PDO::PARAM_STR);
+            $sth->execute();
+            $userid = $db->lastInsertId();
+            if ($userid) {
+                $users_info = json_decode($row['users_info'], true);
+                $query_field = [];
+                $query_field['userid'] = $userid;
+                $result_field = $db->query('SELECT * FROM ' . NV_MOD_TABLE . '_field ORDER BY fid ASC');
+                while ($row_f = $result_field->fetch()) {
+                    if ($row_f['is_system'] == 1) {
+                        continue;
+                    }
+                    if ($row_f['field_type'] == 'number' or $row_f['field_type'] == 'date') {
+                        $default_value = (float) ($row_f['default_value']);
+                    } else {
+                        $default_value = get_value_by_lang($row_f['default_value']);
+                    }
+                    $query_field[$row_f['field']] = (isset($users_info[$row_f['field']])) ? $users_info[$row_f['field']] : $default_value;
                 }
-                if ($row_f['field_type'] == 'number' or $row_f['field_type'] == 'date') {
-                    $default_value = (float) ($row_f['default_value']);
+
+                if (userInfoTabDb($query_field)) {
+                    if (!empty($global_users_config['active_group_newusers'])) {
+                        nv_groups_add_user(7, $row['userid'], 1, $module_data);
+                    } else {
+                        $db->query('UPDATE ' . NV_MOD_TABLE . '_groups SET numbers = numbers+1 WHERE group_id=4');
+                    }
+                    $db->query('DELETE FROM ' . NV_MOD_TABLE . '_reg WHERE userid=' . $row['userid']);
+
+                    // Callback sau khi đăng ký
+                    if (nv_function_exists('nv_user_register_callback')) {
+                        /** @disregard P1010 */
+                        // phpcs:ignore
+                        nv_user_register_callback($userid);
+                    }
+
+                    $check_update_user = true;
+                    nv_insert_logs(NV_LANG_DATA, $module_name, $nv_Lang->getModule('account_active_log'), $row['username'] . ' | ' . $client_info['ip'], 0);
                 } else {
-                    $default_value = get_value_by_lang($row_f['default_value']);
+                    $db->query('DELETE FROM ' . NV_MOD_TABLE . ' WHERE userid=' . $userid);
                 }
-                $query_field[$row_f['field']] = (isset($users_info[$row_f['field']])) ? $users_info[$row_f['field']] : $default_value;
+
+                $nv_Cache->delMod($module_name);
             }
-
-            if (userInfoTabDb($query_field)) {
-                if (!empty($global_users_config['active_group_newusers'])) {
-                    nv_groups_add_user(7, $row['userid'], 1, $module_data);
-                } else {
-                    $db->query('UPDATE ' . NV_MOD_TABLE . '_groups SET numbers = numbers+1 WHERE group_id=4');
-                }
-                $db->query('DELETE FROM ' . NV_MOD_TABLE . '_reg WHERE userid=' . $row['userid']);
-
-                // Callback sau khi đăng ký
-                if (nv_function_exists('nv_user_register_callback')) {
-                    /** @disregard P1010 */
-                    nv_user_register_callback($userid);
-                }
-
-                $check_update_user = true;
-                nv_insert_logs(NV_LANG_DATA, $module_name, $nv_Lang->getModule('account_active_log'), $row['username'] . ' | ' . $client_info['ip'], 0);
-            } else {
-                $db->query('DELETE FROM ' . NV_MOD_TABLE . ' WHERE userid=' . $userid);
-            }
-
-            $nv_Cache->delMod($module_name);
         }
     }
 }
