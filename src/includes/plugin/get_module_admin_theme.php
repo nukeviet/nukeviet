e0d86122c57c3bf436e33a0dff2ad139180f4fdf
Phan Tấn Dũng <dungpt@vinades.vn>
2026-05-29 10:33:51 +0700

    Update admin plugin

--- src/includes/plugin/get_module_admin_theme.php
+++ src/includes/plugin/get_module_admin_theme.php
@@ -23,13 +23,10 @@ nv_add_hook($module_name, 'get_module_admin_theme', $priority, function ($vars) 
     $new_theme = 'admin_future';
     $module_file = $module_info['module_file'] ?? '';
 
-    if (in_array($module_file, ['users', 'banners', 'contact', 'inform', 'menu', 'voting', 'news', 'statistics'], true)) {
+    if (in_array($module_file, ['users', 'banners', 'contact', 'inform', 'menu', 'voting', 'news', 'statistics', 'page'], true)) {
         return $new_theme;
     }
     if (in_array($module_name, ['upload', 'themes', 'emailtemplates', 'settings', 'seotools', 'modules', 'extensions', 'webtools', 'language', 'siteinfo', 'authors', 'database', 'comment', 'myapi'])) {
-        return $new_theme;
-    }
-    if ($module_file == 'page' and in_array($op, ['main', 'config', 'content'])) {
         return $new_theme;
     }
     if ($module_file == 'feeds' and in_array($op, ['main'])) {
