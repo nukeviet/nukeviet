7a7fa92295095fdc97ae5d0f57b6d0c78fc61fe1
VINADES.,JSC <contact@vinades.vn>
2026-07-11 11:23:07 +0700

    Use random IV and HMAC for SSO URL encryption instead of fixed IV

--- src/includes/vendor/vinades/nukeviet/Client/Sso.php
+++ src/includes/vendor/vinades/nukeviet/Client/Sso.php
@@ -46,22 +46,62 @@ class Sso
         $sso_reset = self::encrypt(urlRewriteWithDomain($sso_reset, NV_MY_DOMAIN));
 
         /** @disregard P1011 */
+        // phpcs:ignore
         return SSO_REGISTER_DOMAIN . (!defined('SSO_REGISTER_LANGSINGLE') ? '/' . NV_LANG_DATA : '') . '/users/login/?sso_redirect=' . self::encrypt(str_replace('&amp;', '&', $return_url)) . '&sso_reset=' . $sso_reset . '&client=' . urlencode(NV_MY_DOMAIN);
     }
 
     /**
+     * Mã hóa chuỗi để truyền qua URL giữa các site SSO.
+     *
      * @param string $str
-     * @return string
+     * @return string Chuỗi đã mã hóa, hoặc '' nếu mã hóa thất bại
      */
     public static function encrypt(string $str): string
     {
         /** @disregard P1011 */
-        return strtr(openssl_encrypt($str, 'aes-256-cbc', SSO_REGISTER_SECRET, 0, substr(SSO_REGISTER_SECRET, 0, 16)), '+/=', '-_,');
+        // phpcs:ignore
+        $key = SSO_REGISTER_SECRET;
+
+        $iv = random_bytes(16);
+        $ciphertext = openssl_encrypt($str, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+        if ($ciphertext === false) {
+            return '';
+        }
+
+        $hmac = hash_hmac('sha256', $iv . $ciphertext, $key, true);
+        $packed = $hmac . $iv . $ciphertext;
+
+        return strtr(base64_encode($packed), '+/=', '-_,');
     }
 
+    /**
+     * Giải mã chuỗi nhận từ URL SSO.
+     *
+     * @param string $str
+     * @return string Chuỗi gốc, hoặc '' nếu giải mã/xác thực thất bại
+     */
     public static function decrypt(string $str): string
     {
         /** @disregard P1011 */
-        return openssl_decrypt(strtr($str, '-_,', '+/='), 'aes-256-cbc', SSO_REGISTER_SECRET, 0, substr(SSO_REGISTER_SECRET, 0, 16));
+        // phpcs:ignore
+        $key = SSO_REGISTER_SECRET;
+
+        $packed = base64_decode(strtr($str, '-_,', '+/='), true);
+        if ($packed === false || strlen($packed) < 48) {
+            return '';
+        }
+
+        $hmac = substr($packed, 0, 32);
+        $iv = substr($packed, 32, 16);
+        $ciphertext = substr($packed, 48);
+
+        $calculated_hmac = hash_hmac('sha256', $iv . $ciphertext, $key, true);
+        if (!hash_equals($hmac, $calculated_hmac)) {
+            return '';
+        }
+
+        $plaintext = openssl_decrypt($ciphertext, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+
+        return $plaintext === false ? '' : $plaintext;
     }
 }
