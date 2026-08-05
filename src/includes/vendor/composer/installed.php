2e718105080c514b6305118aeaf82ebd74758dc5
VINADES.,JSC <contact@vinades.vn>
2026-08-05 13:38:13 +0700

    guzzlehttp/guzzle v7.15.2

--- src/includes/vendor/composer/installed.php
+++ src/includes/vendor/composer/installed.php
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'guzzlehttp/guzzle' => array(
-            'pretty_version' => '7.15.1',
-            'version' => '7.15.1.0',
-            'reference' => '61443dfb33c62f308ee8add20f45b4d6e4bf8d2f',
+            'pretty_version' => '7.15.2',
+            'version' => '7.15.2.0',
+            'reference' => '744101956d78b7c1384d0cbf379db13e859167bf',
             'type' => 'library',
             'install_path' => __DIR__ . '/../guzzlehttp/guzzle',
             'aliases' => array(),
